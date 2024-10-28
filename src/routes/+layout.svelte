137d181ef95f87131d6df82b0bfc7dcfa9922385
Eric Branson <eric.branson@gmail.com>
2024-10-28 11:52:56 -0600

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
+	import { Nav } from '@skeletonlabs/skeleton-svelte'
+
 	import '../app.css';
+	interface Link {
+		link: string;
+		label: string;
+	}
+	let links:Link[] =[
+		{link: '/', label: 'Home'},
+		{link: '/register', label: 'Register'},
+		{link: '/register/emailVerification', label:'Email Verification'}
+	]
 	let { children } = $props();
+
+	let navValue = $state('0')
 </script>
+
+<Nav.Bar bind:value={navValue}>
+	{#each links as link, i}
+		<Nav.Tile id={i.toString()} label={link.label} href={link.link}>
+			{link.label}
+		</Nav.Tile>
+	{/each}
+</Nav.Bar>
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+   // @ts-ignore: it works
+   import { PUBLIC_COMPANY_NAME } from '$env/static/public'
+   import type { PageData } from "./$types";
+
+   interface Props {
+      data: PageData;
+   }
+
+   let { data }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>{PUBLIC_COMPANY_NAME} | Home</title>
+</svelte:head>
+
+<h1 class="h1">{PUBLIC_COMPANY_NAME}</h1>
+{#if data.user}
+   You're logged in
+   {:else}
+   You're not logged in
+{/if}
