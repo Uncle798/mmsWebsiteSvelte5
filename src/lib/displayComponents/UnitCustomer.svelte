5262910757bef15af5fe9bc99ff2cc24ab393f6e
Eric Branson <eric.branson@gmail.com>
2024-11-11 15:16:22 -0700

    init

---
+++ src/lib/displayComponents/UnitCustomer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+   import type { Unit } from "@prisma/client";
+   interface Props {
+      unit: Unit
+   }
+   let { unit }:Props = $props();
+</script>
+<div class="card p-4">
+   <span class="h3">Unit</span>
+   <span class="h4">Number {unit.num.replace(/^0+/gm,'')}</span>
+   <span class="h5">Size {unit.size.replace(/^0+/gm, '').replace(/x0/gm, 'x')}</span>
+   <span class="h5">Price ${unit.advertisedPrice}</span>
+</div>
---
+++ src/lib/formComponents/Checkbox.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+   import type { InputConstraint } from "sveltekit-superforms";
+   interface Props {
+      value:boolean;
+      label: string | undefined;
+      errors: string[] | undefined;
+      constraints: InputConstraint | undefined;
+      checked?: boolean;
+      name: string | null | undefined
+   }
+   let { value = $bindable(), label, errors, constraints, checked=false, name, ...others }:Props = $props()
+</script>
+<label for="checkbox" class="label"><span class="label-text">{label}</span>
+   {#if checked}
+      <input 
+         type="checkbox" 
+         name={name} 
+         class="checkbox" 
+         checked
+         bind:value={value} 
+         aria-invalid={errors ? 'true' : undefined}
+         {...constraints}
+         {...others}
+      />
+   {:else}
+      <input 
+         type="checkbox" 
+         name={name} 
+         class="checkbox" 
+         checked
+         bind:value={value} 
+         aria-invalid={errors ? 'true' : undefined}
+         {...constraints}
+         {...others}
+      />
+   {/if}
+</label>
+{#if errors}<span class="invalid">{errors}</span>{/if}
