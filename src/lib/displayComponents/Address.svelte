5e93731d13bd73d5e284ae64495710856f656361
Eric Branson <eric.branson@gmail.com>
2024-11-12 13:34:29 -0700

    partial address type

--- src/lib/displayComponents/Address.svelte
+++ src/lib/displayComponents/Address.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-	import type { ContactInfo } from "@prisma/client";
+	import type { PartialContactInfo } from "$lib/server/partialTypes";
    interface Props {
-      address: ContactInfo;
+      address: PartialContactInfo;
    }
    let {address}:Props = $props()
 </script>
 
 <div class="card p-4">
    <p>{address.address1}</p>
-   {#if address.address2}
-      <p>{address.address2}</p>
-   {/if}
    <p>{address.city.trim()}, {address.state.trim()}, {address.zip.trim()}</p>
 </div>
