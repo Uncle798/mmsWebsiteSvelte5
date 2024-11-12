62d70daaf860ff913a2b64d277736dc81cb998a8
Eric Branson <eric.branson@gmail.com>
2024-11-12 13:50:16 -0700

    gone

--- src/lib/server/qStash.ts
+++
@@ -1,7 +0,0 @@
-import { QSTASH_TOKEN, UPSTASH_WORKFLOW_URL } from "$env/static/private";
-import { Client } from "@upstash/qstash";
-
-export const qstash = new Client({
-   token: QSTASH_TOKEN, 
-   baseUrl: UPSTASH_WORKFLOW_URL
-})
