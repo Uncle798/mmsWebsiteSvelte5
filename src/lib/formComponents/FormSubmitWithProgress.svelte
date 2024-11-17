6fdfcbb65c26a39530566d84ce995c52375a559c
Eric Branson <eric.branson@gmail.com>
2024-11-17 12:27:25 -0700

    better name

--- src/lib/formComponents/FormSubmitWithProgress.svelte
+++ src/lib/formComponents/FormSubmitWithProgress.svelte
@@ -3,13 +3,16 @@
    interface Props {
       delayed: boolean,
       timeout: boolean,
+      buttonText?: string,
    }
-   let { delayed = $bindable(), timeout=$bindable() }:Props = $props();
+   let { delayed = $bindable(), timeout=$bindable(), buttonText=$bindable('Submit') }:Props = $props();
 </script>
-
-{#if delayed && !timeout}
+<div class="flex">
+   <button class="btn">{buttonText}</button>
+   {#if delayed && !timeout}
    <ProgressRing value={null} size="size-8" strokeWidth="6px" meterStroke="stroke-secondary-600-400" trackStroke="stroke-secondary-50-950" />
-{/if}
-{#if timeout}
+   {/if}
+   {#if timeout}
    <Progress value={null} width="size-8" />
-{/if}
+   {/if}
+</div>
