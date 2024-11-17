0c3b13074ce18e0a258787108bbd153601baaacb
Eric Branson <eric.branson@gmail.com>
2024-11-17 12:34:22 -0700

    Header better EmailVerificationForm added badge for email verified

--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -4,15 +4,18 @@
     import AddressForm from '$lib/forms/AddressForm.svelte';
     import NameForm from '$lib/forms/NameChangeForm.svelte';
     import EmailForm from '$lib/forms/EmailChangeForm.svelte';
-    import EmailVerification from '../../register/emailVerification/+page.svelte'
+    import EmailVerification from '$lib/forms/EmailVerificationForm.svelte'
 	import Address from '$lib/displayComponents/Address.svelte';
+	import Header from '$lib/Header.svelte';
+	import { BadgeCheck, Check, Verified } from 'lucide-svelte';
     
     let {data}:{ data: PageData} = $props();
     let addressModalOpen = $state(false);
     let nameModalOpen = $state(false);
     let emailModalOpen = $state(false);
-    let emailVerificationOpen = $state(false);
+    let emailVerificationModalOpen = $state(false);
 </script>
+<Header title='Settings for {data.user?.givenName}' />
 
 <span class="h1">{data.user?.givenName} {data.user?.familyName}</span>
 <Modal
@@ -30,7 +33,19 @@ backdropClasses="backdrop-blur-sm"
     {/snippet}
 </Modal>
 
-<span class="h4">{data.user?.email}</span>
+<span class="h4 flex">{data.user?.email}
+    {#if data.user?.emailVerified}
+    <div class="flex p-1">
+        <BadgeCheck />  
+        <div class="h6 p-1">
+            Email Verified
+        </div>
+    </div>
+    
+    {:else}
+    <button class="btn h6" onclick={()=>emailVerificationModalOpen=true}>Please confirm your email address</button>
+    {/if}
+</span>
 <Modal
     bind:open={emailModalOpen}
     triggerBase="btn preset-tonal"
@@ -41,24 +56,25 @@ backdropClasses="backdrop-blur-sm"
         Change email address
     {/snippet}
     {#snippet content()}
-        <EmailForm data={data.emailForm} bind:emailModalOpen={emailModalOpen} bind:emailVerificationOpen={emailVerificationOpen} />
+        <EmailForm data={data.emailForm} bind:emailModalOpen={emailModalOpen} bind:emailVerificationModalOpen={emailVerificationModalOpen} />
         <button class="btn" onclick={()=>emailModalOpen = false}>Cancel</button>
-    {/snippet}
+        {/snippet}
 </Modal>
-
+        
 <Modal
-    bind:open={emailVerificationOpen}
+    bind:open={emailVerificationModalOpen}
     contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
     backdropClasses="backdrop-blur-sm"
->
+    >
     {#snippet content()}
-        <EmailVerification data={data.emailVerificationForm} bind:emailVerificationOpen={emailVerificationOpen} />
+        <EmailVerification data={data.emailVerificationForm} bind:emailVerificationModalOpen={emailVerificationModalOpen} redirect='false'/>
+        <button class="btn" onclick={()=>emailVerificationModalOpen = false}>Cancel</button>
     {/snippet}
 </Modal>
 
-    {#if data.address}
-        <Address address={data.address} />
-    {/if}
+{#if data.address}
+    <Address address={data.address} />
+{/if}
 
 <Modal
 	bind:open={addressModalOpen}
