d27bcb235393f912cf57d534ccb37468662b5642
Eric Branson <eric.branson@gmail.com>
2024-11-17 12:37:54 -0700

    header

--- src/routes/units/available/+page.svelte
+++ src/routes/units/available/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import Header from '$lib/Header.svelte';
+import type { PageData } from './$types';
     
     let { data }:{data:PageData} = $props();
 </script>
-
+<Header title='Available Units' />
 <div class="table-wrap">
     <table class="table">
         <caption>Available units.</caption>
