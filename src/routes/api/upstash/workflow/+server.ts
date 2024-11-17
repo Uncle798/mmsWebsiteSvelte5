4df0ef21e6d6968002da43f8930d30e66c37760a
Eric Branson <eric.branson@gmail.com>
2024-11-17 12:35:58 -0700

    troubleshooting

--- src/routes/api/upstash/workflow/+server.ts
+++ src/routes/api/upstash/workflow/+server.ts
@@ -1,15 +1,23 @@
-import { serve } from '@upstash/qstash/svelte';
+import { serve } from '@upstash/workflow/svelte';
 import { env } from '$env/dynamic/private';
 import type { RequestHandler } from './$types';
+import { UPSTASH_WORKFLOW_URL } from '$env/static/private';
 
-export const POST: RequestHandler = serve(
+export const { POST } = serve(
    async (context) => {
+      
+      const payload = context.requestPayload
+      console.log(`payload: ${payload}`);
       await context.run("1st step", () => {
-         console.log('1st step ran')
+         console.log('1st step ran' + payload)
       })
       await context.run("second-step", () => {
          console.log("second step ran")
        })
    },
-   { env, verbose:true,  }
+   { 
+      env,
+      verbose: true,
+      baseUrl: process.env.NODE_ENV === 'development' ? 'https://relative-oryx-endlessly.ngrok-free.app/' : undefined
+   }
 )
