ccc27c689c9061e302bbab0584f74f2ac8b4f448
Eric Branson <eric.branson@gmail.com>
2024-11-19 16:57:06 -0700

    towards an employee view of [userId]

--- src/routes/users/[userId]/+page.server.ts
+++ src/routes/users/[userId]/+page.server.ts
@@ -1,65 +1,40 @@
-import { prisma } from '$lib/server/prisma';
-import { fail, redirect } from '@sveltejs/kit';
-import type { PageServerLoad, Actions } from './$types';
-import { superValidate, message } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
-import { addressFormSchema, emailFormSchema, emailVerificationFormSchema, nameFormSchema } from '$lib/formSchemas/schemas';
-
-export const load:PageServerLoad = (async (event) => {
-   if(!event.locals.user){
-      redirect(302, '/login?toast=unauthorized')
+import { error, redirect } from "@sveltejs/kit";
+import type { PageServerLoad, Actions } from "./$types";
+import { prisma } from "$lib/server/prisma";
+import { fail } from "@sveltejs/kit";
+export const load: PageServerLoad = async (event) => {
+   if(!event.locals.user?.employee){
+      redirect(302, '/login?toast=employee')
    }
-   if(event.locals.user.employee){
-      const userId = event.params.userId;
-      if(!userId){
-         fail(404);
-      }
-      const dbUser = await prisma.user.findFirst({
-         where: {
-            id: userId
-         }
-      })
-      const address = prisma.contactInfo.findFirst({
-         where: {
-            userId: dbUser?.id
-         }
-      })
-      const leases = prisma.lease.findMany({
-         where: {
-            customerId: dbUser?.id
-         }
-      });
-      const invoices = prisma.invoice.findMany({
-         where: {
-            customerId: dbUser?.id
-         }
-      });
-      const payments = prisma.paymentRecord.findMany({
-         where: {
-            customerId: dbUser?.id
-         }
-      })
-      return { dbUser, address, leases, invoices, payments }
-   }
-   const address = await prisma.contactInfo.findFirst({
+   const userId = event.params.userId;
+   const dbUser = await prisma.user.findFirst({
       where: {
-         userId: event.locals.user.id,
-         softDelete: false
-      },
-
+         id: userId
+      }
    })
-   const addressForm = await superValidate(zod(addressFormSchema));
-   const nameForm = await superValidate(zod(nameFormSchema));
-   const emailForm = await superValidate(zod(emailFormSchema));
-   const emailVerificationForm = await superValidate(zod(emailVerificationFormSchema));
-   return { addressForm, nameForm, emailForm, emailVerificationForm, address };
-})
-
-
-export const actions: Actions = {
-   default: async (event) => {
-      const formData = await event.request.formData();
-      const addressForm = await superValidate(formData, zod(addressFormSchema));
-      return message(addressForm, 'Address updated')
+   if(dbUser === null){
+      error(404, {message:'User not found'})
    }
+   const addressPromise = prisma.contactInfo.findFirst({
+      where: {
+         userId: dbUser?.id,
+         softDelete: false
+      }
+   })
+   const leasesPromise = prisma.lease.findMany({
+      where: {
+         customerId: dbUser?.id
+      }
+   })
+   const invoicesPromise = prisma.invoice.findMany({
+      where: {
+         customerId: dbUser?.id
+      }
+   })
+   const paymentsPromise = prisma.paymentRecord.findMany({
+      where: {
+         customerId: dbUser.id
+      }
+   })
+   return { dbUser, addressPromise, leasesPromise, invoicesPromise, paymentsPromise}
 };
--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -1,105 +1,30 @@
 <script lang="ts">
-    import { Modal } from '@skeletonlabs/skeleton-svelte';
-    import type { PageData } from './$types';
-    import AddressForm from '$lib/forms/AddressForm.svelte';
-    import NameForm from '$lib/forms/NameChangeForm.svelte';
-    import EmailForm from '$lib/forms/EmailChangeForm.svelte';
-    import EmailVerification from '$lib/forms/EmailVerificationForm.svelte'
-	import Address from '$lib/displayComponents/Address.svelte';
-	import Header from '$lib/Header.svelte';
-	import { BadgeCheck, Check, Verified } from 'lucide-svelte';
-    
-    let {data}:{ data: PageData} = $props();
-    let addressModalOpen = $state(false);
-    let nameModalOpen = $state(false);
-    let emailModalOpen = $state(false);
-    let emailVerification = $state(false);
-    let emailVerificationModalOpen = $state(false);
+    import Address from '$lib/displayComponents/Address.svelte';
+	import LeaseEmployee from '$lib/displayComponents/LeaseEmployee.svelte';
+import User from '$lib/displayComponents/User.svelte';
+import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
 </script>
-<Header title='Settings for {data.user?.givenName}' />
-
-
-<span class="h1">{data.user?.givenName} {data.user?.familyName}</span>
-{#if data.user?.organizationName}
-    <span class="h2">{data.user.organizationName}</span>
+{#if data.dbUser}
+   <User user={data.dbUser}/>
+   {:else}
+   ...loading user
 {/if}
-<Modal
-bind:open={nameModalOpen}
-triggerBase="btn preset-tonal"
-contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-backdropClasses="backdrop-blur-sm"
->
-    {#snippet trigger()}
-        Change Name
-    {/snippet}
-    {#snippet content()}
-        <NameForm data={data.nameForm} bind:nameModalOpen={nameModalOpen} />
-        <button class="btn" onclick={()=>nameModalOpen=false}>Cancel</button>
-    {/snippet}
-</Modal>
+{#await data.addressPromise}
+   ...loading address
+{:then address}
+   {#if address}
+   <Address address={address}/>
+   {/if}
+{/await}
 
-<span class="h4 flex">{data.user?.email}
-    {#if data.user?.emailVerified}
-    <div class="flex p-1">
-        <BadgeCheck />  
-        <div class="h6 p-1">
-            Email Verified
-        </div>
-    </div>
-    
-    {:else}
-    <button class="btn h6" onclick={()=>emailVerificationModalOpen=true}>Please confirm your email address</button>
-    {/if}
-</span>
-<Modal
-    bind:open={emailModalOpen}
-    triggerBase="btn preset-tonal"
-    contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-    backdropClasses="backdrop-blur-sm"
->
-    {#snippet trigger()}
-        Change email address
-    {/snippet}
-    {#snippet content()}
-        {#if !emailVerification}
-            <EmailForm data={data.emailForm} bind:emailModalOpen={emailModalOpen} bind:emailVerification={emailVerification} />
-        {:else}
-            <EmailVerification data={data.emailVerificationForm} bind:emailVerificationModalOpen={emailVerificationModalOpen} redirect='false' bind:emailVerification={emailVerification}/>
-            {/if}
-            <button class="btn" onclick={()=>emailModalOpen = false}>Cancel</button>
-        {/snippet}
-</Modal>
-    
-<Modal
-    bind:open={emailVerificationModalOpen}
-    contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-    backdropClasses="backdrop-blur-sm"
-    >
-    {#snippet content()}
-        <EmailVerification data={data.emailVerificationForm} bind:emailVerificationModalOpen={emailVerificationModalOpen} redirect='false' bind:emailVerification={emailVerification}/>
-        <button class="btn" onclick={()=>emailVerificationModalOpen = false}>Cancel</button>
-    {/snippet}
-</Modal>
-
-{#if data.address}
-    <Address address={data.address} />
-{/if}
-
-<Modal
-	bind:open={addressModalOpen}
-	triggerBase="btn preset-tonal"
-	contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-	backdropClasses="backdrop-blur-sm"
->
-    {#snippet trigger()}
-        {#if data.address}
-            Change Address
-        {:else}
-            Add address
-        {/if}
-    {/snippet}
-    {#snippet content()}
-        <AddressForm data={data.addressForm} bind:addressModalOpen={addressModalOpen}/>
-        <button class="btn" onclick={()=>addressModalOpen=false}>Close</button>
-    {/snippet}
-</Modal>
+{#await data.leasesPromise}
+   ...loading leases
+{:then leases}
+   {#if leases} 
+      {#each leases as lease}
+         <LeaseEmployee lease={lease} customer={data.dbUser}/>
+      {/each}
+   {/if}
+{/await}
