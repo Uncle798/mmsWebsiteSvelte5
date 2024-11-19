2231031ab10b50518ae5589caea8ba96db013775
Eric Branson <eric.branson@gmail.com>
2024-11-19 16:55:19 -0700

    updated to new docs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,13 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
-import { join } from 'path';
-import { skeleton } from '@skeletonlabs/skeleton/plugin'
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin'
 import * as themes from '@skeletonlabs/skeleton/themes'
 
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+		contentPath(import.meta.url, 'svelte')
 	],
 
 	theme: {
