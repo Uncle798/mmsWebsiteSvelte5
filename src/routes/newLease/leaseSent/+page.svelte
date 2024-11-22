f179a3fdf94fa958d0c0796e03505dc1e90e0d65
Eric Branson <eric.branson@gmail.com>
2024-11-22 14:26:53 -0700

    better copy

--- src/routes/newLease/leaseSent/+page.svelte
+++ src/routes/newLease/leaseSent/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import Header from '$lib/Header.svelte';
-import type { PageData } from './$types';
+    import type { PageData } from './$types';
 
     let { data }: { data: PageData } = $props();
+	import { PUBLIC_ANVIL_EMAIL, PUBLIC_COMPANY_NAME } from '$env/static/public';
 </script>
 <Header title='Lease Sent' />
-<pre>{data.packetDetails}</pre>
+<span>Your lease has been sent to {data.user?.email} from {PUBLIC_ANVIL_EMAIL}</span>
+<span>Leases are valid when both parties have signed. {PUBLIC_COMPANY_NAME} will sign your lease during normal business hours.</span>
