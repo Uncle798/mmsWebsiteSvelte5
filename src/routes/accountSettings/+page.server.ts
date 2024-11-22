9a8ef11a6cc3d2cebb10ecd70047e93656c0d494
Eric Branson <eric.branson@gmail.com>
2024-11-22 14:24:49 -0700

    now returning promises to be awaited on the front end.

--- src/routes/accountSettings/+page.server.ts
+++ src/routes/accountSettings/+page.server.ts
@@ -1,33 +1,52 @@
 import { prisma } from '$lib/server/prisma';
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad, Actions } from './$types';
-import { superValidate, message } from 'sveltekit-superforms';
+import type { PageServerLoad, } from './$types';
+import { superValidate,  } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { addressFormSchema, emailFormSchema, emailVerificationFormSchema, nameFormSchema } from '$lib/formSchemas/schemas';
+import { addressFormSchema, emailFormSchema, emailVerificationFormSchema, endLeaseSchema, nameFormSchema, passwordFormSchema } from '$lib/formSchemas/schemas';
 
 export const load:PageServerLoad = (async (event) => {
    if(!event.locals.user){
       redirect(302, '/login?toast=unauthorized')
    }
-   const address = await prisma.contactInfo.findFirst({
+   const addressPromise = prisma.contactInfo.findFirst({
       where: {
          userId: event.locals.user.id,
          softDelete: false
       },
 
    })
+   const leasesPromise = prisma.lease.findMany({
+      where: {
+         customerId: event.locals.user.id
+      }
+   });
+   const invoicesPromise = prisma.invoice.findMany({
+      where: {
+         customerId: event.locals.user.id
+      }
+   });
+   const paymentsPromise = prisma.paymentRecord.findMany({
+      where: {
+         customerId: event.locals.user.id
+      }
+   })
    const addressForm = await superValidate(zod(addressFormSchema));
    const nameForm = await superValidate(zod(nameFormSchema));
    const emailForm = await superValidate(zod(emailFormSchema));
    const emailVerificationForm = await superValidate(zod(emailVerificationFormSchema));
-   return { addressForm, nameForm, emailForm, emailVerificationForm, address };
+   const passwordChangeForm = await superValidate(zod(passwordFormSchema));
+   const leaseEndForm = await superValidate(zod(endLeaseSchema));
+   return { 
+      addressForm, 
+      nameForm, 
+      emailForm, 
+      emailVerificationForm, 
+      passwordChangeForm,
+      leaseEndForm,
+      addressPromise, 
+      leasesPromise, 
+      invoicesPromise, 
+      paymentsPromise };
 })
 
-
-export const actions: Actions = {
-   default: async (event) => {
-      const formData = await event.request.formData();
-      const addressForm = await superValidate(formData, zod(addressFormSchema));
-      return message(addressForm, 'Address updated')
-   }
-};
