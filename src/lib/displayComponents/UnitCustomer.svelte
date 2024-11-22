a59cb77e83a00d69ef923536490065319ff82d6a
Eric Branson <eric.branson@gmail.com>
2024-11-22 14:23:33 -0700

    added :

--- src/lib/displayComponents/UnitCustomer.svelte
+++ src/lib/displayComponents/UnitCustomer.svelte
@@ -7,9 +7,9 @@
 </script>
 <div class="card p-4">
    <span class="h3">Unit</span>
-   <span class="h4">Number {unit.num.replace(/^0+/gm,'')}</span>
-   <span class="h5">Size {unit.size.replace(/^0+/gm, '').replace(/x0/gm, 'x')}</span>
-   <span class="h5">Description {unit.description}</span>
-   <span class="h5">Price ${unit.advertisedPrice}</span>
-   <span class="h5">Deposit ${unit.deposit}</span>
+   <span class="h4">Number: {unit.num.replace(/^0+/gm,'')}</span>
+   <span class="h5">Size: {unit.size.replace(/^0+/gm, '').replace(/x0/gm, 'x')}</span>
+   <span class="h5">Description: {unit.description}</span>
+   <span class="h5">Price: ${unit.advertisedPrice}</span>
+   <span class="h5">Deposit: ${unit.deposit}</span>
 </div>
