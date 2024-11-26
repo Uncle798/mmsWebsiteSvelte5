385d7a99f4cdc32f9e7e13c53ea35e6d602f1685
Eric Branson <eric.branson@gmail.com>
2024-11-26 10:40:18 -0700

    can change unit prices

--- src/routes/units/size/[size]/+page.server.ts
+++ src/routes/units/size/[size]/+page.server.ts
@@ -1,8 +1,11 @@
 import { prisma } from '$lib/server/prisma';
+import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters';
+import { unitPricingFormSchema } from '$lib/formSchemas/schemas';
 
 export const load = (async (event) => {
-
+   const unitPricingForm = await superValidate(zod(unitPricingFormSchema));
    const size = event.params.size
    const units = await prisma.unit.findMany({
       where: {
@@ -20,5 +23,5 @@ export const load = (async (event) => {
       }
    })
    
-   return { units, size };
+   return { units, size, unitPricingForm };
 }) satisfies PageServerLoad;
--- src/routes/units/size/[size]/+page.svelte
+++ src/routes/units/size/[size]/+page.svelte
@@ -2,15 +2,34 @@
    import LeaseEmployee from '$lib/displayComponents/LeaseEmployee.svelte';
    import UnitEmployee from '$lib/displayComponents/UnitEmployee.svelte';
 	import Header from '$lib/Header.svelte';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
    import type { PageData } from './$types';
+	import UnitPricingForm from '$lib/forms/UnitPricingForm.svelte';
 
    let { data }: { data: PageData } = $props();
+   let unitPricingModalOpen = $state(false);
 </script>
 <Header title='All {data.size} units' />
+<Modal
+   bind:open={unitPricingModalOpen}
+   triggerBase="btn preset-tonal"
+   contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
+   backdropClasses=""
+>  
+{#snippet content()}
+   <UnitPricingForm data={data.unitPricingForm} bind:unitPricingFormModalOpen={unitPricingModalOpen} size={data.size}/>
+   <button class="btn" onclick={()=>unitPricingModalOpen = false}>Close</button>
+{/snippet}
+
+</Modal>
+
 {#each data.units as unit} 
    {@const {lease} = unit }
    <div class="flex">
+      <div>
       <UnitEmployee unit={unit}/>
+      <button class="btn " onclick={()=> unitPricingModalOpen=true}>Change all {data.size.replace(/^0+/gm,'').replace(/x0/gm,'x')} prices</button>
+   </div>
       {#each lease as l}
       <LeaseEmployee lease={l} />
       {/each}
