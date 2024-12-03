e8c002e19029f0d9523a46034dd237f95b5b4414
Eric Branson <eric.branson@gmail.com>
2024-12-03 08:45:34 -0700

    pulls 1 rolling year of payment records

--- src/routes/paymentRecords/+page.server.ts
+++ src/routes/paymentRecords/+page.server.ts
@@ -4,39 +4,23 @@ import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { searchFormSchema } from '$lib/formSchemas/schemas';
 import type { Actions } from '@sveltejs/kit';
+import dayjs from 'dayjs';
 
 export const load = (async (event) => {
-    const search = event.url.searchParams.get('search');
-    const paymentSearchForm = await superValidate(zod(searchFormSchema));
-    const customers = await prisma.user.findMany();
-    
-    if(search) {
-        const paymentRecords = await prisma.paymentRecord.findMany({
-            include: {
-                customer: true,
-            },
-            orderBy: {
-                paymentCompleted: 'desc'
-            },
-            where: {
-                OR:[
-                    { paymentNumber: {
-                        equals: parseInt(search, 10)
-                    }}
-                ]
-            }
-        })
-        return { paymentRecords, paymentSearchForm, customers }
-    }
     const paymentRecords = await prisma.paymentRecord.findMany({
         include: {
             customer: true
         },
         orderBy: {
             paymentCompleted: 'desc'
-        }
+        },
+        where: {
+            paymentCreated: {
+                gte: dayjs(Date.now()).subtract(1, 'year').toDate()
+            }
+        }, 
     });
-    return { paymentRecords, paymentSearchForm, customers };
+    return { paymentRecords, };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
