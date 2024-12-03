3ebf7063bad5a8067ae50d1e5920c4d98395761a
Eric Branson <eric.branson@gmail.com>
2024-12-03 08:42:28 -0700

    displays unpaid invoices

--- src/routes/invoices/+page.server.ts
+++ src/routes/invoices/+page.server.ts
@@ -1,5 +1,11 @@
+import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-    return {};
+    const unpaidInvoices = await prisma.invoice.findMany({
+        where: {
+            paymentRecord: null
+        }
+    })
+    return { unpaidInvoices };
 }) satisfies PageServerLoad;
--- src/routes/invoices/+page.svelte
+++ src/routes/invoices/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+    import Invoice from '$lib/displayComponents/Invoice.svelte';
+import type { PageData } from './$types';
 
     let { data }: { data: PageData } = $props();
 </script>
+
+{#each data.unpaidInvoices as invoice}
+    <div>
+        <Invoice invoice={invoice} />
+    </div>
+{/each}
