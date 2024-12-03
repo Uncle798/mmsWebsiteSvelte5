9a60cc502cc1a87bf24c0c9002ecb99ed925a18c
Eric Branson <eric.branson@gmail.com>
2024-12-03 08:37:28 -0700

    send magic link email

--- src/lib/server/mailtrap.ts
+++ src/lib/server/mailtrap.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_COMPANY_NAME, PUBLIC_URL } from "$env/static/public";
 import { MailtrapClient } from "mailtrap";
 
 const token = process.env.MAILTRAP_TOKEN!;
@@ -13,6 +14,23 @@ export async function sendVerificationEmail(verificationCode:string, email:strin
       to: [{email}],
       subject: "Please verify your email",
       html: `Verification code: ${verificationCode}`
+   }).catch((err) =>{
+      console.error(err);
+   })
+   return response;
+}
+
+export async function sendMagicLinkEmail(magicLink:string, email:string) {
+   const sender = {
+      name: 'computer@bransonschlegel.com',
+      email: 'computer@bransonschlegel.com',
+   }
+   const response = await mailtrap.send({
+      from:sender,
+      to: [{email}],
+      subject: `Login link from ${PUBLIC_COMPANY_NAME} `,
+      html: `Please click this link or paste it into your browser \
+      to log in: <a href="${PUBLIC_URL}/login/magicLink/${magicLink}">${PUBLIC_URL}/login/magicLink/${magicLink}</a>`
    }).catch((err) =>{
       console.error(err);
    })
