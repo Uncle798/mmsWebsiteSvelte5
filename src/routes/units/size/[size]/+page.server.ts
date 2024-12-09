e5f7514f030053ca4da0af3b8394fd3638a2fd38
Eric Branson <eric.branson@gmail.com>
2024-12-09 16:15:30 -0700

    returns all sizes for links

--- src/routes/units/size/[size]/+page.server.ts
+++ src/routes/units/size/[size]/+page.server.ts
@@ -3,10 +3,15 @@ import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { unitPricingFormSchema } from '$lib/formSchemas/schemas';
+import { redirect } from '@sveltejs/kit';
 
 export const load = (async (event) => {
+   if(!event.locals.user?.employee){
+      redirect(302, '/login?toast=employee')
+   }
    const unitPricingForm = await superValidate(zod(unitPricingFormSchema));
    const size = event.params.size
+   
    const units = await prisma.unit.findMany({
       where: {
          size
@@ -18,10 +23,30 @@ export const load = (async (event) => {
          lease:{
             where: {
                leaseEnded: null
+            },
+            include: {
+               customer: true,
             }
          }
       }
    })
-   
-   return { units, size, unitPricingForm };
+   const allUnits = await prisma.unit.findMany({
+      orderBy:{
+         size: 'asc'
+      },
+      where:{
+         size: {
+            not: 'ours'
+         }
+      }
+   });
+   const sizes:string[] = [];
+   allUnits.forEach((unit) =>{
+      const unitSize = unit.size;
+      const sizeSize = sizes.find((size) => size === unitSize);
+      if(!sizeSize){
+         sizes.push(unitSize);
+      }
+   })
+   return { units, size, unitPricingForm, sizes };
 }) satisfies PageServerLoad;
