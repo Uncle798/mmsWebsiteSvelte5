d77cd0fcf65f2bcdeeab5046ed23be2b00a53cc3
Eric Branson <eric.branson@gmail.com>
2024-12-12 17:17:04 -0700

    Hides payment completed if not completed

--- src/lib/displayComponents/PaymentRecord.svelte
+++ src/lib/displayComponents/PaymentRecord.svelte
@@ -12,7 +12,11 @@
 <div class="card p-4">
    <p>Payment Record Number: <a href="/paymentRecords/{paymentRecord.paymentNumber}">{paymentRecord.paymentNumber}</a></p>
    <p>${paymentRecord.paymentAmount}</p>
+   {#if paymentRecord.paymentCompleted}
    <p>Payment completed { dayjs(paymentRecord.paymentCompleted).format('M/D/YYYY')}</p>
+   {:else}
+      Payment not completed
+   {/if}
    {#if paymentRecord.paymentNotes}
    <p><a href='/invoices/{paymentRecord.paymentNotes.replace(/[^$0-9]/gm, '')}'>{paymentRecord.paymentNotes}</a></p>
    {/if}
