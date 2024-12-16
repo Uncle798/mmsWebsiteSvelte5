c5601545d55a3ce220c1bfd3d32e2bc559563bdf
Eric Branson <eric.branson@gmail.com>
2024-12-16 09:43:06 -0700

    removing base url

--- src/lib/server/qStash.ts
+++ src/lib/server/qStash.ts
@@ -1,7 +1,6 @@
-import { QSTASH_TOKEN, UPSTASH_WORKFLOW_URL } from "$env/static/private";
-import { Client } from "@upstash/qstash";
+import { QSTASH_TOKEN } from "$env/static/private";
+import { Client } from "@upstash/workflow";
 
-export const qstash = new Client({
+export const qStash = new Client({
    token: QSTASH_TOKEN, 
-   baseUrl: UPSTASH_WORKFLOW_URL
 })
