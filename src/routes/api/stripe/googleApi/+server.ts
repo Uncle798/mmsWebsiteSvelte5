b33b9249a6ee44c181a33ded302cb2de69ce0baf
Eric Branson <eric.branson@gmail.com>
2024-12-26 17:37:19 -0700

    just using event.local

--- src/routes/api/stripe/googleApi/+server.ts
+++ src/routes/api/stripe/googleApi/+server.ts
@@ -1,15 +1,9 @@
 import { GOOGLE_MAPS_KEY } from '$env/static/private';
-import { prisma } from '$lib/server/prisma';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async (event) => {
-   const body = await event.request.json();
-   const { userId } = body
-   const user = await prisma.user.findUnique({
-      where: {
-         id: userId,
-      }
-   })
+
+   const user = event.locals.user
    if(!user){
       return new Response(JSON.stringify('User not specified'), { status:400 })
    }
