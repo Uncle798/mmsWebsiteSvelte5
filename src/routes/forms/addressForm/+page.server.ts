4e1bac1bf499d1fdca0d2cc4962e89183d4a34b3
Eric Branson <eric.branson@gmail.com>
2024-12-26 17:38:03 -0700

    towards stripe address

--- src/routes/forms/addressForm/+page.server.ts
+++ src/routes/forms/addressForm/+page.server.ts
@@ -5,7 +5,6 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { addressFormSchema } from '$lib/formSchemas/schemas';
 import { prisma } from '$lib/server/prisma';
 import { fail, redirect } from '@sveltejs/kit';
-import { RADAR_SECRET_LIVE } from '$env/static/private';
 
 export const load:PageServerLoad = (async () => {
    const addressForm = await superValidate(zod(addressFormSchema));
@@ -23,6 +22,7 @@ export const actions: Actions = {
          return fail(400, {message: 'User not specified'})
       }
       const formData = await event.request.formData();
+      console.log(formData);
       const addressForm = await superValidate(formData, zod(addressFormSchema));
       const { success, reset } = await ratelimit.customerForm.limit(event.locals.user.id)
 		if(!success) {
@@ -50,27 +50,27 @@ export const actions: Actions = {
             }
          })
       }
-      const response = await fetch(`https://api.radar.io/v1/addresses/validate?city=${addressForm.data.city}&stateCode=${addressForm.data.state}&postalCode=${addressForm.data.postalCode}&addressLabel=${addressForm.data.address1}&unit=${addressForm.data.address2}&countryCode=${addressForm.data.country}`,
-         {
-            method: 'GET',
-            headers: {
-               Authorization: RADAR_SECRET_LIVE
-            }
-         }
-      )
-      const data = await response.json();
-      if(data.result.verificationStatus === 'verified'){
-         const newAddress = {
-            userId,
-            ...addressForm.data
-         }
-         await prisma.address.create({
-            data: newAddress
-         })
-      }
-      if(data.result.verificationStatus === 'unverified'){
-         return message(addressForm, 'Unable to verify address, please contact the office');
-      }
-      return message(addressForm, 'Address updated')
+      // const response = await fetch(`https://api.radar.io/v1/addresses/validate?city=${addressForm.data.city}&stateCode=${addressForm.data.state}&postalCode=${addressForm.data.postalCode}&addressLabel=${addressForm.data.address1}&unit=${addressForm.data.address2}&countryCode=${addressForm.data.country}`,
+      //    {
+      //       method: 'GET',
+      //       headers: {
+      //          Authorization: RADAR_SECRET_LIVE
+      //       }
+      //    }
+      // )
+      // const data = await response.json();
+      // if(data.result.verificationStatus === 'verified'){
+      //    const newAddress = {
+      //       userId,
+      //       ...addressForm.data
+      //    }
+      //    await prisma.address.create({
+      //       data: newAddress
+      //    })
+      // }
+      // if(data.result.verificationStatus === 'unverified'){
+      //    return message(addressForm, 'Unable to verify address, please contact the office');
+      // }
+      return {addressForm}
    }
 };
