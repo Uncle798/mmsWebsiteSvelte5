2364caac542d6aa3aff588abb7d0354524bc0c52
Eric Branson <eric.branson@gmail.com>
2025-01-16 09:44:58 -0700

    init

---
+++ src/lib/formComponents/TextArea.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { FullAutoFill } from 'svelte/elements';
+   import type { InputConstraint } from 'sveltekit-superforms';
+   
+   interface Props {
+      value:string | undefined | null;
+      label: string | undefined;
+      errors: string[] | undefined;
+      constraints: InputConstraint | undefined;
+      placeholder?:string | undefined;
+      name: string | null | undefined;
+      autocomplete?: FullAutoFill;
+   }
+   let { value = $bindable(), label, errors, constraints, placeholder, name, autocomplete,...others }:Props = $props()
+</script>
+<div class="mx-4">
+<label class="label">
+   {#if label}<span class="label-text">{label}</span><br />{/if}
+   <textarea
+      class="input"
+      name={name}
+      bind:value={value}
+      aria-invalid={errors ? 'true' : undefined}
+      placeholder={placeholder}
+      autocomplete={autocomplete}
+      {...constraints}
+      {...others}
+   >
+   {value}
+   </textarea>
+ </label>
+ {#if errors}<span class="invalid">{errors}</span>{/if}
+</div>
