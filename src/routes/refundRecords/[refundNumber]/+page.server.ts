836c45b4dc2f3999c113a9ec5512163780d39d3f
Eric Branson <eric.branson@gmail.com>
2025-01-16 10:53:09 -0700

    returns refund record

--- src/routes/refundRecords/[refundNumber]/+page.server.ts
+++ src/routes/refundRecords/[refundNumber]/+page.server.ts
@@ -1,5 +1,19 @@
+import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async () => {
-    return {};
+export const load = (async (event) => {
+   if(!event.locals.user?.employee){
+      redirect(302, '/login?toast=employee')
+   }
+   const refundRecordNum = event.params.refundNumber;
+   const refundRecord = await prisma.refundRecord.findUnique({
+      where: { 
+         refundNumber: parseInt(refundRecordNum, 10)
+      },
+      include: {
+         customer: true
+      },
+   })
+   return { refundRecord, };
 }) satisfies PageServerLoad;
