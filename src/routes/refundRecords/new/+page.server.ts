66e1fbfbc049d26989af5463f855ebf3081a03f8
Eric Branson <eric.branson@gmail.com>
2025-01-18 11:25:31 -0700

    towards a new refund workflow

--- src/routes/refundRecords/new/+page.server.ts
+++ src/routes/refundRecords/new/+page.server.ts
@@ -1,15 +1,72 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import { redirect } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma'
+import { fail, message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { refundFormSchema } from '$lib/formSchemas/schemas';
+import { z } from 'zod';
+import { ratelimit } from '$lib/server/rateLimit';
+
+const formSchema = z.object({
+   search: z.string().min(0).max(255)
+})
 
 export const load = (async (event) => {
    if(!event.locals.user?.employee){
       redirect(302, '/login?toast=employee')
    }
-   const paymentRecords = await prisma.paymentRecord.findMany({
-      where: {
-         refunded: false
+   const refundForm = await superValidate(zod(refundFormSchema));
+   const searchForm = await superValidate(zod(formSchema));
+   const paymentNum = event.url.searchParams.get('paymentNum');
+   if(paymentNum){
+      const paymentRecord = await prisma.paymentRecord.findUnique({
+         where:{
+            paymentNumber: parseInt(paymentNum, 10)
+         }
+      })
+      if(!paymentNum){
+         return fail(404, searchForm);
       }
-   })
-   return { paymentRecords };
+      return { paymentRecord, refundForm, searchForm}
+   }
+   const deposits = await prisma.paymentRecord.findMany({
+      where: {
+         AND:[
+            {
+               refunded: false
+            },
+            {
+               deposit: true
+            }
+         ]
+      },
+      orderBy: {
+         paymentCreated: 'desc'
+      }
+   });
+
+   return { deposits, refundForm, searchForm };
 }) satisfies PageServerLoad;
+
+
+
+export const actions: Actions = {
+   default: async (event) => {
+      if(!event.locals.user?.employee){
+         redirect(302, '/login?toast=employee')
+      }
+      const formData = await event.request.formData();
+      console.log('refundRecords/new formData: ', formData);
+      const form = await superValidate(formData, zod(formSchema));
+      const { success, reset } = await ratelimit.employeeForm.limit(event.locals.user.id);
+      if(!success){
+         const timeRemaining = Math.floor((reset - Date.now()) / 1000);
+         return message(form, `Please wait ${timeRemaining} seconds before trying again`);
+      }
+      if(!form.valid){
+         return message(form, 'Unable to process');
+      }
+      const paymentNum = form.data.search
+      redirect(302,`/refundRecords/new?paymentNum=${paymentNum}`)
+   }
+};
--- src/routes/refundRecords/new/+page.svelte
+++ src/routes/refundRecords/new/+page.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
     import PaymentRecord from '$lib/displayComponents/PaymentRecord.svelte';
-import type { PageData } from './$types';
-
+	import { Combobox } from '@skeletonlabs/skeleton-svelte';
+    import type { PageData } from './$types';
+	import NewRefundForm from '$lib/forms/NewRefundForm.svelte';
+    import { superForm } from 'sveltekit-superforms';
     let { data }: { data: PageData } = $props();
+    let selectedPayment = $state(['']);
+    let paymentNumber = $state(0)
+    interface ComboboxData {
+        label: string;
+        value: string;
+    }
+    const paymentRecordComboboxData:ComboboxData[] = [];
+    data.deposits?.forEach((paymentRecord) => {
+        const label = paymentRecord.paymentNotes ? paymentRecord.paymentNotes : '';
+        const value = paymentRecord.paymentNumber.toString();
+        paymentRecordComboboxData.push({label, value})
+    })
+    let { form, errors, message, constraints, enhance, delayed, timeout} = superForm(data.searchForm, {
+      onSubmit({formData}) {
+
+      },
+   });
 </script>
 
-{#each data.paymentRecords as paymentRecord}
-    <PaymentRecord paymentRecord={paymentRecord}/>
-{/each}
+{#if data.deposits}    
+<form method="POST" use:enhance>
+    <Combobox 
+        data={paymentRecordComboboxData}
+        bind:value={selectedPayment}
+        placeholder="Select deposit to refund"
+        openOnClick={true}
+        onValueChange={(details) =>{
+            paymentNumber = parseInt(details.value[0], 10)
+        }}
+    />
+</form>
+{/if}
+
+<NewRefundForm data={data.refundForm} paymentRecordNumber={paymentNumber}/>
