15bcf159eae494f26985c328ded7a04ecc65c1aa
Eric Branson <eric.branson@gmail.com>
2025-01-25 12:12:26 -0700

    returns customers

--- src/routes/paymentRecords/+page.server.ts
+++ src/routes/paymentRecords/+page.server.ts
@@ -3,7 +3,6 @@ import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { searchFormSchema } from '$lib/formSchemas/schemas';
-import type { Actions } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 import { arrayOfYears } from '$lib/server/utils';
 
@@ -23,20 +22,7 @@ export const load = (async (event) => {
         orderBy: {
             paymentNumber: 'desc'
         },
-        include: {
-            customer: true
-        }
     });
-    return { paymentRecords, searchForm, paymentCount, years };
+    const customers = prisma.user.findMany()
+    return { paymentRecords, searchForm, paymentCount, years, customers };
 }) satisfies PageServerLoad;
-
-export const actions: Actions = {
-    default: async (event) => {
-        if(!event.locals.user?.employee){
-            redirect(302, '/login?toast=employee');
-        }
-        const formData = await event.request.formData();
-        const searchForm = await superValidate(formData, zod(searchFormSchema));
-        return {searchForm}
-    }
- };
