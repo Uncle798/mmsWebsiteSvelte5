647c4984a429afa0f52343e896806310260c626d
Eric Branson <eric.branson@gmail.com>
2025-01-26 10:47:19 -0700

    remove extra form instantiation

--- src/routes/paymentRecords/+page.svelte
+++ src/routes/paymentRecords/+page.svelte
@@ -7,26 +7,10 @@
 	import Pagination from '$lib/displayComponents/Pagination.svelte';
 	import User from '$lib/displayComponents/User.svelte';
 	import { fade } from 'svelte/transition';
-	import FormMessage from '$lib/formComponents/FormMessage.svelte';
 	import Placeholder from '$lib/displayComponents/Placeholder.svelte';
 	import Search from '$lib/forms/Search.svelte';
 
     let { data }: { data: PageData } = $props();
-    let { form, enhance, message } = superForm(data.searchForm,{
-        onChange(event){
-            const inputText = event.get('search')
-            if(inputText){
-                search=inputText!;
-            }
-        },
-        onSubmit(input) {
-            input.cancel();
-            const inputText = input.formData.get('search')?.toString();
-            if(search){
-                search=inputText!;
-            }
-        },
-    });
     let pageNum = $state(1);
     let size = $state(25);
     let search = $state('')
@@ -49,7 +33,6 @@
         loading customers
     {:then customers} 
         <div transition:fade={{duration:600}}>
-            <FormMessage message={$message} />
             <Search search={search} searchType='payment records' data={data.searchForm}/>
             {#each slicedSource(searchedPaymentRecords(paymentRecords)) as paymentRecord}
             {@const customer = customers.find((customer) => customer.id === paymentRecord.customerId) }
