4ff472b4a2ee322e3239e3d37d959ce450718f5d
Eric Branson <eric.branson@gmail.com>
2025-01-26 10:47:39 -0700

    returns searchForm

--- src/routes/paymentRecords/deposits/+page.server.ts
+++ src/routes/paymentRecords/deposits/+page.server.ts
@@ -3,14 +3,14 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { refundFormSchema } from '$lib/formSchemas/schemas';
-
+import { refundFormSchema, searchFormSchema } from '$lib/formSchemas/schemas';
 export const load = (async (event) => {
    if(!event.locals.user?.employee){
       redirect(302, '/login?toast=employee')
    }
    const refundForm = await superValidate(zod(refundFormSchema));
-   const deposits = await prisma.paymentRecord.findMany({
+   const searchForm = await superValidate(zod(searchFormSchema));
+   const deposits = prisma.paymentRecord.findMany({
       where: {
          AND: [
             { 
@@ -25,5 +25,6 @@ export const load = (async (event) => {
          paymentCreated: 'desc'
       }
    })
-   return { deposits, refundForm };
+   
+   return { deposits, refundForm, searchForm,  };
 }) satisfies PageServerLoad;
