4d81ef94c72ccd4f64245846d5bcdf41e33b0c48
Eric Branson <eric.branson@gmail.com>
2025-01-28 12:16:31 -0700

    deposit count Revenue added

--- src/routes/paymentRecords/deposits/+page.svelte
+++ src/routes/paymentRecords/deposits/+page.svelte
@@ -7,6 +7,7 @@
 	import RefundForm from '$lib/forms/NewRefundForm.svelte';
 	import Search from '$lib/forms/Search.svelte';
 	import Pagination from '$lib/displayComponents/Pagination.svelte';
+	import Revenue from '$lib/displayComponents/Revenue.svelte';
     interface Props {
         data: PageData;
     }
@@ -17,6 +18,7 @@
     let pageNum = $state(1);
     let size = $state(25);
     let search = $state('');
+    const numberFormatter = new Intl.NumberFormat('en-US');
     let slicedSource = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.slice((pageNum -1) * size, pageNum*size));
     let searchedPaymentRecords = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.filter((paymentRecord) => paymentRecord.paymentNumber.toString().includes(search) )) 
     let paymentRecord=$state<PaymentRecord>({} as PaymentRecord);
@@ -24,6 +26,15 @@
         paymentRecord = deposit;
         refundModalOpen = true;
     }
+    let totalRevenue = $derived((paymentRecords:PaymentRecord[]) => {
+        let totalRevenue = 0;
+        paymentRecords.forEach((paymentRecord) => {
+            if(paymentRecord.paymentCompleted && !paymentRecord.refunded){
+                totalRevenue += paymentRecord.paymentAmount
+            }
+        })
+        return totalRevenue;
+    })
 </script>
 <Header title='Deposits' />
 <Modal
@@ -39,9 +50,10 @@
 </Modal>
 
 {#await data.deposits}
-    loading deposits
+    loading {numberFormatter.format(data.depositCount)} deposits
 {:then deposits} 
-    <Search search={search} searchType='invoice number' data={data.searchForm} />
+    <Revenue amount={totalRevenue(searchedPaymentRecords(deposits))} label='Amount of deposits: ' />
+    <Search bind:search={search} searchType='payment record number' data={data.searchForm} />
     {#each slicedSource(searchedPaymentRecords(deposits)) as deposit}
         <PaymentRecordEmployee paymentRecord={deposit} />
         <button type="button" class="btn" onclick={() => refundModal(deposit)}>Refund this deposit</button>
