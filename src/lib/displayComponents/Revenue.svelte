873d7314714c472690e79aaa123d4b213d91fcb5
Eric Branson <eric.branson@gmail.com>
2025-01-28 11:51:57 -0700

    init

---
+++ src/lib/displayComponents/Revenue.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+   interface Props {
+      amount: number
+      label: string
+   }
+   let { amount, label }:Props = $props()
+   const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
+</script>
+
+<div>
+   {label}: {currencyFormatter.format(amount)}
+</div>
