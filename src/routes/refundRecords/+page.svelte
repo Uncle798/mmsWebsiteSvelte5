da24f0b8e556e3e86dbc202908cda1898835bcc0
Eric Branson <eric.branson@gmail.com>
2025-01-28 13:23:00 -0700

    date search and Revenue

--- src/routes/refundRecords/+page.svelte
+++ src/routes/refundRecords/+page.svelte
@@ -1,24 +1,50 @@
 <script lang="ts">
-   import RefundRecordDisplay from '$lib/displayComponents/RefundRecordDisplay.svelte';
+	import RefundRecordDisplay from '$lib/displayComponents/RefundRecordDisplay.svelte';
 	import User from '$lib/displayComponents/User.svelte';
-	import Header from '$lib/Header.svelte';
-   import type { PageData } from './$types';
-   import type { RefundRecord } from '@prisma/client';
-   import { superForm } from 'sveltekit-superforms';
-   import Pagination from '$lib/displayComponents/Pagination.svelte';
-	import Placeholder from '$lib/displayComponents/Placeholder.svelte';
 	import Search from '$lib/forms/Search.svelte';
-
-   let { data }: { data: PageData } = $props();
-   let pageNum = $state(1);
-   let size = $state(25);
-   let search = $state('');
-   const numberFormatter = new Intl.NumberFormat('en-US');
-   let slicedRefunds = $derived((refunds:RefundRecord[]) => refunds.slice((pageNum-1)*size, pageNum*size))
-   let searchRefunds = $derived((refunds:RefundRecord[]) => refunds.filter((refund) => refund.refundNumber.toString().includes(search)))
+   import dayjs from 'dayjs';
+   import type { PageData } from './$types';
+   import type { RefundRecord } from '@prisma/client'
+	import DateSearch from '$lib/forms/DateSearch.svelte';
+	import Pagination from '$lib/displayComponents/Pagination.svelte';
+	import Revenue from '$lib/displayComponents/Revenue.svelte';
+   import Header from '$lib/Header.svelte'; 
+   import Placeholder from '$lib/displayComponents/Placeholder.svelte';
+    let { data }: { data: PageData } = $props();
+    let size = $state(25)
+    let pageNum = $state(1)
+    let search = $state('');
+    let startDate = $state<Date>(new Date());
+    let endDate = $state<Date>(new Date());
+    let maxDate = $state<Date>();
+    let minDate = $state<Date>();
+    let wrapper = new Promise<RefundRecord[]>(async res => {
+        const refunds = await data.refunds
+        startDate = dayjs(refunds[0].refundCreated).startOf('year').toDate()
+        minDate = startDate;
+        maxDate = new Date();
+        endDate = maxDate;
+        res(refunds)
+    })
+    const numberFormatter = new Intl.NumberFormat('en-US');
+    let slicedRefunds = $derived((refunds:RefundRecord[]) => refunds.slice((pageNum-1)*size, pageNum*size))
+    let searchRefunds = $derived((refunds:RefundRecord[]) => refunds.filter((refund) => refund.refundNumber.toString().includes(search)))
+    let dateSearchRefunds = $derived((refunds:RefundRecord[]) => refunds.filter((refund) => {
+        if(!startDate || !endDate){
+            return refund
+        }
+        return refund.refundCreated >= startDate && refund.refundCreated <= endDate
+    }))
+    let totalRevenue = $derived((refunds:RefundRecord[]) => {
+        let totalRevenue:number = 0;
+        refunds.forEach((refund) =>{
+            totalRevenue += refund.refundAmount
+        })
+        return totalRevenue
+    })
 </script>
 <Header title='Refunds' />
-{#await data.refunds}
+{#await wrapper}
    loading {numberFormatter.format(data.refundCount)} refunds 
    {#if data.years}
       or select year: 
@@ -31,8 +57,12 @@
    {#await data.customers}
       loading customers
    {:then customers} 
-      <Search bind:search={search} searchType='Refund records' data={data.searchForm}/>
-      {#each slicedRefunds(searchRefunds(refunds)) as refund (refund.refundNumber)}
+      <Revenue label='Total refunds' amount={totalRevenue(searchRefunds(dateSearchRefunds(refunds)))}/>
+      <div class="flex">
+         <Search bind:search={search} searchType='Refund records' data={data.searchForm}/>
+         <DateSearch bind:endDate={endDate} bind:startDate={startDate} data={data.dateSearchForm} {minDate} {maxDate}/>
+      </div>
+      {#each slicedRefunds(searchRefunds(dateSearchRefunds(refunds))) as refund (refund.refundNumber)}
       {@const customer = customers.find((customer) => customer.id === refund.customerId)}
          <div class="flex">
             <RefundRecordDisplay refundRecord={refund} />
@@ -41,6 +71,6 @@
             {/if}
          </div>
       {/each}
-      <Pagination bind:size={size} bind:pageNum={pageNum} label='refund records' array={searchRefunds(refunds)}/> 
+      <Pagination bind:size={size} bind:pageNum={pageNum} label='refund records' array={searchRefunds(dateSearchRefunds(refunds))}/> 
    {/await}
 {/await}
