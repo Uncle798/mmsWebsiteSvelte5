e9a8af154df879c3c0f8b82019394ad6bae5ec7e
Eric Branson <eric.branson@gmail.com>
2025-01-30 13:04:47 -0700

    added horizontal divider to bottom added margin to left

--- src/lib/displayComponents/Revenue.svelte
+++ src/lib/displayComponents/Revenue.svelte
@@ -1,4 +1,6 @@
 <script lang='ts'>
+	import HorizontalDivider from "./HorizontalDivider.svelte";
+
    interface Props {
       amount: number
       label: string
@@ -6,7 +8,7 @@
    let { amount, label }:Props = $props()
    const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
 </script>
-
-<div>
+<div class="ml-4">
    {label}: {currencyFormatter.format(amount)}
 </div>
+<HorizontalDivider />
