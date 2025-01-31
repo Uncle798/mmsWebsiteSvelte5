07376c41dd8c368bc1c43dc395a7f835aa6515a6
Eric Branson <eric.branson@gmail.com>
2025-01-31 15:42:20 -0700

    w-8 m-0 p-0

--- src/lib/displayComponents/VerticalDivider.svelte
+++ src/lib/displayComponents/VerticalDivider.svelte
@@ -5,6 +5,6 @@
    let { heightClass,  }:Props = $props()
 </script>
 
-<div class="grid {heightClass} grid-cols-[1fr_auto_auto_auto_auto_1fr] items-center">
+<div class="grid container {heightClass} grid-cols-[1fr_auto_auto_auto_auto_1fr] items-center w-8 m-0 p-0">
    <span class="vr border-l-2"></span>
 </div>
