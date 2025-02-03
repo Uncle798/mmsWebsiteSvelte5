0163cf6ad325d864fb003a951a27021f2fb06bb5
Eric Branson <eric.branson@gmail.com>
2025-02-03 09:31:56 -0700

    styling button

--- src/lib/formComponents/FormSubmitWithProgress.svelte
+++ src/lib/formComponents/FormSubmitWithProgress.svelte
@@ -8,7 +8,7 @@
    let { delayed = $bindable(), timeout=$bindable(), buttonText=$bindable('Submit') }:Props = $props();
 </script>
 <div class="flex">
-   <button class="btn">{buttonText}</button>
+   <button class="btn bg-primary-950 m-4 rounded-lg hover:bg-primary-900 transition hover:text-white">{buttonText}</button>
    {#if delayed && !timeout}
       <ProgressRing value={null} size="size-8" strokeWidth="6px" meterStroke="stroke-secondary-600-400" trackStroke="stroke-secondary-50-950" />
    {/if}
