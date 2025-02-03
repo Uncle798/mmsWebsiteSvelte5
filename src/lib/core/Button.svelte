2b25ffa11657863a7ea05247a704d22803997ab7
Eric Branson <eric.branson@gmail.com>
2025-02-03 15:23:39 -0700

    init

---
+++ src/lib/core/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+   interface Props {
+      label: string;
+      onClick?: ()=>void;
+      classes?: string;
+      type: 'button' | 'submit' | 'reset' | null | undefined
+   }
+   let { label, onClick, classes, type }:Props = $props();
+
+</script>
+
+<button class="btn {classes}" {type}>{label}</button>
