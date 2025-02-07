d7cc68991d948fb052197a9b37fb4e61e94dabe8
Eric Branson <eric.branson@gmail.com>
2025-02-07 12:29:42 -0700

    numCols numRows heightClass

--- src/lib/displayComponents/Placeholder.svelte
+++ src/lib/displayComponents/Placeholder.svelte
@@ -1,11 +1,23 @@
+<script lang="ts">
+
+    interface Props {
+        numCols: number;
+        numRows: number;
+        heightClass: string
+    }
+    let {
+        numCols,
+        numRows,
+        heightClass
+    }:Props = $props()
+</script>
+
 <div class="w-full space-y-4">
    <div class="space-y-4">
-       <div class="placeholder animate-pulse p-4"></div>
-       <div class="grid grid-cols-2 gap-4 p-4">
-       <div class="placeholder animate-pulse"></div>
-       <div class="placeholder animate-pulse"></div>
-       <div class="placeholder animate-pulse"></div>
-       <div class="placeholder animate-pulse"></div>
+       <div class="grid grid-cols-{numCols.toString()} gap-x-1 gap-y-3 p-2">
+        {#each {length:numRows*numCols} as row }
+            <div class="placeholder animate-pulse {heightClass}"></div>
+        {/each}
        </div>
    </div>
 </div>
