9464e5eec89273621a10dd29c14a4cacb15e6285
Eric Branson <eric.branson@gmail.com>
2025-02-06 18:57:36 -0700

    search by notes styling

--- src/routes/paymentRecords/deposits/+page.svelte
+++ src/routes/paymentRecords/deposits/+page.svelte
@@ -19,9 +19,11 @@
     let pageNum = $state(1);
     let size = $state(25);
     let search = $state('');
+    let noteSearch = $state('');
     const numberFormatter = new Intl.NumberFormat('en-US');
     let slicedSource = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.slice((pageNum -1) * size, pageNum*size));
     let searchedPaymentRecords = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.filter((paymentRecord) => paymentRecord.paymentNumber.toString().includes(search) )) 
+    const searchByNotes = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.filter((paymentRecord) => paymentRecord.paymentNotes?.includes(noteSearch)));
     let paymentRecord=$state<PaymentRecord>({} as PaymentRecord);
     function refundModal(deposit:PaymentRecord) {
         paymentRecord = deposit;
@@ -53,15 +55,19 @@
 {#await data.deposits}
     loading {numberFormatter.format(data.depositCount)} deposits
 {:then deposits} 
+    <div class="flex">
+        <Search bind:search={search} searchType='payment record number' data={data.searchForm} classes='w-1/2'/>
+        <Search bind:search={noteSearch} searchType='Payment notes' data={data.searchForm} classes='w-1/2'/>
+
+    </div>
+    <HorizontalDivider />
     <Revenue amount={totalRevenue(searchedPaymentRecords(deposits))} label='Amount of deposits: ' />
     <HorizontalDivider />
-    <Search bind:search={search} searchType='payment record number' data={data.searchForm} />
-    <HorizontalDivider />
     <div class="flex flex-col">
-        {#each slicedSource(searchedPaymentRecords(deposits)) as deposit}
-        <div class="flex flex-col border-y-2 justify-self-start">
+        {#each slicedSource(searchedPaymentRecords(searchByNotes(deposits))) as deposit}
+        <div class="flex flex-col border-y-2 dark:border-primary-950 border-primary-50">
             <PaymentRecordEmployee paymentRecord={deposit} classes='px-2'/>
-            <button type="button" class="btn" onclick={() => refundModal(deposit)}>Refund this deposit</button>
+            <button type="button" class="btn rounded-lg preset-filled-primary-50-950 m-2" onclick={() => refundModal(deposit)}>Refund this deposit</button>
         </div>
         {/each}
         <Pagination pageNum={pageNum} size={size} array={searchedPaymentRecords(deposits)} label='invoices'/>
