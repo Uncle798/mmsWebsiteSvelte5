54a6ee5ee18ca3435f069217bdf46dfa4b3ffc75
Eric Branson <eric.branson@gmail.com>
2025-02-07 10:45:24 -0700

    moved button by 1

--- src/lib/forms/Search.svelte
+++ src/lib/forms/Search.svelte
@@ -30,7 +30,7 @@
 				bind:value={search}
 			/>
 			<button
-				class="btn rounded-lg preset-filled-primary-50-950"
+				class="btn rounded-lg preset-filled-primary-50-950 m-1"
 				type="button"
 				onclick={() => {
 					$form.search = '';
