f2e7cac31866d26c5784b66caaaf18440fd513f9
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:17:50 -0700

    only displays if array is larger than size

--- src/lib/displayComponents/Pagination.svelte
+++ src/lib/displayComponents/Pagination.svelte
@@ -17,11 +17,13 @@
 </script>
 
 <footer class="flex w-full m-2">
-   <select name="size" id="size" class="select w-1/2" bind:value={size}>
-       {#each [5,10,25,50] as v}
-       <option value={v}>Show {v} {label} per page</option>
-       {/each}
-       <option value={array.length}>Show all {array.length} {label}</option>
-   </select>
+   {#if array.length > size}  
+      <select name="size" id="size" class="select w-1/2" bind:value={size}>
+         {#each [5,10,25,50] as v}
+         <option value={v}>Show {v} {label} per page</option>
+         {/each}
+         <option value={array.length}>Show all {array.length} {label}</option>
+      </select>
+   {/if}
    <Pagination data={array} bind:page={pageNum} bind:pageSize={size} classes=''/>
 </footer>
