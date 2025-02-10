11adf473ea186d023b9129f07b5c7278bf0887be
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:22:37 -0700

    towards displaying payment receipt

--- src/routes/newLease/leaseSent/+page.svelte
+++ src/routes/newLease/leaseSent/+page.svelte
@@ -4,7 +4,13 @@
 
     let { data }: { data: PageData } = $props();
 	import { PUBLIC_ANVIL_EMAIL, PUBLIC_COMPANY_NAME } from '$env/static/public';
+	import PaymentRecordCustomer from '$lib/displayComponents/CustomerViews/PaymentRecordCustomer.svelte';
 </script>
 <Header title='Lease Sent' />
-<span>Your lease has been sent to {data.customer?.email} from {PUBLIC_ANVIL_EMAIL}</span>
-<span>Leases are valid when both parties have signed. {PUBLIC_COMPANY_NAME} will sign your lease during normal business hours.</span>
+<div class="mx-2">
+    <span>Your lease has been sent to {data.customer?.email} from {PUBLIC_ANVIL_EMAIL}</span>
+    <span>Leases are valid when both parties have signed. {PUBLIC_COMPANY_NAME} will sign your lease during normal business hours.</span>
+</div>
+{#if data.paymentRecord}
+    <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
+{/if}
