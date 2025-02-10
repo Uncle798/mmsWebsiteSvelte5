5d2f7856e0bcec2638ed40149215f2fbd3819a56
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:21:24 -0700

    cleanup

--- src/lib/displayComponents/UnitEmployee.svelte
+++ src/lib/displayComponents/UnitEmployee.svelte
@@ -11,7 +11,7 @@
 
 <div class="grid grid-cols-2 gap-x-2 {classes}">
    <div class="col-span-2 text-center text-sm font-semibold -m-1 -mt-2">Unit</div>
-   <div class="text-right">Unit Number</div>
+   <div class="text-right">Number</div>
    <div class="font-medium">{unit.num.replace(/^0+/gm,'')}</div>
    <HorizontalDivider classes='col-span-2'/>
    <div class="text-right">Size</div>
