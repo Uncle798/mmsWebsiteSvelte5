f27479e5cd6d17eef8127ca587d062b3d0de1305
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:16:39 -0700

    made employee made /customerViews

--- src/lib/displayComponents/CustomerViews/RefundRecordCustomer.svelte
+++ src/lib/displayComponents/CustomerViews/RefundRecordCustomer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { RefundRecord } from '@prisma/client';
 	import dayjs from 'dayjs';
-	import HorizontalDivider from './HorizontalDivider.svelte';
+	import HorizontalDivider from '../HorizontalDivider.svelte';
 
 	interface Props {
 		refundRecord: RefundRecord;
@@ -12,23 +12,24 @@
 </script>
 
 <div class="grid grid-cols-2 gap-x-2 {classes}">
-	<div class="text-right">Refund record number</div>
+	<div class="col-span-2 text-center text-sm font-semibold">Refund</div>
+	<div class="text-right">Number</div>
 	<div class="font-medium"><a href="/refundRecords/{refundRecord.refundNumber}">{refundRecord.refundNumber}</a></div>
 	<HorizontalDivider classes='col-span-2' />
-	<div class="text-right">Refund amount</div>
+	<div class="text-right">Amount</div>
 	<div class="font-medium">{currencyFormatter.format(refundRecord.refundAmount)}</div>
 	<HorizontalDivider classes='col-span-2' />
 	{#if refundRecord.refundCompleted}
-		<div class="text-right">Refund completed</div>
+		<div class="text-right">Completed</div>
 		<div class="font-medium">{dayjs(refundRecord.refundCompleted).format('M/D/YYYY')}</div>
 	{:else}
 		<div class="col-span-2 text-center font-medium">Refund not completed</div>
 	{/if}
 	<HorizontalDivider classes='col-span-2' />
-	<div class="text-right">Refund type</div>
+	<div class="text-right">Type</div>
 	<div class="font-medium">{refundRecord.refundType.replace(/\w\S*/g,(text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase())}</div>
 	<HorizontalDivider classes='col-span-2' />
-	<div class="text-right">Refund notes</div>
+	<div class="text-right">Notes</div>
 	{#if refundRecord.paymentRecordNum}
 		<div class="text-left font-medium"><a href="/paymentRecords/{refundRecord.paymentRecordNum}">{refundRecord.refundNotes}</a></div>
 	{:else}
---
+++ src/lib/displayComponents/CustomerViews/UnitCustomer.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+   import type { Unit } from "@prisma/client";
+   import HorizontalDivider from "../HorizontalDivider.svelte";
+   interface Props {
+      unit: Unit
+      classes?: string;
+   }
+   let { unit, classes }:Props = $props();
+   const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
+</script>
+<div class="grid grid-cols-2 gap-x-2 {classes}">
+   <div class="col-span-2 text-center text-sm font-semibold -m-1 -mt-2">Unit</div>
+   <div class="text-right">Number</div>
+   <div class="font-medium">{unit.num.replace(/^0+/gm,'')}</div>
+   <HorizontalDivider classes='col-span-2'/>
+   <div class="text-right">Size</div>
+   <div class="font-medium"> <a href="/units/size/{unit.size}">{unit.size.replace(/^0+/gm, '').replace(/x0/gm, 'x')}</a></div>
+   <HorizontalDivider classes='col-span-2'/>
+   <div class="text-right">Price</div>
+   <div class="font-medium">{currencyFormatter.format(unit.advertisedPrice)}</div>
+   <div class="text-right">Deposit</div>
+   <div>{currencyFormatter.format(unit.deposit)}</div>
+   <HorizontalDivider classes='col-span-2'/>
+   {#if unit.leasedPrice}
+      <div class="text-right">Leased Price</div>
+      <div class="font-medium">{currencyFormatter.format(unit.leasedPrice)}</div>
+      <HorizontalDivider classes='col-span-2'/>
+   {/if}
+   {#if unit.notes}
+      <div class="text-right">Notes:</div>
+      <div class="font-medium"> {unit.notes}</div>
+      <HorizontalDivider classes='col-span-2'/>
+   {/if}
+</div>
