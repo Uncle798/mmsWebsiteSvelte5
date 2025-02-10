622ceff6ee8768311f2f36090a75f1b1d5cededb
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:23:27 -0700

    send payment receipt via email returns for customer vs employee

--- src/routes/paymentRecords/[paymentRecordNum]/+page.server.ts
+++ src/routes/paymentRecords/[paymentRecordNum]/+page.server.ts
@@ -1,50 +1,125 @@
 import { prisma } from '$lib/server/prisma';
 import { fail, redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import type { RefundRecord } from '@prisma/client';
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { paymentRecordDeleteSchema } from '$lib/formSchemas/schemas';
+import { sendPaymentReceipt } from '$lib/server/mailtrap';
 
 export const load = (async (event) => {
-   if(!event.locals.user?.employee){
-      redirect(302, '/login?toast=employee')
+   if(!event.locals.user){
+      redirect(302, '/login?toast=unauthorized')
    }
    const paymentRecordNum = event.params.paymentRecordNum;
-   console.log(paymentRecordNum)
    if(!paymentRecordNum){
       return fail(404)
    }
    if(paymentRecordNum){
+      if(event.locals.user.employee){
+         const paymentRecord = await prisma.paymentRecord.findUnique({
+            where: {
+               paymentNumber:parseInt(paymentRecordNum, 10)
+            },
+         })
+         const customer = await prisma.user.findUnique({
+            where: {
+               id: paymentRecord?.customerId
+            }
+         });
+         const invoice = await prisma.invoice.findUnique({
+            where: {
+               invoiceNum: paymentRecord?.invoiceNum ? paymentRecord.invoiceNum : undefined
+            }
+         });
+         const address = await prisma.address.findFirst({
+            where: {
+               AND:[
+                  { softDelete: false },
+                  { userId: customer?.id },
+               ]
+            }
+         })
+         let refundRecord:RefundRecord | null = null;
+         if(paymentRecord?.refundNumber){
+            refundRecord = await prisma.refundRecord.findUnique({
+               where: {
+                  refundNumber: paymentRecord.refundNumber
+               }
+            })
+         }
+         return { paymentRecord, customer, invoice, refundRecord, address };
+      } else {
+         const paymentRecord = await prisma.paymentRecord.findUnique({
+            where: {
+               paymentNumber:parseInt(paymentRecordNum, 10)
+            },
+         })
+         if(paymentRecord?.customerId !== event.locals.user.id){
+            fail(400)
+         }
+         const customer = await prisma.user.findUnique({
+            where: {
+               id: paymentRecord?.customerId
+            }
+         });
+         const invoice = await prisma.invoice.findUnique({
+            where: {
+               invoiceNum: paymentRecord?.invoiceNum ? paymentRecord.invoiceNum : undefined
+            }
+         });
+         const address = await prisma.address.findFirst({
+            where: {
+               AND:[
+                  { softDelete: false },
+                  { userId: customer?.id },
+               ]
+            }
+         })
+         let refundRecord:RefundRecord | null = null;
+         if(paymentRecord?.refundNumber){
+            refundRecord = await prisma.refundRecord.findUnique({
+               where: {
+                  refundNumber: paymentRecord.refundNumber
+               }
+            })
+         }
+         return { paymentRecord, customer, invoice, refundRecord, address };
+
+      }
+   }
+   return {}
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+   default: async (event) => {
+      if(!event.locals.user?.employee){
+         redirect(302, '/login?toast=employee')
+      }
+      const formData = await event.request.formData()
+      const form = await superValidate(formData, zod(paymentRecordDeleteSchema));
+      if(!form.valid){
+         message(form, 'Not a valid form')
+      }
+      console.log(form)
+
       const paymentRecord = await prisma.paymentRecord.findUnique({
          where: {
-            paymentNumber:parseInt(paymentRecordNum, 10)
-         },
+            paymentNumber: form.data.paymentRecordNumber
+         }
       })
+      if(!paymentRecord){
+         message(form, 'Payment record not found')
+      }
       const customer = await prisma.user.findUnique({
          where: {
             id: paymentRecord?.customerId
          }
-      });
-      const invoice = await prisma.invoice.findUnique({
-         where: {
-            invoiceNum: paymentRecord?.invoiceNum ? paymentRecord.invoiceNum : undefined
-         }
-      });
-      const address = await prisma.address.findFirst({
-         where: {
-            AND:[
-               { softDelete: false },
-               { userId: customer?.id },
-            ]
-         }
       })
-      let refundRecord:RefundRecord | null = null;
-      if(paymentRecord?.refundNumber){
-         refundRecord = await prisma.refundRecord.findUnique({
-            where: {
-               refundNumber: paymentRecord.refundNumber
-            }
-         })
+      if(!customer){
+         message(form, 'Customer not found')
       }
-      return { paymentRecord, customer, invoice, refundRecord, address };
+      const response = await sendPaymentReceipt(customer!, paymentRecord!)
+      console.log(response);
    }
-   return {}
-}) satisfies PageServerLoad;
+};
