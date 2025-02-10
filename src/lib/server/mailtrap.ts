554f472569a3593ce35d4420925f05ca1a7c7ac6
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:21:50 -0700

    sendPaymentReceipt()

--- src/lib/server/mailtrap.ts
+++ src/lib/server/mailtrap.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_COMPANY_NAME, PUBLIC_URL } from "$env/static/public";
-import type { Invoice } from "@prisma/client";
+import type { Invoice, PaymentRecord, User } from "@prisma/client";
 import { MailtrapClient } from "mailtrap";
 import type { PartialUser } from "./partialTypes";
 
@@ -43,6 +43,16 @@ export async function sendInvoiceEmail(invoice:Invoice, customer:PartialUser) {
       subject: `Invoice ${invoice.invoiceNum} from ${PUBLIC_COMPANY_NAME}`,
       html: `Hello ${customer.givenName} <br/>Please pay $${invoice.invoiceAmount} for ${invoice.invoiceNotes} to pay please visit \
       <a href="${PUBLIC_URL}/invoices/pay?invoiceNum=${invoice.invoiceNum}>Pay invoice </a>`
+   })
+   return response
+}
+
+export async function sendPaymentReceipt(customer:User, paymentRecord:PaymentRecord) {
+   const response = await mailtrap.send({
+      from: sender,
+      to: [{email: customer.email!}],
+      subject: `${PUBLIC_COMPANY_NAME} receipt number ${paymentRecord.paymentNumber}`,
+      html: `Hello ${customer.givenName} <br/>Please visit <a href="${PUBLIC_URL}/paymentRecords/${paymentRecord.paymentNumber}>Record number ${paymentRecord.paymentNumber}</a> to view your receipt.`,
    })
    return response
 }
