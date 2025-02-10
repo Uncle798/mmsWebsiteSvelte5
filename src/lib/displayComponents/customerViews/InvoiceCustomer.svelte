ef10de9bd6b31ebde929c0475fb126b5606c6ca5
Eric Branson <eric.branson@gmail.com>
2025-02-10 13:42:29 -0700

    cleanup

---
+++ src/lib/displayComponents/customerViews/InvoiceCustomer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+   import type { Invoice } from "@prisma/client";
+   import dayjs from "dayjs";
+	import HorizontalDivider from "../HorizontalDivider.svelte";
+   interface Props {
+      invoice:Invoice;
+      classes?: string;
+   }
+   let { invoice, classes }:Props = $props();
+   const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
+</script>
+
+<div class="grid grid-cols-2 gap-x-2 {classes}">
+   <div class="col-span-2 text-center text-sm font-semibold">Invoice</div>
+   <div class='text-right'>Number</div> 
+   <div class="font-medium"><a href="/invoices/{invoice.invoiceNum}">{invoice.invoiceNum}</a></div>
+   <HorizontalDivider classes='col-span-2' />
+   <div class='text-right'>Amount</div>
+   <div class="font-medium ">{currencyFormatter.format(invoice.invoiceAmount)}</div>
+   <HorizontalDivider classes='col-span-2' />
+   <div class='text-right'>Created</div> 
+   <div class="font-medium">{dayjs(invoice.invoiceCreated).format('M/D/YYYY')}</div>
+   <HorizontalDivider classes='col-span-2' />
+   <div class='text-right'>Lease ID</div> 
+   <div class="truncate font-medium"><a href="/leases/{invoice.leaseId}">{invoice.leaseId}</a></div>
+   <HorizontalDivider classes='col-span-2' />
+   <div class="text-right">Notes</div> 
+   <div class="pl-4 -indent-4 pr-4 font-medium">{invoice.invoiceNotes}</div>
+   <HorizontalDivider classes='col-span-2' />
+   {#if invoice.paymentRecordNum}
+      <div class='text-right'>Payment number</div> 
+      <div class="font-medium"><a href="/paymentRecords/{invoice.paymentRecordNum}" class="">{invoice.paymentRecordNum}</a></div>
+      <HorizontalDivider classes='col-span-2'/>
+   {:else}
+      <div class='col-span-2 m-2'><a href="/paymentRecords/new?defaultCustomer={invoice.customerId}&defaultInvoice={invoice.invoiceNum}" class="btn preset-filled-primary-50-950 rounded-lg">Make a payment record for this lease</a></div>
+   {/if}
+   {#if invoice.deposit}
+      <div class="col-span-2 font-medium text-center">Deposit</div>
+   {/if}
+</div>
---
+++ src/lib/displayComponents/customerViews/RefundRecordCustomer.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { RefundRecord } from '@prisma/client';
+	import dayjs from 'dayjs';
+	import HorizontalDivider from '../HorizontalDivider.svelte';
+
+	interface Props {
+		refundRecord: RefundRecord;
+		classes?: string;
+	}
+	let { refundRecord, classes }: Props = $props();
+	const currencyFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+</script>
+
+<div class="grid grid-cols-2 gap-x-2 {classes}">
+	<div class="col-span-2 text-center text-sm font-semibold">Refund</div>
+	<div class="text-right">Number</div>
+	<div class="font-medium"><a href="/refundRecords/{refundRecord.refundNumber}">{refundRecord.refundNumber}</a></div>
+	<HorizontalDivider classes='col-span-2' />
+	<div class="text-right">Amount</div>
+	<div class="font-medium">{currencyFormatter.format(refundRecord.refundAmount)}</div>
+	<HorizontalDivider classes='col-span-2' />
+	{#if refundRecord.refundCompleted}
+		<div class="text-right">Completed</div>
+		<div class="font-medium">{dayjs(refundRecord.refundCompleted).format('M/D/YYYY')}</div>
+	{:else}
+		<div class="col-span-2 text-center font-medium">Refund not completed</div>
+	{/if}
+	<HorizontalDivider classes='col-span-2' />
+	<div class="text-right">Type</div>
+	<div class="font-medium">{refundRecord.refundType.replace(/\w\S*/g,(text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase())}</div>
+	<HorizontalDivider classes='col-span-2' />
+	<div class="text-right">Notes</div>
+	{#if refundRecord.paymentRecordNum}
+		<div class="text-left font-medium"><a href="/paymentRecords/{refundRecord.paymentRecordNum}">{refundRecord.refundNotes}</a></div>
+	{:else}
+		<div class="text-left font-medium">{refundRecord.refundNotes}</div>
+	{/if}
+</div>
