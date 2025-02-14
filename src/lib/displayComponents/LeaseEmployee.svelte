88d4ca6d4653ef3d9e31addb3b8d510e95c76976
Eric Branson <eric.branson@gmail.com>
2025-02-13 17:16:51 -0700

    Styling removing extra titles

--- src/lib/displayComponents/LeaseEmployee.svelte
+++ src/lib/displayComponents/LeaseEmployee.svelte
@@ -10,19 +10,19 @@
    const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
 </script>
 
-<div class="p-4 {classes} grid grid-cols-2 gap-x-2 items-start">
-   <div class="col-span-2 text-center text-sm font-semibold -m-1 -mt-2">Lease</div>
+<div class="p-4 {classes} grid grid-cols-2 gap-x-2">
+   <div class="col-span-2 text-center text-sm font-semibold">Lease</div>
    <div class="text-right">Unit number:</div>
    <div class="font-medium"><a href="/units/{lease.unitNum}">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
    <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right -indent-2">Lease effective date:</div>
+   <div class="text-right -indent-2">Effective date:</div>
    <div class="font-medium">{dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</div>
    <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right">Lease price:</div>
+   <div class="text-right">Price:</div>
    <div class="font-medium">{currencyFormatter.format(lease.price)}</div>
    <HorizontalDivider classes="col-span-2"/>
    {#if lease.leaseEnded}
-      <div class="text-right">Lease end date:</div>
+      <div class="text-right">End date:</div>
       <div class="font-medium"> {dayjs(lease.leaseEnded).format('M/D/YYYY')}</div>
       <HorizontalDivider classes="col-span-2"/>
    {:else}
