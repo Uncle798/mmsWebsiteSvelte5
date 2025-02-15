11db0d38f148f1575fbb5832cc74c28fca1db04a
Eric Branson <eric.branson@gmail.com>
2025-02-15 11:45:27 -0700

    styling

--- src/lib/displayComponents/Pagination.svelte
+++ src/lib/displayComponents/Pagination.svelte
@@ -15,14 +15,14 @@
    let slicedSource = $derived((source:[]) => source.slice((pageNum-1)*size, pageNum*size));
 </script>
 
-<footer class="flex w-full m-2">
+<footer class="flex w-full m-2 gap-1">
    {#if array.length > size}  
-      <select name="size" id="size" class="select w-1/2" bind:value={size}>
+      <select name="size" id="size" class="select w-1/2 rounded-lg" bind:value={size}>
          {#each [5,10,25,50] as v}
          <option value={v}>Show {v} {label} per page</option>
          {/each}
          <option value={array.length}>Show all {array.length} {label}</option>
       </select>
    {/if}
-   <Pagination data={array} bind:page={pageNum} bind:pageSize={size} classes=''/>
+   <Pagination data={array} bind:page={pageNum} bind:pageSize={size} classes='rounded-lg'/>
 </footer>
