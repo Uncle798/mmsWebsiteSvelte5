358257e5c706931d09132119c5dce90e7be4bde1
Eric Branson <eric.branson@gmail.com>
2025-02-15 11:46:04 -0700

    logoutForm

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,12 @@
+import { superValidate } from "sveltekit-superforms";
 import type { LayoutServerLoad } from "./$types";
+import { blankFormSchema } from "$lib/formSchemas/schemas";
+import { zod } from "sveltekit-superforms/adapters";
+
+
 
 export const load: LayoutServerLoad = async ({locals}) =>{
    const { user } = locals;
-   return { user };
+   const logOutForm = await superValidate(zod(blankFormSchema))
+   return { user, logOutForm };
 }
