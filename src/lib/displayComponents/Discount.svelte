95ed534bdbac64708ab83b2a20e443c8a3de3856
Eric Branson <eric.branson@gmail.com>
2025-02-16 13:05:21 -0700

    modernizing

--- src/lib/displayComponents/Discount.svelte
+++ src/lib/displayComponents/Discount.svelte
@@ -10,9 +10,13 @@
    let { discount, classes }:Props = $props();
 </script>
 
-<div class="{classes}">
-   <p>Code: {discount.code}</p>
-   <p>Amount Off: {currencyFormatter.format(discount.amountOff)}</p>
-   <p>Notes: {discount.notes}</p>
-   <p>Date created {dayjs(discount.discountCreated).format('M/D/YYYY')}</p>
+<div class="{classes} grid grid-cols-2 gap-x-2">
+   <div class="text-right">Code</div> <div>{discount.code}</div>
+   {#if discount.percentage}
+      <div class="text-right">Percent Off</div><div>{discount.amountOff}%</div>
+   {:else}
+      <div class="text-right">Amount Off</div><div>{currencyFormatter.format(discount.amountOff)}</div>
+   {/if}
+   <div class="text-right">Date created</div><div>{dayjs(discount.discountCreated).format('M/D/YYYY')}</div>
+   <div class="text-right">Notes</div><div class=" text-wrap">{discount.notes}</div>
 </div>
