f9de979cbf8cc6a5f22030a0d3cbc5c7aa13ff36
Eric Branson <eric.branson@gmail.com>
2025-02-16 13:05:43 -0700

    styling

--- src/lib/displayComponents/LeaseEmployee.svelte
+++ src/lib/displayComponents/LeaseEmployee.svelte
@@ -8,21 +8,23 @@
    }
    let { lease, classes }:Props = $props();
    const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
+
+   console.log(lease.price)
 </script>
 
 <div class="p-4 {classes} grid grid-cols-2 gap-x-2">
    <div class="col-span-2 text-center text-sm font-semibold">Lease</div>
-   <div class="text-right">Unit number:</div>
+   <div class="text-right">Unit number</div>
    <div class="font-medium"><a href="/units/{lease.unitNum}" class="anchor">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
    <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right -indent-2">Effective date:</div>
+   <div class="text-right -indent-2">Effective date</div>
    <div class="font-medium">{dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</div>
    <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right">Price:</div>
+   <div class="text-right">Price</div>
    <div class="font-medium">{currencyFormatter.format(lease.price)}</div>
    <HorizontalDivider classes="col-span-2"/>
    {#if lease.leaseEnded}
-      <div class="text-right">End date:</div>
+      <div class="text-right">End date</div>
       <div class="font-medium"> {dayjs(lease.leaseEnded).format('M/D/YYYY')}</div>
       <HorizontalDivider classes="col-span-2"/>
    {:else}
@@ -30,10 +32,10 @@
       <HorizontalDivider classes="col-span-2"/>
    {/if}
 
-   <div class="text-right">lease EID:</div>
+   <div class="text-right">lease EID</div>
    <div class="truncate font-medium">{lease.anvilEID}</div>
    <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right">leaseID:</div>
+   <div class="text-right">leaseID</div>
    <div class="truncate font-medium"><a href="/leases/{lease.leaseId}" class="anchor">{lease.leaseId}</a></div>
    <HorizontalDivider classes="col-span-2"/>
 </div>
