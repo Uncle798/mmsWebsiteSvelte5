1cc84e9f9b590dc85263d4461571e7223e2900a0
Eric Branson <eric.branson@gmail.com>
2025-02-16 14:11:54 -0700

    init

---
+++ src/lib/displayComponents/customerViews/UserCustomer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PartialUser } from '$lib/server/partialTypes';
+
+	interface Props {
+		user: PartialUser;
+		classes?: string;
+	}
+	let { user, classes }: Props = $props();
+</script>
+
+<div class="{classes}">
+	<p>{user.givenName} {user.familyName}</p>
+	{#if user.organizationName}
+		<p>{user.organizationName}</p>
+	{/if}
+	<p class="truncate">{user.email}</p>
+</div>
