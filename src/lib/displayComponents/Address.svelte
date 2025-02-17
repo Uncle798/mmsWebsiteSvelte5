b71afe4770e1789d0717f4cc1992628d2ac912cf
Eric Branson <eric.branson@gmail.com>
2025-02-17 11:27:32 -0700

    phone number anchor

--- src/lib/displayComponents/Address.svelte
+++ src/lib/displayComponents/Address.svelte
@@ -5,6 +5,7 @@
       classes?: string;
    }
    let { address = $bindable(), classes }:Props = $props()
+   const formattedPhone = address.phoneNum1.substring(0,3) + '.' + address.phoneNum1.substring(3,6) + '.' + address.phoneNum1.substring(6,10);
 </script>
 
 <div class="{classes}">
@@ -13,5 +14,5 @@
       <p>{address.address2}</p>
    {/if}
    <p>{address.city}, {address.state}, {address.postalCode}</p>
-   <div><a href="tel:{address.phoneNum1}">{address.phoneNum1}</a></div>
+   <div><a href="tel:{address.phoneNum1}" class="anchor">{formattedPhone}</a></div>
 </div>
