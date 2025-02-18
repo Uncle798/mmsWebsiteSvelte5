9ce936496364f93d5621f4f2bf9c748797d061d6
Eric Branson <eric.branson@gmail.com>
2025-02-18 13:46:47 -0700

    no need for form data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,7 @@
-import { superValidate } from "sveltekit-superforms";
+
 import type { LayoutServerLoad } from "./$types";
-import { blankFormSchema } from "$lib/formSchemas/schemas";
-import { zod } from "sveltekit-superforms/adapters";
-
-
 
 export const load: LayoutServerLoad = async ({locals}) =>{
    const { user } = locals;
-   const logOutForm = await superValidate(zod(blankFormSchema))
-   return { user, logOutForm };
+   return { user, };
 }
