828c72efe9c2bd36efde560096a5659820af7cd0
Eric Branson <eric.branson@gmail.com>
2025-02-18 15:21:09 -0700

    styling removed employee only stuff

--- src/lib/displayComponents/customerViews/UnitCustomer.svelte
+++ src/lib/displayComponents/customerViews/UnitCustomer.svelte
@@ -10,26 +10,19 @@
 </script>
 
 <div class="grid grid-cols-2 gap-x-2 {classes}">
-   <div class="col-span-2 text-center text-sm font-semibold -m-1 -mt-2">Unit</div>
+   <div class="col-span-2 text-center text-sm font-semibold">Unit</div>
    <div class="text-right">Number</div>
    <div class="font-medium">{unit.num.replace(/^0+/gm,'')}</div>
    <HorizontalDivider classes='col-span-2'/>
    <div class="text-right">Size</div>
    <div class="font-medium"> <a href="/units/size/{unit.size}">{unit.size.replace(/^0+/gm, '').replace(/x0/gm, 'x')}</a></div>
    <HorizontalDivider classes='col-span-2'/>
-   <div class="text-right">Advertised Price</div>
+   <div class="text-right">Price</div>
    <div class="font-medium">{currencyFormatter.format(unit.advertisedPrice)}</div>
+   <HorizontalDivider classes='col-span-2'/>
    <div class="text-right">Deposit</div>
    <div>{currencyFormatter.format(unit.deposit)}</div>
    <HorizontalDivider classes='col-span-2'/>
-   {#if unit.leasedPrice}
-      <div class="text-right">Leased Price</div>
-      <div class="font-medium">{currencyFormatter.format(unit.leasedPrice)}</div>
-      <HorizontalDivider classes='col-span-2'/>
-   {/if}
-   {#if unit.notes}
-      <div class="text-right">Notes:</div>
-      <div class="font-medium"> {unit.notes}</div>
-      <HorizontalDivider classes='col-span-2'/>
-   {/if}
+   <div class="text-right text-wrap">Approximate Amount of Stuff</div>
+   <div>{unit.description}</div>
 </div>
