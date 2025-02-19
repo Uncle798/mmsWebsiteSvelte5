467aa6d234d35babe6cd69d840f481350e18c7d8
Eric Branson <eric.branson@gmail.com>
2025-02-19 13:16:28 -0700

    returns promises returns unitCount

--- src/routes/units/available/+page.server.ts
+++ src/routes/units/available/+page.server.ts
@@ -1,15 +1,18 @@
 import { prisma } from '$lib/server/prisma';
-import type { Unit } from '@prisma/client';
+import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters';
+import { unitNotesFormSchema } from '$lib/formSchemas/schemas';
 
 export const load:PageServerLoad = (async (event) => {
-   const userId = event.url.searchParams.get('userId')
-   const leases = await prisma.lease.findMany({
+   const userId = event.url.searchParams.get('userId');
+   const unitNotesForm = await superValidate(zod(unitNotesFormSchema));
+   const leases = prisma.lease.findMany({
       where: {
          leaseEnded: null
       }
    });
-   const units = await prisma.unit.findMany({
+   const units = prisma.unit.findMany({
       orderBy: {
          size: 'asc'
       }, 
@@ -17,22 +20,6 @@ export const load:PageServerLoad = (async (event) => {
          unavailable: false
       },
    });
-   const availableUnits:Unit[] = [];
-   units.forEach((unit) => {
-      const lease = leases.find((l) => l.unitNum === unit.num)
-      if(!lease){
-         availableUnits.push(unit);
-      }
-   })
-   if(event.locals.user?.employee){
-      let lostRevenue = 0;
-      const percentAvailable = (availableUnits.length * 100)/units.length;
-      const totalUnits = units.length
-      availableUnits.forEach((unit) => {
-         lostRevenue += unit.advertisedPrice
-      })
-      
-      return { availableUnits, userId, totalUnits, lostRevenue, percentAvailable, }
-   }
-   return { availableUnits, userId }; 
+   const unitCount = await prisma.unit.count();
+   return { units, leases, userId, unitCount, unitNotesForm }; 
 }) 
