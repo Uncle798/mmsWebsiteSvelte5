5612458a749f418b8d7f852e4bc370324d0df155
Eric Branson <eric.branson@gmail.com>
2025-02-19 12:52:01 -0700

    location.reload()

--- src/lib/displayComponents/ThemeSelector.svelte
+++ src/lib/displayComponents/ThemeSelector.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
    import * as themes from '@skeletonlabs/skeleton/themes'
    const themeNames = Object.keys(themes)
    let currentTheme = $state(themeNames[0]);
    $effect(()=>{
-      if(typeof window !== undefined){
-         const theme = window.localStorage.getItem('theme');
-         if(theme && themeNames.includes(theme)){
-            document.documentElement.setAttribute('data-theme', theme);
-            currentTheme=theme
-         }
+      const theme = window.localStorage.getItem('theme');
+      if(theme && themeNames.includes(theme)){
+         document.documentElement.setAttribute('data-theme', theme);
+         currentTheme=theme
       }
    })
    function setTheme(event:Event){
       const select = event.target as HTMLSelectElement;
       const theme = select.value;
       if(themeNames.includes(theme)){
-         const year = 60*60*24*365
-         window.localStorage.setItem('theme', theme);
+         const year = 60*60*24*365;
+         const thisPage = window.location.pathname;
          document.cookie = `theme=${theme}; max-age=${year}; path=/; SameSite=Lax`
          document.documentElement.setAttribute('data-theme', theme);
-         currentTheme=theme
-         
+         currentTheme=theme;
+         location.reload();
       }
    }
 </script>
@@ -28,7 +27,7 @@
    <label for="themeSelect" class="label-text mx-2">Theme Selector
       <select name="themeSelect" id="themeSelect" class="select w-72 m-2" bind:value={currentTheme} onchange={setTheme}>
          {#each themeNames as themeName}
-            <option value={themeName} class=" capitalize">{themeName}</option>
+            <option value={themeName} class="capitalize">{themeName}</option>
          {/each}
       </select>
    </label>
