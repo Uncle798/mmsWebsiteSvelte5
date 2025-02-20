849fa273610bd314ff79c2f587b0c85b442ece2b
Eric Branson <eric.branson@gmail.com>
2025-02-20 11:56:42 -0700

    split to employee and customer

---
+++ src/lib/displayComponents/customerViews/AddressCustomer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Address } from "@prisma/client";
+   interface Props {
+      address: Address;
+      classes?: string;
+   }
+   let { address = $bindable(), classes }:Props = $props()
+   const formattedPhone = address.phoneNum1.substring(0,3) + '.' + address.phoneNum1.substring(3,6) + '.' + address.phoneNum1.substring(6,10);
+</script>
+
+<div class="{classes}">
+   <p>{address.address1}</p>
+   {#if address.address2}
+      <p>{address.address2}</p>
+   {/if}
+   <p>{address.city}, {address.state}, {address.postalCode}</p>
+   <div>{formattedPhone}</div>
+</div>
