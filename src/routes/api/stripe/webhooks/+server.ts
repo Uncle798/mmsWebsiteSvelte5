b6d3b6f8747477a20870df2c590465d30bab329f
Eric Branson <eric.branson@gmail.com>
2025-02-22 08:41:15 -0700

    towards subscription handling

--- src/routes/api/stripe/webhooks/+server.ts
+++ src/routes/api/stripe/webhooks/+server.ts
@@ -2,7 +2,7 @@ import { STRIPE_SIGNING_SECRET } from '$env/static/private';
 import { stripe } from '$lib/server/stripe';
 import { prisma } from '$lib/server/prisma';
 import type { RequestHandler } from './$types'; 
-import type { Invoice } from '@prisma/client';
+import type { Invoice, PaymentRecord } from '@prisma/client';
 
 
 export const POST: RequestHandler = async (event) => {
@@ -24,6 +24,7 @@ export const POST: RequestHandler = async (event) => {
          switch (stripeEvent?.type) {
             case 'payment_intent.created': {
                const paymentIntent = stripeEvent.data.object;
+               console.log(paymentIntent);
                const handlePaymentIntent = async (intent:typeof paymentIntent)=> {
                   let invoice:Invoice | null = null
                   const customerId = intent.metadata.customerId
@@ -49,48 +50,74 @@ export const POST: RequestHandler = async (event) => {
                      })
                   }
                   console.log('stripe webhooks invoice: ', invoice?.invoiceNum)
-                  const paymentRecord = await prisma.paymentRecord.create({
-                     data: {
-                        invoiceNum: invoice!.invoiceNum,
-                        customerId: customerId,
-                        paymentAmount: intent.amount / 100,
-                        paymentType: 'STRIPE',
-                        stripeId: intent.id,
-                        unitNum: intent.metadata.unitNum,
-                        paymentNotes: `Payment for invoice number: ${invoice?.invoiceNum}, ${invoice?.invoiceNotes}`,
-                        deposit: invoice?.deposit,
-                     }
-                  })
-                  await prisma.invoice.update({
-                     where: {
-                        invoiceNum: invoice?.invoiceNum
-                     },
-                     data: {
-                        paymentRecordNum: paymentRecord.paymentNumber
-                     }
-                  })
-                  await stripe.paymentIntents.update(intent.id, {
-                     metadata: {
-                        paymentNum: paymentRecord.paymentNumber
-                     }
-                  })
+                  let paymentRecord:PaymentRecord = {} as PaymentRecord
+                  try {                     
+                     paymentRecord = await prisma.paymentRecord.create({
+                        data: {
+                           invoiceNum: invoice!.invoiceNum,
+                           customerId: customerId,
+                           paymentAmount: intent.amount / 100,
+                           paymentType: 'STRIPE',
+                           stripeId: intent.id,
+                           unitNum: intent.metadata.unitNum,
+                           paymentNotes: `Payment for invoice number: ${invoice?.invoiceNum}, ${invoice?.invoiceNotes}`,
+                           deposit: invoice?.deposit,
+                        }
+                     })
+                  } catch (error) {
+                     console.error(error)
+                  }
+                  if(paymentRecord){
+                     await prisma.invoice.update({
+                        where: {
+                           invoiceNum: invoice?.invoiceNum
+                        },
+                        data: {
+                           paymentRecordNum: paymentRecord.paymentNumber
+                        }
+                     })
+                     await stripe.paymentIntents.update(intent.id, {
+                        metadata: {
+                           paymentNum: paymentRecord.paymentNumber
+                        }
+                     })
+                  }
                }
                handlePaymentIntent(paymentIntent);
                return new Response(JSON.stringify('ok'), {status: 200});
             }
-            case 'payment_intent.succeeded':{
-               const paymentIntent = stripeEvent.data.object;
-               const handlePaymentIntent = async (intent:typeof paymentIntent)=> {
-                  const paymentRecord = await prisma.paymentRecord.update({
+            case 'customer.subscription.created': {
+               const subscription = stripeEvent.data.object;
+               console.log('subscription', subscription.items);
+               const handleSubscription = async (s: typeof subscription)=>{
+                  const customer = await prisma.user.findFirst({
                      where: {
-                        stripeId: intent.id,
-                     },
-                     data: {
-                        paymentCompleted: new Date,
-                        paymentAmount: intent.amount_received / 100,
+                        stripeId: s.customer.toString()
                      }
                   })
-                  console.log('payment_intent.succeeded ' + paymentRecord.paymentCompleted)
+                  console.log(customer)
+               }
+               handleSubscription(subscription);
+               return new Response(JSON.stringify('ok'), {status:200})
+            }
+            case 'payment_intent.succeeded':{
+               const paymentIntent = stripeEvent.data.object;
+               console.log(paymentIntent);
+               const handlePaymentIntent = async (intent:typeof paymentIntent)=> {
+                  try {                
+                     const paymentRecord = await prisma.paymentRecord.update({
+                        where: {
+                           stripeId: intent.id,
+                        },
+                        data: {
+                           paymentCompleted: new Date,
+                           paymentAmount: intent.amount_received / 100,
+                        }
+                     })
+                     console.log('payment_intent.succeeded ' + paymentRecord?.paymentCompleted)
+                  } catch (error) {
+                     console.error(error)
+                  }
                }
                handlePaymentIntent(paymentIntent);
                return new Response(JSON.stringify('ok'), {status: 200});
