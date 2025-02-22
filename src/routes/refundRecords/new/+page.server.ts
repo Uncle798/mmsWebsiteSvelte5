38aad85d96ecdf73a3d9f8f3668d1450b444a289
Eric Branson <eric.branson@gmail.com>
2025-02-22 10:30:13 -0700

    better returns

--- src/routes/refundRecords/new/+page.server.ts
+++ src/routes/refundRecords/new/+page.server.ts
@@ -3,20 +3,16 @@ import { fail, redirect } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma'
 import { message, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { refundFormSchema } from '$lib/formSchemas/schemas';
-import { z } from 'zod';
+import { refundFormSchema, searchFormSchema } from '$lib/formSchemas/schemas';
 import { ratelimit } from '$lib/server/rateLimit';
 
-const formSchema = z.object({
-   search: z.string().min(0).max(255)
-})
 
 export const load = (async (event) => {
    if(!event.locals.user?.employee){
       redirect(302, '/login?toast=employee')
    }
    const refundForm = await superValidate(zod(refundFormSchema));
-   const searchForm = await superValidate(zod(formSchema));
+   const searchForm = await superValidate(zod(searchFormSchema));
    const paymentNum = event.url.searchParams.get('paymentNum');
    if(paymentNum){
       const paymentRecord = await prisma.paymentRecord.findUnique({
@@ -64,7 +60,7 @@ export const actions: Actions = {
       }
       const formData = await event.request.formData();
       console.log('refundRecords/new formData: ', formData);
-      const searchForm = await superValidate(formData, zod(formSchema));
+      const searchForm = await superValidate(formData, zod(searchFormSchema));
       const { success, reset } = await ratelimit.employeeForm.limit(event.locals.user.id);
       if(!success){
          const timeRemaining = Math.floor((reset - Date.now()) / 1000);
