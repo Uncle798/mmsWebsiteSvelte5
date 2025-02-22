351d6c416cf699b044052e5d8cdd27a569d3917b
Eric Branson <eric.branson@gmail.com>
2025-02-22 08:40:44 -0700

    reworking account settings

--- src/routes/accountSettings/+page.svelte
+++ src/routes/accountSettings/+page.svelte
@@ -7,13 +7,14 @@
     import EmailVerification from '$lib/forms/EmailVerificationForm.svelte'
 	import AddressCustomer from '$lib/displayComponents/customerViews/AddressCustomer.svelte';
 	import Header from '$lib/Header.svelte';
-	import { BadgeCheck, } from 'lucide-svelte';
+	import { BadgeCheck, User, } from 'lucide-svelte';
 	import LeaseCustomer from '$lib/displayComponents/customerViews/LeaseCustomer.svelte';
 	import LeaseEndForm from '$lib/forms/LeaseEndForm.svelte';
 	import { fade } from 'svelte/transition';
 	import InvoiceCustomer from '$lib/displayComponents/customerViews/InvoiceCustomer.svelte';
 	import PaymentRecordCustomer from '$lib/displayComponents/customerViews/PaymentRecordCustomer.svelte';
 	import ThemeSelector from '$lib/displayComponents/ThemeSelector.svelte';
+	import UserCustomer from '$lib/displayComponents/customerViews/UserCustomer.svelte';
     
     let {data}:{ data: PageData} = $props();
     let addressModalOpen = $state(false);
@@ -31,90 +32,80 @@
 <Header title='Settings for {data.user?.givenName}' />
 
 <div transition:fade={{duration:600}} class="mx-2 mt-9">
-    <span class="h1">{data.user?.givenName} {data.user?.familyName}</span>
-    {#if data.user?.organizationName}
-        <span class="h2">{data.user.organizationName}</span>
-    {/if}
+    <div class="flex flex-row gap-3">
+        <div>
+            {#if data.user}
+                <UserCustomer user={data.user}/>
+            {/if}
+            {#if data.user?.emailVerified}
+                <BadgeCheck size='20' />
+            {:else}
+                <button class="btn preset-filled-primary-50-950 text-wrap h-20 sm:h-8" onclick={()=>emailVerificationModalOpen=true}>Please confirm your email address</button>
+                <Modal
+                    bind:open={emailVerificationModalOpen}
+                    contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
+                    backdropClasses="backdrop-blur-sm"
+                >
+                    {#snippet content()}
+                        <EmailVerification data={data.emailVerificationForm} bind:emailVerificationModalOpen={emailVerificationModalOpen} redirect='false' bind:emailVerification={emailVerification}/>
+                        <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>emailVerificationModalOpen = false}>Cancel</button>
+                    {/snippet}
+                </Modal>
+            {/if}
+        </div>
+        <div class="flex flex-col sm:flex-row gap-2 m-2">
+            <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>nameModalOpen=true}>Change Name</button>
+            <button class="btn preset-filled-primary-50-950 rounded-lg " onclick={()=>emailModalOpen=true}>Change Email</button>
+        </div>
+    </div>
     <Modal
         bind:open={nameModalOpen}
-        triggerBase="btn preset-filled-primary-50-950 rounded-lg mt-2"
         contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
         backdropClasses="backdrop-blur-sm"
     >
-        {#snippet trigger()}
-            Change Name
-        {/snippet}
         {#snippet content()}
             <NameForm data={data.nameForm} bind:nameModalOpen={nameModalOpen} />
             <button class="btn" onclick={()=>nameModalOpen=false}>Cancel</button>
         {/snippet}
     </Modal>
 
-    <span class="h6 flex">{data.user?.email}
-        {#if data.user?.emailVerified}
-        <div class="flex p-1">
-            <BadgeCheck size='20'/>  
-            <div class="px-1 align-super text-xs">
-                Email Verified
-            </div>
-        </div>
-        
-        {:else}
-        <button class="btn preset-filled-primary-50-950 text-wrap h-20 sm:h-8" onclick={()=>emailVerificationModalOpen=true}>Please confirm your email address</button>
-        {/if}
-    </span>
     <Modal
         bind:open={emailModalOpen}
-        triggerBase="btn preset-filled-primary-50-950 rounded-lg text-wrap mt-2"
         contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
         backdropClasses="backdrop-blur-sm"
     >
-        {#snippet trigger()}
-            Change email address
-        {/snippet}
         {#snippet content()}
             <EmailForm data={data.emailForm} bind:emailModalOpen={emailModalOpen} bind:emailVerification={emailVerification} />
             <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>emailModalOpen = false}>Cancel</button>
         {/snippet}
     </Modal>
-        
-    <Modal
-        bind:open={emailVerificationModalOpen}
-        contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-        backdropClasses="backdrop-blur-sm"
-        >
-        {#snippet content()}
-            <EmailVerification data={data.emailVerificationForm} bind:emailVerificationModalOpen={emailVerificationModalOpen} redirect='false' bind:emailVerification={emailVerification}/>
-            <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>emailVerificationModalOpen = false}>Cancel</button>
-        {/snippet}
-    </Modal>
-
-    {#await data.addressPromise}
-        ...loading address
-    {:then address} 
+    <div class="flex flex-col sm:flex-row gap-3">
+        {#await data.addressPromise}
+            ...loading address
+        {:then address} 
         {#if address}
             <AddressCustomer {address} classes='pt-2'/>
         {/if}
-        
-        <Modal
-            bind:open={addressModalOpen}
-            triggerBase="btn preset-filled-primary-50-950 rounded-lg"
-            contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
-            backdropClasses="backdrop-blur-sm"
-        >
-        {#snippet trigger()}
-            {#if address}
-                Change Address
-            {:else}
-                Add address
-            {/if}
-        {/snippet}
-        {#snippet content()}
-            <AddressForm data={data.addressForm} bind:addressModalOpen={addressModalOpen} userId={data.user?.id!}/>
-            <button class="btn" onclick={()=>addressModalOpen=false}>Close</button>
-        {/snippet}
-    </Modal>
-    {/await}
+            <Modal
+                bind:open={addressModalOpen}
+                triggerBase="btn preset-filled-primary-50-950 rounded-lg"
+                contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-screen-sm"
+                backdropClasses="backdrop-blur-sm"
+            >
+                {#snippet trigger()}
+                    {#if address}
+                        Change Address
+                    {:else}
+                        Add address
+                    {/if}
+                {/snippet}
+                {#snippet content()}
+                    <AddressForm data={data.addressForm} bind:addressModalOpen={addressModalOpen} userId={data.user?.id!}/>
+                    <button class="btn" onclick={()=>addressModalOpen=false}>Close</button>
+                {/snippet}
+            </Modal>
+        {/await}
+    </div>
 
     {#await data.leasesPromise}
         ...loading leases
@@ -130,12 +121,21 @@
                 <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>leaseEndModalOpen=false}>Cancel</button>
             {/snippet}
         </Modal>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {#each leases as lease}
-                <LeaseCustomer lease={lease} classes='w-64  pt-4'/>
-                {#if !lease.leaseEnded}
-                <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=> setCurrentLeaseId(lease.leaseId)}>End Lease</button>
-                {/if}
+                <div>
+                    <LeaseCustomer lease={lease} classes='w-64'/>
+                    {#if !lease.leaseEnded}
+                        <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=> setCurrentLeaseId(lease.leaseId)}>End Lease</button>
+                        {#if !lease.stripeSubscriptionId}
+                            <a class="btn preset-filled-primary-50-950 rounded-lg m-2" href="/autopay?leaseId={lease.leaseId}">Sign Up for Autopay</a>
+                        {:else}
+                            Thanks for auto-paying!
+                        {/if}
+                    {/if}
+                </div>
             {/each}
+        </div>
         {/if}
     {/await}
     <div class="grid grid-cols-1 gap-x-1 gap-y-3 py-2 mt-10">
@@ -145,15 +145,15 @@
             {#await data.paymentsPromise}
                 loading payments
             {:then payments} 
-                {#each invoices as invoice}
-                {@const paymentRecord = payments.find((payment) => payment.invoiceNum === invoice.invoiceNum)}
-                    <div class="sm:grid sm:grid-cols-2">
+                <div class="sm:grid sm:grid-cols-2 gap-x-1 gap-y-3">
+                    {#each invoices as invoice}
+                    {@const paymentRecord = payments.find((payment) => payment.invoiceNum === invoice.invoiceNum)}
                         <InvoiceCustomer {invoice} classes="border border-primary-50 dark:border-primary-950 rounded-lg"/>
                         {#if paymentRecord}
                             <PaymentRecordCustomer {paymentRecord} classes="border border-primary-50 dark:border-primary-950 rounded-lg"/>
                         {/if}
-                    </div>
-                {/each}
+                    {/each}
+                </div>
             {/await}
         {/await}
     </div>
