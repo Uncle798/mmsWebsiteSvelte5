c6876da6e19b7a2e598fdc75fe08f3a863a2e068
Eric Branson <eric.branson@gmail.com>
2025-02-22 08:39:02 -0700

    Unit num links to lease

--- src/lib/displayComponents/customerViews/LeaseCustomer.svelte
+++ src/lib/displayComponents/customerViews/LeaseCustomer.svelte
@@ -13,7 +13,7 @@
 <div class="{classes} grid grid-cols-2 gap-x-2 items-start">
    <div class="col-span-2 text-center text-sm font-semibold -m-1 -mt-2">Lease</div>
    <div class="text-right">Unit number:</div>
-   <div class="font-medium"><a href="/units/{lease.unitNum}">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
+   <div class="font-medium"><a href="/leases/{lease.leaseId}" class="anchor">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
    <HorizontalDivider classes="col-span-2"/>
    <div class="text-right -indent-2">Effective date:</div>
    <div class="font-medium">{dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</div>
