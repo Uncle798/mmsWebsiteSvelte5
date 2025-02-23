ca9be657ad19e99f30b4c25ea8e66dab3dc2cf5f
Eric Branson <eric.branson@gmail.com>
2025-02-23 13:19:38 -0700

    displays employee view to employees else customer view

--- src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
+++ src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Address from '$lib/displayComponents/AddressEmployee.svelte';
+	import AddressEmployee from '$lib/displayComponents/AddressEmployee.svelte';
 	import AddressCustomer from '$lib/displayComponents/customerViews/AddressCustomer.svelte';
 	import InvoiceCustomer from '$lib/displayComponents/customerViews/InvoiceCustomer.svelte';
 	import PaymentRecordCustomer from '$lib/displayComponents/customerViews/PaymentRecordCustomer.svelte';
@@ -14,14 +14,17 @@
 
    let { data }: { data: PageData } = $props();
 </script>
-{#if data.user?.employee}
-   {#if data.paymentRecord }
+{#if data.paymentRecord }
    <Header title='Payment Record Num: {data.paymentRecord}' />
    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 mx-2 mt-10 border-2 dark:border-primary-950 border-primary-50 rounded-lg">
       <div class="flex flex-col p-2">
-         <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes=''/>
-         {#if !data.paymentRecord.refunded}
-            <a href='/refundRecords/new?paymentNumber={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2">Refund this payment</a>
+         {#if data.user?.employee}
+            <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes=''/>
+            {#if !data.paymentRecord.refunded}
+               <a href='/refundRecords/new?paymentNumber={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2">Refund this payment</a>
+            {/if}
+         {:else}
+            <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
          {/if}
          <form method="POST">
             <input type="hidden" value={data.paymentRecord.paymentNumber} name='paymentRecordNumber' />
@@ -30,45 +33,33 @@
       </div>
       <div class="flex flex-col p-2">
          {#if data.customer}
-            <UserEmployee user={data.customer} classes=''/>
+            {#if data.user?.employee}
+               <UserEmployee user={data.customer} classes=''/>
+            {:else}
+               <UserCustomer user={data.customer} />
+            {/if}
             {#if data.address}
-               <Address address={data.address} />
+               {#if data.user?.employee}
+                  <AddressEmployee address={data.address} />
+               {:else}
+                  <AddressCustomer address={data.address} />
+               {/if}
             {/if}
          {/if}
       </div>
       {#if data.invoice}
-         <InvoiceEmployee invoice={data.invoice} classes='p-2'/>
+         {#if data.user?.employee}
+            <InvoiceEmployee invoice={data.invoice} classes='p-2'/>
+         {:else}
+            <InvoiceCustomer invoice={data.invoice} />
+         {/if}
       {/if}
       {#if data.refundRecord}
-         <RefundRecordEmployee refundRecord={data.refundRecord} classes='p-2 '/>
+         {#if data.user?.employee}      
+            <RefundRecordEmployee refundRecord={data.refundRecord} classes='p-2 '/>
+         {:else}
+            <RefundRecordCustomer refundRecord={data.refundRecord} />
+         {/if}
       {/if}
    </div>
-   {/if}
-{:else}
-   {#if data.paymentRecord}
-      <Header title='Payment Record Num: {data.paymentRecord}' />
-      <div class="grid grid-cols-1 gap-3 sm:mx-2 mx-1 mt-10 sm:grid-cols-2">
-         <div class="flex flex-col">
-            <PaymentRecordCustomer paymentRecord={data.paymentRecord} classes=''/>
-            <form method="POST">
-               <input type="hidden" value={data.paymentRecord.paymentNumber} name='paymentRecordNumber' />
-               <button type="submit" class="btn rounded-lg preset-filled-primary-50-950 m-2">Email receipt</button>
-            </form>
-         </div>
-         <div class="flex flex-col ">
-            {#if data.customer}
-               <UserCustomer user={data.customer} classes='mx-1 sm:mx-2'/>
-               {#if data.address}
-                  <AddressCustomer address={data.address} classes='mx-1 sm:mx-2'/>
-               {/if}
-            {/if}
-         </div>
-         {#if data.invoice}
-            <InvoiceCustomer invoice={data.invoice} classes='m-2 sm:col-span-2'/>
-         {/if}
-         {#if data.refundRecord}
-            <RefundRecordCustomer refundRecord={data.refundRecord} classes='m-2 sm:col-span-2'/>
-         {/if}
-      </div>
-   {/if}
 {/if}
--- src/routes/refundRecords/[refundNumber]/+page.svelte
+++ src/routes/refundRecords/[refundNumber]/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import Address from '$lib/displayComponents/AddressEmployee.svelte';
+	import AddressEmployee from '$lib/displayComponents/AddressEmployee.svelte';
+	import AddressCustomer from '$lib/displayComponents/customerViews/AddressCustomer.svelte';
+	import InvoiceCustomer from '$lib/displayComponents/customerViews/InvoiceCustomer.svelte';
+	import PaymentRecordCustomer from '$lib/displayComponents/customerViews/PaymentRecordCustomer.svelte';
+	import RefundRecordCustomer from '$lib/displayComponents/customerViews/RefundRecordCustomer.svelte';
+	import UserCustomer from '$lib/displayComponents/customerViews/UserCustomer.svelte';
 	import HorizontalDivider from '$lib/displayComponents/HorizontalDivider.svelte';
 	import InvoiceEmployee from '$lib/displayComponents/InvoiceEmployee.svelte';
 	import PaymentRecordEmployee from '$lib/displayComponents/PaymentRecordEmployee.svelte';
@@ -13,26 +18,45 @@
 {#if data.refundRecord}
     {@const customer = data.refundRecord.customer}
     <Header title='Refund Record number: {data.refundRecord.refundNumber}' />
-    <div class="m-2 border border-primary-50 dark:border-primary-950 rounded-lg grid grid-cols-1 lg:grid-cols-2">
+    <div class="m-2 mt-10 border border-primary-50 dark:border-primary-950 rounded-lg grid grid-cols-1 lg:grid-cols-2">
         <div class="mb-2 sm:grid sm:grid-cols-2">
-            <RefundRecordEmployee refundRecord={data.refundRecord} classes=''/>
+            {#if data.user?.employee}
+                <RefundRecordEmployee refundRecord={data.refundRecord} classes=''/>
+            {:else}
+                <RefundRecordCustomer refundRecord={data.refundRecord} />
+            {/if}
             {#if customer}
                 <div class="flex flex-col p-2">
-                    <UserEmployee user={customer} classes=''/>
+                    {#if data.user?.employee}
+                        <UserEmployee user={customer} classes=''/>
+                    {:else}
+                        <UserCustomer user={customer} />
+                    {/if}
                     {#if data.address}
-                        <Address address={data.address} />
+                        {#if data.user?.employee}
+                            <AddressEmployee address={data.address} />
+                        {:else}
+                            <AddressCustomer address={data.address} />
+                        {/if}
                     {/if}
                 </div>
-    
             {/if}
         </div>
         <HorizontalDivider />
         <div class="sm:flex">
             {#if data.paymentRecord}
-                <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes='sm:w-1/2'/>
+                {#if data.user?.employee}
+                    <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes='sm:w-1/2'/>
+                {:else}
+                    <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
+                {/if}
             {/if}
             {#if data.invoice}
-                <InvoiceEmployee invoice={data.invoice} />
+                {#if data.user?.employee}
+                    <InvoiceEmployee invoice={data.invoice} />
+                {:else}
+                    <InvoiceCustomer invoice={data.invoice} />
+                {/if}
             {/if}
         </div>
     </div>
