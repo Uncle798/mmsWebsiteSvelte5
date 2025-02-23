d3e3f65f797b8d99ae8489cf2762b3a18c044a94
Eric Branson <eric.branson@gmail.com>
2025-02-23 13:16:31 -0700

    styling returns refundForm

--- src/routes/paymentRecords/+page.server.ts
+++ src/routes/paymentRecords/+page.server.ts
@@ -2,33 +2,43 @@ import { prisma } from '$lib/server/prisma';
 import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
-import { dateSearchFormSchema, searchFormSchema } from '$lib/formSchemas/schemas';
+import { dateSearchFormSchema, refundFormSchema, searchFormSchema } from '$lib/formSchemas/schemas';
 import { redirect } from '@sveltejs/kit';
 import { arrayOfYears } from '$lib/server/utils';
 
 export const load = (async (event) => {
-    if(!event.locals.user?.employee){
-        redirect(302, '/login?toast=employee');
-    }
-    const searchForm = await superValidate(zod(searchFormSchema));
-    const dateSearchForm = await superValidate(zod(dateSearchFormSchema));
-    const paymentRecordCount = await prisma.paymentRecord.count();
-    const firstPayment = await prisma.paymentRecord.findFirst({
-        orderBy: {
-            paymentCreated: 'asc'
-        }
-    })
-    const years = arrayOfYears(firstPayment?.paymentCreated.getFullYear())
-    const paymentRecords = prisma.paymentRecord.findMany({
-        orderBy: {
-            paymentCreated: 'asc'
-        },
-    });
-    const customers = prisma.user.findMany({});
-    const addresses = prisma.address.findMany({
-        where: {
-            softDelete: false
-        }
-    });
-    return { paymentRecords, searchForm, paymentRecordCount, years, customers, dateSearchForm, addresses };
+   if(!event.locals.user?.employee){
+      redirect(302, '/login?toast=employee');
+   }
+   const searchForm = await superValidate(zod(searchFormSchema));
+   const dateSearchForm = await superValidate(zod(dateSearchFormSchema));
+   const refundForm = await superValidate(zod(refundFormSchema));
+   const paymentRecordCount = await prisma.paymentRecord.count();
+   const firstPayment = await prisma.paymentRecord.findFirst({
+      orderBy: {
+         paymentCreated: 'asc'
+      }
+   })
+   const years = arrayOfYears(firstPayment?.paymentCreated.getFullYear())
+   const paymentRecords = prisma.paymentRecord.findMany({
+      orderBy: {
+         paymentCreated: 'asc'
+      },
+   });
+   const customers = prisma.user.findMany({});
+   const addresses = prisma.address.findMany({
+      where: {
+         softDelete: false
+      }
+   });
+   return { 
+      paymentRecords, 
+      searchForm, 
+      paymentRecordCount, 
+      years, 
+      customers, 
+      dateSearchForm, 
+      addresses, 
+      refundForm
+   };
 }) satisfies PageServerLoad;
