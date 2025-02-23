dbc9eea555d6d5b19fb9ed3ec66893e147480622
Eric Branson <eric.branson@gmail.com>
2025-02-23 13:20:12 -0700

    this is probably going away

--- src/routes/refundRecords/new/+page.svelte
+++ src/routes/refundRecords/new/+page.svelte
@@ -5,9 +5,14 @@
    import type { PaymentRecord } from '@prisma/client';
    import PaymentRecordEmployee from '$lib/displayComponents/PaymentRecordEmployee.svelte';
    import HorizontalDivider  from '$lib/displayComponents/HorizontalDivider.svelte';
+	import Pagination from '$lib/displayComponents/Pagination.svelte';
+	import Search from '$lib/forms/Search.svelte';
    let { data }: { data: PageData } = $props();
    let selectedPayment = $state<PaymentRecord>();
    let allPayments = $state<PaymentRecord[]>([]);
+   let size = $state(25);
+   let pageNum = $state(1);
+   let search = $state('')
    const depositWrapper = new Promise<PaymentRecord[]>(async res => {
       const deposits = await data.deposits;
       if(deposits){
@@ -28,48 +33,44 @@
          })
       }
    })
+   const slicedPayments = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.slice((pageNum -1) * size, pageNum*size));
+   const searchedPayments = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.filter((paymentRecord) => paymentRecord.paymentNotes?.includes(search)))
 </script>
 
 {#if selectedPayment}
    <PaymentRecordEmployee paymentRecord={selectedPayment} classes='mt-10'/>
    <HorizontalDivider />   
    <NewRefundForm data={data.refundForm} paymentRecord={selectedPayment} classes='p-2'/>
-{/if}
-{#await depositWrapper}
-   loading deposits
-{:then deposits}
-   <div class="grid grid-cols-1 gap-3 mt-10">
-      {#if allPayments.length > 0}
-         {#each allPayments as paymentRecord}
-            <div class="flex flex-col border-2 border-primary-50 dark:border-primary-950">
-               <PaymentRecordEmployee {paymentRecord} />
-               <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=>{selectedPayment=paymentRecord}} >Refund this payment</button>
-            </div>
-         {/each}
-      {/if}
-   </div>
-   {#await paymentsWrapper}
-      <div class="grid grid-cols-1 gap-3 mt-10">
-         {#if allPayments.length > 0}
-         {#each allPayments as paymentRecord}
-            <div class="flex flex-col border-2 border-primary-50 dark:border-primary-950">
-               <PaymentRecordEmployee {paymentRecord} />
-               <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=>{selectedPayment=paymentRecord}} >Refund this payment</button>
-            </div>
-         {/each}
-      {/if}
+{:else}
+   {#await depositWrapper}
+      <div class="mt-10 mx-2">
+         loading deposits ...
       </div>
-   {:then paymentRecords} 
-      <div class="grid grid-cols-1 gap-3 mt-10">
-         {#if allPayments.length > 0}
-            {#each allPayments as paymentRecord}
-               <div class="flex flex-col border-2 border-primary-50 dark:border-primary-950">
-                  <PaymentRecordEmployee {paymentRecord} />
-                  <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=>{selectedPayment=paymentRecord}} >Refund this payment</button>
-               </div>
-            {/each}
-         {/if}
-      </div>
+   {:then deposits}
+      {#await paymentsWrapper}
+         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-10 m-2">
+            <Search bind:search={search} searchType='Payment notes' data={data.searchForm} classes='col-span-full' />
+            {#if allPayments.length > 0}
+               {#each slicedPayments(allPayments) as paymentRecord}
+                  <div class="flex flex-col border-2 border-primary-50 dark:border-primary-950 rounded-lg">
+                     <PaymentRecordEmployee {paymentRecord} />
+                     <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=>{selectedPayment=paymentRecord}} >Refund this payment</button>
+                  </div>
+               {/each}
+               <Pagination bind:size={size} bind:pageNum={pageNum} array={allPayments} label='deposits' classes='col-span-3'/>
+            {/if}
+         </div>
+      {:then paymentRecords} 
+         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-10 m-2">
+            {#if allPayments.length > 0}
+               {#each allPayments as paymentRecord}
+                  <div class="flex flex-col border-2 border-primary-50 dark:border-primary-950 rounded-lg">
+                     <PaymentRecordEmployee {paymentRecord} />
+                     <button class="btn preset-filled-primary-50-950 rounded-lg m-2" onclick={()=>{selectedPayment=paymentRecord}} >Refund this payment</button>
+                  </div>
+               {/each}
+            {/if}
+         </div>
+      {/await}
    {/await}
-{/await}
-
+{/if}
