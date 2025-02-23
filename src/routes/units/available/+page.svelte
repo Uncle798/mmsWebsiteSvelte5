d478bc0b9033d0a5aa14e1a61695402deae961b6
Eric Branson <eric.branson@gmail.com>
2025-02-22 18:16:12 -0700

    filter by size

--- src/routes/units/available/+page.svelte
+++ src/routes/units/available/+page.svelte
@@ -25,19 +25,39 @@
       })
       res(availableUnits)
    })
+   let sizeFilter = $state('');
+   const filterSize = $derived((units:Unit[]) => units.filter((unit) => unit.size.includes(sizeFilter)))
+   function setSizeFilter(event:Event){
+      const select = event.target as HTMLSelectElement;
+      const size = select.value
+      sizeFilter = size;
+      console.log(sizeFilter)
+   }
 </script>
 <Header title='Available Units' />
 {#await wrapper}
-    ...loading available units
+   <div class="mt-10">
+      ...loading available units
+   </div>
 {:then availableUnits}   
    {#if data.user?.employee}
-      <div class="flex sticky top-8 dark:bg-tertiary-950 bg-tertiary-50 rounded-lg">
+      <div class="flex sticky top-8 dark:bg-tertiary-950 bg-tertiary-50 rounded-lg" transition:fade={{duration:600}}>
          <span class="m-2">Available: {availableUnits.length} of {data.unitCount}</span>
          <span class="m-2">Available percentage {Math.round((availableUnits.length*100)/data.unitCount)}%</span>
          <span class="m-2">Open revenue per month: {currencyFormatter.format(lostRevenue)}</span>
       </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-3 m-1 sm:m-2 pt-10">
-         {#each availableUnits as unit}
+         <div class="col-span-full">
+            <label for="sizeFilter" class="label-text">Filter by size
+               <select name="sizeFilter" id="sizeFilter" class="select" bind:value={sizeFilter} onchange={setSizeFilter}>
+                  <option value="">All</option>
+                  {#each data.sizes as size}
+                     <option value={size}>{size.replace(/^0/gm, '').replace(/x0/gm, 'x')}</option>
+                  {/each}
+               </select>
+            </label>
+         </div>
+         {#each filterSize(availableUnits) as unit}
             <div class="border-2 border-primary-50 dark:border-primary-950 rounded-lg">
                <UnitEmployee {unit}/>
                <div class="text-center sm:col-span-2">
@@ -48,9 +68,19 @@
          {/each}
       </div>
    {:else}
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 m-1 sm:m-2 pt-8">
-         <span class="sm:col-span-2 md:col-span-3 lg:col-span-4  h2 text-center">Available Units</span>
-         {#each availableUnits as unit}
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 m-1 sm:m-2 pt-8" transition:fade={{duration:600}}>
+         <span class="col-span-full h2 text-center">Available Units</span>
+         <div class="col-span-full">
+            <label for="sizeFilter" class="label-text">Filter by size
+               <select name="sizeFilter" id="sizeFilter">
+                  <option value="">All</option>
+                  {#each data.sizes as size}
+                     <option value={size}>{size.replace(/^0/gm, '').replace(/x0/gm, 'x')}</option>
+                  {/each}
+               </select>
+            </label>
+         </div>
+         {#each filterSize(availableUnits) as unit}
             <div class="border border-primary-50 dark:border-primary-950 rounded-lg">
                <UnitCustomer {unit}/>
                <div class="text-center sm:col-span-2">
