c1671e6afad9cae9ed22d9b79f50730e09827eb0
Eric Branson <eric.branson@gmail.com>
2025-02-22 18:15:28 -0700

    returns sizes

--- src/routes/units/available/+page.server.ts
+++ src/routes/units/available/+page.server.ts
@@ -3,6 +3,7 @@ import { superValidate } from 'sveltekit-superforms';
 import type { PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import { unitNotesFormSchema } from '$lib/formSchemas/schemas';
+import pricingData from '$lib/server/pricingData';
 
 export const load:PageServerLoad = (async (event) => {
    const userId = event.url.searchParams.get('userId');
@@ -21,5 +22,11 @@ export const load:PageServerLoad = (async (event) => {
       },
    });
    const unitCount = await prisma.unit.count();
-   return { units, leases, userId, unitCount, unitNotesForm }; 
+   const sizes:string[] = []
+   pricingData.forEach((datum) => {
+      if(datum.size !== 'ours'){
+         sizes.push(datum.size)
+      }
+   })
+   return { units, leases, userId, unitCount, unitNotesForm, sizes }; 
 }) 
