89de2052be76620827c57f404c076d68fd123103
Eric Branson <eric.branson@gmail.com>
2025-02-25 10:50:11 -0700

    not returning ours units

--- src/routes/units/unavailable/+page.server.ts
+++ src/routes/units/unavailable/+page.server.ts
@@ -4,6 +4,7 @@ import { prisma } from '$lib/server/prisma';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { unitNotesFormSchema } from '$lib/formSchemas/schemas';
+import pricingData from '$lib/server/pricingData';
 
 export const load = (async (event) => {
    if(!event.locals.user?.employee){
@@ -11,9 +12,19 @@ export const load = (async (event) => {
    }
    const units = await prisma.unit.findMany({
       where: {
-         unavailable: true
+         AND: [
+            {unavailable: true},
+            {size: {not: 'ours'}}
+         ]
+      },
+   });
+   const unitCount = await prisma.unit.count();
+   const unitNotesForm = await superValidate(zod(unitNotesFormSchema));
+   const sizes:string[] = [];
+   pricingData.forEach((datum) => {
+      if(datum.size !== 'ours'){
+         sizes.push(datum.size)
       }
    })
-   const unitNotesForm = await superValidate(zod(unitNotesFormSchema));
-   return { units, unitNotesForm };
+   return { units, unitNotesForm, unitCount, sizes };
 }) satisfies PageServerLoad;
