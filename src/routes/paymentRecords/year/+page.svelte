791ae68804d5aa0e0467f65d2c956372ffb24ba4
Eric Branson <eric.branson@gmail.com>
2025-02-25 10:49:25 -0700

    uses a select now

--- src/routes/paymentRecords/year/+page.svelte
+++ src/routes/paymentRecords/year/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
     import type { PageData } from './$types';
 
     let { data }: { data: PageData } = $props();
+    let currentYear = $state('');
+    function onSelect(event:Event){
+        const select = event.target as HTMLSelectElement;
+        const year = select.value;
+        goto(`/paymentRecords/year/${year}`);
+    }
 </script>
-Select year: 
-{#each data.years as year}
-   <a href="/paymentRecords/year/{year}" class="btn">{year}</a>
-{/each}
+<div class="mt-10">
+    <label for="selectYear" class="label-text"> 
+        Select year:
+        <select name="selectYear" id="selectYear" class="select" bind:value={currentYear} onchange={onSelect}>
+            {#each data.years as year}
+            <option value={year}>{year}</option>
+            {/each}
+        </select> 
+    </label>
+</div>
