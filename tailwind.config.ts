404313a1f8230558d70c19839d73d5469486f335
Eric Branson <eric.branson@gmail.com>
2025-02-26 08:34:53 -0700

    removing skeleton plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,10 @@ import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin'
-import * as themes from '@skeletonlabs/skeleton/themes'
 
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		contentPath(import.meta.url, 'svelte')
 	],
 
 	theme: {
@@ -16,31 +13,6 @@ export default {
 	},
 
 	plugins: [typography, forms, containerQueries, 
-		skeleton({
-			themes: [ 
-				themes.catppuccin,
-				themes.cerberus,
-				themes.concord,
-				themes.crimson,
-				themes.fennec,
-				themes.hamlindigo,
-				themes.legacy,
-				themes.mint,
-				themes.modern,
-				themes.mona,
-				themes.nosh,
-				themes.nouveau,
-				themes.pine,
-				themes.reign,
-				themes.rocket,
-				themes.rose,
-				themes.sahara,
-				themes.seafoam,
-				themes.terminus,
-				themes.vintage,
-				themes.vox,
-				themes.wintry,
-			]
-		})
+
 	]
 } as Config;
