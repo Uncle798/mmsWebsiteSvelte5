89887bf4160de05175904c48ff727c0b7d86118d
Eric Branson <eric.branson@gmail.com>
2025-03-01 13:57:08 -0700

    just returns leases ended in the last year

--- src/routes/units/recentlyMovedOut/+page.server.ts
+++ src/routes/units/recentlyMovedOut/+page.server.ts
@@ -1,10 +1,9 @@
 import { prisma } from '$lib/server/prisma';
 import dayjs from 'dayjs';
 import type { PageServerLoad } from './$types';
-import type { Invoice, PaymentRecord } from '@prisma/client';
 
 export const load = (async () => {
-   const leases = await prisma.lease.findMany({
+   const leases = prisma.lease.findMany({
       where: {
          leaseEnded:{
             gt: dayjs(new Date()).subtract(1, 'year').toDate()
@@ -14,32 +13,5 @@ export const load = (async () => {
          leaseEnded: 'desc'
       }
    })
-   const invoices:Invoice[] =[];
-   leases.forEach(async (lease)=> {
-      const invoice = await prisma.invoice.findFirst({
-         where: {
-            AND:[
-               { leaseId: lease.leaseId },
-               { invoiceNotes: {
-                  contains: 'deposit'
-               }}
-            ]
-         }
-      })
-      if(invoice){
-         invoices.push(invoice);
-      }
-   })
-   const paymentRecords:PaymentRecord[] =[]
-   invoices.forEach(async (invoice) =>{
-      const paymentRecord = await prisma.paymentRecord.findUnique({
-         where: {
-            paymentNumber: invoice.paymentRecordNum!
-         }
-      })
-      if(paymentRecord){
-         paymentRecords.push(paymentRecord);
-      }
-   })
-   return { leases, invoices, paymentRecords};
+   return { leases,};
 }) satisfies PageServerLoad;
