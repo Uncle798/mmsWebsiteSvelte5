7e07d4dc51ef820b5a5c271487d186a39a5b1684
Eric Branson <eric.branson@gmail.com>
2025-03-02 18:59:46 -0700

    styling

--- src/lib/displayComponents/Pagination.svelte
+++ src/lib/displayComponents/Pagination.svelte
@@ -1,5 +1,10 @@
 <script lang='ts'>
    import { Pagination } from "@skeletonlabs/skeleton-svelte";
+   import IconArrowLeft from 'lucide-svelte/icons/arrow-left';
+   import IconArrowRight from 'lucide-svelte/icons/arrow-right';
+   import IconEllipsis from 'lucide-svelte/icons/ellipsis';
+   import IconFirst from 'lucide-svelte/icons/chevrons-left';
+   import IconLast from 'lucide-svelte/icons/chevron-right';
    interface Props {
       pageNum: number;
       size: number;
@@ -17,14 +22,20 @@
    let slicedSource = $derived((source:[]) => source.slice((pageNum-1)*size, pageNum*size));
 </script>
 
-<footer class="flex w-full m-2 gap-1 {classes}">
+<div class="flex flex-col sm:flex-row m-2 gap-1 {classes}">
    {#if array.length > size}  
-      <select name="size" id="size" class="select w-1/2 rounded-lg" bind:value={size}>
+      <select name="size" id="size" class="select rounded-lg truncate sm:w-1/2" bind:value={size}>
          {#each [5,10,25,50] as v}
          <option value={v}>Show {v} {label} per page</option>
          {/each}
          <option value={array.length}>Show all {array.length} {label}</option>
       </select>
    {/if}
-   <Pagination data={array} bind:page={pageNum} bind:pageSize={size} classes='rounded-lg'/>
-</footer>
+   <Pagination data={array} bind:page={pageNum} bind:pageSize={size} classes='rounded-lg' showFirstLastButtons={true} siblingCount={3}>
+      {#snippet labelEllipsis()}<IconEllipsis class="size-4" />{/snippet}
+      {#snippet labelNext()}<IconArrowRight class="size-4" />{/snippet}
+      {#snippet labelPrevious()}<IconArrowLeft class="size-4" />{/snippet}
+      {#snippet labelFirst()}<IconFirst class="size-4" />{/snippet}
+      {#snippet labelLast()}<IconLast class="size-4" />{/snippet}
+   </Pagination>
+</div>
