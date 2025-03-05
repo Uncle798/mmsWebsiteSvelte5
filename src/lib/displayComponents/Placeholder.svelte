b3ae41b05b8289f33c78b5327d56b77a79c88f92
Eric Branson <eric.branson@gmail.com>
2025-03-05 09:39:20 -0700

    added defaults

--- src/lib/displayComponents/Placeholder.svelte
+++ src/lib/displayComponents/Placeholder.svelte
@@ -3,21 +3,21 @@
     interface Props {
         numCols: number;
         numRows: number;
-        heightClass: string
+        heightClass: string;
+        classes?: string;
     }
     let {
-        numCols,
-        numRows,
-        heightClass
+        numCols = 1,
+        numRows = 1,
+        heightClass = 'h-10',
+        classes = '',
     }:Props = $props()
 </script>
 
-<div class="w-full space-y-4">
-   <div class="space-y-4">
-       <div class="grid grid-cols-{numCols.toString()} gap-x-1 gap-y-3 p-3">
+<div class="{classes}">
+    <div class="grid grid-cols-{numCols.toString()} gap-x-1 gap-y-3 p-3">
         {#each {length:numRows*numCols} as row }
             <div class="placeholder rounded-2xl animate-pulse {heightClass}"></div>
         {/each}
-       </div>
-   </div>
+    </div>
 </div>
