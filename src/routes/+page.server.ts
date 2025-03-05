94ce55011ab079c8279469006e0be09eed4c6ada
Eric Branson <eric.branson@gmail.com>
2025-03-05 09:39:36 -0700

    returns just available units

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,24 +2,29 @@ import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 import pricingData from '$lib/server/pricingData';
 export const load:PageServerLoad = (async () => {
-   const leases = prisma.lease.findMany({
+   const availableUnits = prisma.unit.findMany({
       where: {
-         leaseEnded: null
-      }
-   });
-   const units = prisma.unit.findMany({
+         AND: [
+            { unavailable: false },
+            { size: {
+               not: 'ours' 
+            }},
+            { lease: {
+               none: {
+                  leaseEnded: null
+               }
+            }}
+         ]
+      },
       orderBy: {
          size: 'asc'
-      }, 
-      where: {
-         unavailable: false
-      },
-   });
+      }
+   })
    const sizes:string[] = [];
    pricingData.forEach((datum) => {
       if(datum.size !== 'ours'){
          sizes.push(datum.size)
       }
    })
-   return { units, leases, sizes }
+   return { availableUnits, sizes }
 })
