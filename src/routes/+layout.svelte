960366feaa9f3996baeccd7a58fb8cc82bca5210
Eric Branson <eric.branson@gmail.com>
2025-05-08 12:13:46 -0600

    bind:open for modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -57,7 +57,7 @@
 </script>
 
 <Modal
-	bind:open={menuOpen}
+	open={menuOpen}
 	triggerBase="btn bg-primary-50 dark:bg-primary-950 hover:shadow-xl hover:border-2 border-secondary-50 dark:border-secondary-950 fixed top-0 z-50"
 	contentBase="bg-surface-100-900 p-2 space-y-2 shadow-xl w-[240px] h-screen"
 	positionerJustify="justify-start"
