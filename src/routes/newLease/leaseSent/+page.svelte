7187a5341478702a07d33edfb7499162f119329c
Eric Branson <eric.branson@gmail.com>
2025-05-18 15:29:17 -0600

    Only displays if packetDetails

--- src/routes/newLease/leaseSent/+page.svelte
+++ src/routes/newLease/leaseSent/+page.svelte
@@ -6,11 +6,13 @@
 	import { PUBLIC_ANVIL_EMAIL, PUBLIC_COMPANY_NAME } from '$env/static/public';
 	import PaymentRecordCustomer from '$lib/displayComponents/customerViews/PaymentRecordCustomer.svelte';
 </script>
-<Header title='Lease Sent' />
-<div class="mx-1 sm:mx-2 mt-10">
-    <span>Your lease has been sent to {data.customer?.email} from {PUBLIC_ANVIL_EMAIL}</span>
-    <span>Leases are valid when both parties have signed. {PUBLIC_COMPANY_NAME} will sign your lease during normal business hours.</span>
-</div>
+{#if data.packetDetails}
+    <Header title='Lease Sent' />
+    <div class="mx-1 sm:mx-2 mt-10">
+        <span>Your lease has been sent to {data.customer?.email} from {PUBLIC_ANVIL_EMAIL}</span>
+        <span>Leases are valid when both parties have signed. {PUBLIC_COMPANY_NAME} will sign your lease during normal business hours.</span>
+    </div>
+{/if}
 {#if data.paymentRecord}
     <PaymentRecordCustomer paymentRecord={data.paymentRecord} classes='m-2 w-72 border border-primary-50 dark:border-primary-950 rounded-lg'/>
 {/if}
