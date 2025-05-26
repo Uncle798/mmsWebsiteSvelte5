e3644f125bd4c1e9b0111bfb5e84c58a7ec34c3e
Eric Branson <eric.branson@gmail.com>
2025-05-26 13:09:18 -0600

    init

---
+++ src/routes/api/newAutopay/+server.ts
@@ -0,0 +1,38 @@
+import { prisma } from '$lib/server/prisma';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async (event) => {
+   const body = await event.request.json();
+   const {leaseId} = body;
+   if(!leaseId){
+      return new Response(JSON.stringify('leaseId not provided'), {status:400})
+   }
+   const lease = await prisma.lease.findUnique({
+      where: {
+         leaseId,
+      }
+   });
+   if(!lease){
+      return new Response(JSON.stringify('lease not found'), {status:404})
+   }
+   const invoice = await prisma.invoice.findFirst({
+      where: {
+         AND: [
+            { leaseId: lease.leaseId },
+            { paymentRecordNum: null }
+         ]
+      }
+   })
+   if(invoice){
+      return new Response(JSON.stringify(invoice.invoiceNum), {status: 200})
+   } else {
+      const previousInvoice = await prisma.invoice.findFirst({
+         orderBy:{
+            
+         }
+      })
+
+   }
+
+   return new Response();
+};
