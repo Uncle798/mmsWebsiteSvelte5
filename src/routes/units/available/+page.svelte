8bc579c33435b73b4943509dd2ec51b8c208cfed
Eric Branson <eric.branson@gmail.com>
2025-05-29 14:00:03 -0600

    size filter styling

--- src/routes/units/available/+page.svelte
+++ src/routes/units/available/+page.svelte
@@ -19,27 +19,27 @@
 </script>
 <Header title='Available Units' />
 {#await data.availableUnits}
-   <div class="mt-10">
+   <div class="m-2">
       ...loading available units
    </div>
 {:then availableUnits}   
    {#if data.user?.employee}
-      <div class="flex sticky top-8 dark:bg-tertiary-950 bg-tertiary-50 rounded-b-lg" transition:fade={{duration:600}}>
+      <div class="flex fixed bg-tertiary-50-950 rounded-b-lg z-40 w-full" in:fade={{duration:600}}>
          <span class="m-2">Available: {availableUnits.length} of {data.unitCount}</span>
          <span class="m-2">Available percentage {Math.round((availableUnits.length*100)/data.unitCount)}%</span>
          <span class="m-2">Open revenue per month: {currencyFormatter.format(lostRevenue)}</span>
       </div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 m-1 sm:m-2 pt-10">
-         <div class="col-span-full">
-            <label for="sizeFilter" class="label-text">Filter by size
-               <select name="sizeFilter" id="sizeFilter" class="select" bind:value={sizeFilter} onchange={setSizeFilter}>
-                  <option value="">All</option>
-                  {#each data.sizes as size}
-                     <option value={size}>{size.replace(/^0/gm, '').replace(/x0/gm, 'x')}</option>
-                  {/each}
-               </select>
-            </label>
-         </div>
+      <div class="fixed top-18 w-full z-30 bg-surface-50-950 p-2" in:fade={{duration:600}}>
+         <label for="sizeFilter" class="label-text">Filter by size
+            <select name="sizeFilter" id="sizeFilter" class="select" bind:value={sizeFilter} onchange={setSizeFilter}>
+               <option value="">All</option>
+               {#each data.sizes as size}
+                  <option value={size}>{size.replace(/^0/gm, '').replace(/x0/gm, 'x')}</option>
+               {/each}
+            </select>
+         </label>
+      </div>
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 m-1 sm:m-2 bg-surface-50-950 sm:mt-28">
          {#each filterSize(availableUnits) as unit}
             <div class="border-2 border-primary-50 dark:border-primary-950 rounded-lg">
                <UnitEmployee {unit}/>
