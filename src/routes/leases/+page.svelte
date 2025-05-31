f16c979308ce4bc4783ee3204fa6df2f9e2197ea
Eric Branson <eric.branson@gmail.com>
2025-05-31 09:27:46 -0600

    styling

--- src/routes/leases/+page.svelte
+++ src/routes/leases/+page.svelte
@@ -1,36 +1,91 @@
 <script lang="ts">
    import Header from '$lib/Header.svelte';
-	import type { Lease } from '@prisma/client';
+	import type { User, Lease } from '@prisma/client';
    import type { PageData } from './$types';
 	import LeaseEmployee from '$lib/displayComponents/LeaseEmployee.svelte';
    import UserEmployee from '$lib/displayComponents/UserEmployee.svelte';
 	import Search from '$lib/forms/Search.svelte';
 	import Pagination from '$lib/displayComponents/Pagination.svelte';
+	import AddressEmployee from '$lib/displayComponents/AddressEmployee.svelte';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+	import { SearchIcon, PanelTopClose } from 'lucide-svelte';
+	import { leaseEndFormSchema } from '$lib/formSchemas/schemas';
+	import { snapshot } from 'node:test';
    let { data }: { data: PageData } = $props();
    let search = $state('')
    let pageNum = $state(1);
    let size = $state(25);
    let searchedLeases = $derived((leases:Lease[]) => leases.filter((lease) => lease.leaseId.includes(search)))
    let slicedLeases = $derived((leases:Lease[]) => leases.slice((pageNum-1)*size, pageNum*size));
+   let customers = $state<User[]>();
+   const wrapper = new Promise<User[]>(async res => {
+      customers = await data.customers
+      res(customers)
+   })
+   const searchByCustomer = $derived((leases:Lease[]) => {
+      const filteredCustomers = customers!.filter((customer) => {
+         return customer.familyName?.includes(search) || customer.givenName?.includes(search)
+      })
+      const returnedLeases:Lease[] =[];
+      for(const customer of filteredCustomers){
+         const customerLeases = leases.filter((lease) => lease.customerId === customer.id)
+         for(const cL of customerLeases){
+            returnedLeases.push(cL)
+         }
+      }
+      return returnedLeases
+   })
+   let customerSearch = $state('')
+   let searchDrawerOpen = $state(false)
 </script>
 <Header title='All leases' />
 
 {#await data.leases}
-   loading {data.leaseCount} leases
+   Loading {data.leaseCount} leases...
 {:then leases}
-   {#await data.customers}
-      loading customers
+   {#await wrapper}
+      Loading customers...
    {:then customers} 
-      <Search search={search} searchType='lease id' data={data.searchForm} />
-      {#each slicedLeases(searchedLeases(leases)) as lease}
+      {#await data.addresses}
+         Loading addresses...
+      {:then addresses}
+         <Modal
+            open={searchDrawerOpen}
+            onOpenChange={(event)=>(searchDrawerOpen = event.open)}
+            triggerBase='btn preset-filled-primary-50-950 rounded-lg fixed top-0 right-0 z-50'
+            contentBase='bg-surface-100-900 h-[240px] w-screen rounded-lg'
+            positionerJustify=''
+            positionerAlign=''
+            positionerPadding=''
+            transitionsPositionerIn={{y:-360, duration: 600}}
+            transitionsPositionerOut={{y:-360, duration: 600}}
+            modal={false}
+         >
+            {#snippet trigger()}
+               <SearchIcon />
+            {/snippet}
+            {#snippet content()}
+               <button onclick={()=>searchDrawerOpen=false} class='btn preset-filled-primary-50-950 rounded-lg m-1 absolute top-0 right-0'><PanelTopClose/></button>
+               <Search search={search} searchType='lease id' data={data.searchForm} classes='mt-10 mx-1'/>
+               <Search search={customerSearch} searchType='customer name' data={data.searchForm} classes='mx-1' />
+            {/snippet}
+         </Modal>
+         {#each slicedLeases(searchByCustomer(searchedLeases(leases))) as lease}
          {@const customer = customers.find((customer) => customer.id === lease.customerId)}
-         <div class="flex">
-            <LeaseEmployee lease={lease} />
+         {@const leaseAddress = addresses.find((address) => address.addressId === lease.addressId)}
+         <div class="grid sm:grid-cols-2 border border-primary-50-950 m-1 sm:m-2 rounded-lg">
+            <LeaseEmployee lease={lease} classes='mx-2'/>
             {#if customer}
-               <UserEmployee user={customer} />
+               <div class='m-2'> 
+                  <UserEmployee user={customer} />
+                  {#if leaseAddress}
+                     <AddressEmployee address={leaseAddress} />
+                  {/if}
+               </div>
             {/if}
          </div>
-      {/each}
-      <Pagination bind:size={size} bind:pageNum={pageNum} array={searchedLeases(leases)} label='leases'/>
+         {/each}
+         <Pagination bind:size={size} bind:pageNum={pageNum} array={searchedLeases(leases)} label='leases'/>
+      {/await}
    {/await}
 {/await}
--- src/routes/leases/[leaseId]/+page.svelte
+++ src/routes/leases/[leaseId]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AddressEmployee from '$lib/displayComponents/AddressEmployee.svelte';
 	import LeaseCustomer from '$lib/displayComponents/customerViews/LeaseCustomer.svelte';
 	import UserCustomer from '$lib/displayComponents/customerViews/UserCustomer.svelte';
    import LeaseEmployee from '$lib/displayComponents/LeaseEmployee.svelte';
@@ -11,13 +12,30 @@
 
 <Header title='Lease {data.lease?.leaseId}' />
 {#if data.user?.employee}   
-   <div class="flex mt-10 mx-1 sm:mx-2">
+   <div class="grid sm:grid-cols-2 m-1 sm:m-2 border-2 border-primary-50-950 rounded-lg">
       {#if data.lease}
-         <LeaseEmployee lease={data.lease} classes='mx-2 border rounded-lg p-2'/>
+         <LeaseEmployee lease={data.lease} classes='border-b border-primary-50-950'/>
       {/if}
-      {#if data.customer}
-         <UserEmployee user={data.customer} classes='mx-2 border rounded-lg p-2'/>
-      {/if}
+      <div class="m-1 sm:m-2">
+         {#if data.customer}
+            <UserEmployee user={data.customer} classes='mx-2'/>
+         {/if}
+         {#if data.address}
+            {#if data.currentAddress}
+            <label class="label-text">Lease Address:
+               <AddressEmployee address={data.address} classes='text-base mx-2' />
+            </label>
+            {:else}
+               <AddressEmployee address={data.address} classes='mx-2'/>
+            {/if}
+         {/if}
+         {#if data.currentAddress}
+            <label >Current address
+               <AddressEmployee address={data.currentAddress} classes='mx-2'/>
+            </label>
+            
+         {/if}
+      </div>
    </div>
 {:else}
    <div class="flex flex-col sm:flex-row mt-10 mx-1 sm:mx-2">
