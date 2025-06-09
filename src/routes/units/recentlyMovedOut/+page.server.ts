f01bc08bff3df5aa9b5731091453bf5940495684
Eric Branson <eric.branson@gmail.com>
2025-06-08 19:15:30 -0600

    by units then leases todo add customers

--- src/routes/units/recentlyMovedOut/+page.server.ts
+++ src/routes/units/recentlyMovedOut/+page.server.ts
@@ -1,8 +1,22 @@
 import { prisma } from '$lib/server/prisma';
 import dayjs from 'dayjs';
 import type { PageServerLoad } from './$types';
+import { superValidate } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import { unitNotesFormSchema } from '$lib/formSchemas/schemas';
 
 export const load = (async () => {
+   const units = prisma.unit.findMany({
+      where: {
+         lease: {
+            some: {
+               leaseEnded: {
+                  gt: dayjs(new Date()).subtract(1,'year').toDate()
+               }
+            }
+         }
+      }
+   })
    const leases = prisma.lease.findMany({
       where: {
          leaseEnded:{
@@ -13,5 +27,6 @@ export const load = (async () => {
          leaseEnded: 'desc'
       }
    })
-   return { leases,};
+   const unitNotesForm = await superValidate(valibot(unitNotesFormSchema))
+   return { units, leases, unitNotesForm, };
 }) satisfies PageServerLoad;
--- src/routes/units/recentlyMovedOut/+page.svelte
+++ src/routes/units/recentlyMovedOut/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
    import LeaseEmployee from '$lib/displayComponents/LeaseEmployee.svelte';
+	import UnitEmployee from '$lib/displayComponents/UnitEmployee.svelte';
+	import UnitNotesForm from '$lib/forms/UnitNotesForm.svelte';
    import Header from '$lib/Header.svelte';
    import type { PageData } from './$types';
 
@@ -8,14 +10,27 @@
 
 <Header title='Recently Moved Out' />
 
-{#await data.leases}
-   <div class="mt-8">
-      loading units...
+{#await data.units}
+   <div class="mt-10">
+      Loading units...
    </div>
-{:then leases} 
-   <div class="mt-8">
-      {#each leases as lease}
-         <LeaseEmployee {lease} />
-      {/each}
-   </div>
+{:then units}
+   {#await data.leases}
+      <div class="mt-10">
+         Loading leases...
+      </div>
+   {:then leases} 
+      <div class="grid grid-cols-1 gap-1 mt-10">
+         {#each units as unit}
+         {@const unitLeases = leases.filter((lease) => lease.unitNum === unit.num)}
+            <div class="mx-2 border border-primary-50-950 rounded-lg">
+               <UnitEmployee {unit} classes=''/>
+               <UnitNotesForm data={data.unitNotesForm} {unit} classes='mx-1'/>
+               {#each unitLeases as lease}
+                  <LeaseEmployee {lease} />
+               {/each}
+            </div>
+         {/each}
+      </div>
+   {/await}
 {/await}
