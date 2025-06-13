3018f1f5be42f96307320839b0073abc6a67da99
Eric Branson <eric.branson@gmail.com>
2025-06-13 10:49:38 -0600

    cleanup

--- src/routes/invoices/new/+page.server.ts
+++ src/routes/invoices/new/+page.server.ts
@@ -6,23 +6,23 @@ import { redirect } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
 
 export const load = (async (event) => {
-    if(!event.locals.user?.employee){
-        redirect(302, '/login?toast=employee')
-     }
-    const newInvoiceForm = await superValidate(valibot(newInvoiceFormSchema));
-    const registerForm = await superValidate(valibot(registerFormSchema));
-    const customers = await prisma.user.findMany({
-        orderBy: {
-            familyName: 'asc'
-        }
-    });
-    const leases = await prisma.lease.findMany({
-        orderBy: {
-            leaseEnded: { 
-                sort: 'desc', 
-                nulls: 'first'
-            }
-        }
-    })
-    return { newInvoiceForm, customers, leases, registerForm };
+   if(!event.locals.user?.employee){
+      redirect(302, '/login?toast=employee')
+   }
+   const newInvoiceForm = await superValidate(valibot(newInvoiceFormSchema));
+   const registerForm = await superValidate(valibot(registerFormSchema));
+   const customers = await prisma.user.findMany({
+      orderBy: {
+         familyName: 'asc'
+      },
+   });
+   const leases = await prisma.lease.findMany({
+      orderBy: {
+         leaseEnded: { 
+               sort: 'desc', 
+               nulls: 'first'
+         }
+      }
+   })
+   return { newInvoiceForm, customers, leases, registerForm };
 }) satisfies PageServerLoad;
