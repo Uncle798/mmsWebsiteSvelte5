619be0bcb845159cdb424e2415ad8dac66ffa53f
Eric Branson <eric.branson@gmail.com>
2025-06-15 16:53:40 -0600

    cleanup resend

--- src/lib/forms/EmailVerificationForm.svelte
+++ src/lib/forms/EmailVerificationForm.svelte
@@ -3,48 +3,78 @@
    import TextInput from '$lib/formComponents/TextInput.svelte';
    import type { EmailVerificationFormSchema } from "$lib/formSchemas/schemas";
    import FormProgress from "$lib/formComponents/FormSubmitWithProgress.svelte";
-   import { invalidateAll } from "$app/navigation";
    import FormMessage from "$lib/formComponents/FormMessage.svelte";
+	import { onMount } from "svelte";
+	import { BookDashed } from "lucide-svelte";
    
    interface Props {
       data: SuperValidated<Infer<EmailVerificationFormSchema>>; 
       emailVerificationModalOpen: boolean;
+      userId: string;
       redirect: string;
-      emailVerification: Boolean;
       classes?: string;
    }
    let {
       data, 
       emailVerificationModalOpen=$bindable(),
+      userId,
       redirect=$bindable(''),
-      emailVerification=$bindable(),
       classes,
    }:Props = $props();
    let { form, errors, constraints, message, enhance, submitting, delayed, timeout } = superForm(data, {
       onSubmit(){
-         emailVerification=false;
       }, 
       onUpdated(event) {
          emailVerificationModalOpen = false;
-         emailVerification = false;
        },
    });
+   let emailAddress = $state('');
+   let mounted = $state(false)
+   onMount(async () => {
+      const response = await fetch('/api/sendEmailVerification', {
+         method: 'POST',
+         body: JSON.stringify({userId})
+      })
+      const body = await response.json();
+      if(body.email){
+         emailAddress = body.email
+      }
+      mounted = true;
+   })
+   async function resend(){
+      mounted = false
+      const response = await fetch('/api/sendEmailVerification', {
+         method: 'POST',
+         body: JSON.stringify({userId})
+      })
+      const body = await response.json()
+      if(body.email){
+         emailAddress = body.email
+      }
+      mounted = true
+   }
 </script>
 <div class={classes}>
    <FormMessage message={$message} />
-   <form method="POST" action="/register/emailVerification?/verify&redirect={redirect}" use:enhance>
-      <TextInput
+   {#if emailAddress !== ''}
+      <div>
+         An email has been sent to {emailAddress}, please enter the code contained below
+      </div>
+   {/if}
+   {#if mounted}
+      <form method="POST" action="/register/emailVerification?/verify&redirect={redirect}" use:enhance>
+         <TextInput
          label="Code: "
          name="code"
          bind:value={$form.code}
          errors={$errors.code}
          constraints={$constraints.code}
          placeholder='12345678'
-      />
-      <FormProgress delayed={$delayed} timeout={$timeout}/>
-   </form>
-
-   <form method="POST" action="/register/emailVerification?/resend" >
-      <button class="btn preset-filled-primary-50-950 rounded-lg">Resend email</button>
-   </form>
+         />
+         <FormProgress delayed={$delayed} timeout={$timeout}/>
+      </form>
+      <button class="btn preset-filled-primary-50-950 rounded-lg" onclick={()=>resend()}>Resend email</button>
+   {:else}
+      Sending verification...
+   {/if}
 </div>
