addc05b8137b73fc8405bbf540e28e020ad641d7
Eric Branson <eric.branson@gmail.com>
2025-06-16 11:01:54 -0600

    fixed not absolute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -109,7 +109,7 @@
 	<header> 
 	<Modal
 		bind:open={menuOpen}
-		triggerBase="btn bg-primary-50-950 hover:shadow-xl hover:border-2 border-secondary-50-950 absolute top-0 left-0 z-50"
+		triggerBase="btn bg-primary-50-950 hover:shadow-xl hover:border-2 border-secondary-50-950 fixed top-0 left-0 z-50"
 		contentBase="bg-surface-100-900 p-2 space-y-2 shadow-xl w-[125px] h-screen"
 		positionerJustify="justify-start"
 		positionerAlign=""
