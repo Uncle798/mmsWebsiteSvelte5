d385ea94b919cdd28b2e2cd4c76199ff0671fd80
Eric Branson <eric.branson@gmail.com>
2025-06-17 13:48:46 -0600

    removed stripe ID

--- src/lib/displayComponents/UserAdmin.svelte
+++ src/lib/displayComponents/UserAdmin.svelte
@@ -15,7 +15,6 @@
 	{/if}
 	<div class="truncate"><a href="mailto:{user.email}" class="anchor">{user.email}</a></div>
 	<div class="grid grid-cols-2 gap-x-2">
-		<div class="text-right">StripeId: </div><div class="truncate">{user.stripeId}</div>
 		{#if user.admin}
 			<div class="col-span-2 font-medium text-center">Admin</div>
 			{:else if user.employee}	
