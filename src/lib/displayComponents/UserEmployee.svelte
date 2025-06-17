7ce380a8f8302ebd2f9e0cbef5093b2cb14411c2
Eric Branson <eric.branson@gmail.com>
2025-06-17 11:51:53 -0600

    spans not p

--- src/lib/displayComponents/UserEmployee.svelte
+++ src/lib/displayComponents/UserEmployee.svelte
@@ -8,10 +8,10 @@
 	let { user, classes }: Props = $props();
 </script>
 
-<div class="{classes}">
-	<p><a href="/users/{user.id}" class="anchor">{user.givenName} {user.familyName}</a></p>
+<div class="{classes} flex flex-col">
+	<span><a href="/users/{user.id}" class="anchor">{user.givenName} {user.familyName}</a></span>
 	{#if user.organizationName}
-		<p>{user.organizationName}</p>
+		<span>{user.organizationName}</span>
 	{/if}
-	<p class="truncate"><a href="mailto:{user.email}" class="anchor">{user.email}</a></p>
+	<span class=""><a href="mailto:{user.email}" class="anchor">{user.email}</a></span>
 </div>
