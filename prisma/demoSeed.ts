aa1caf1974adfdf71bf84221e3981e8d118c4f6f
Eric Branson <eric.branson@gmail.com>
2025-06-30 13:59:57 -0600

    protected creating user and session

--- prisma/demoSeed.ts
+++ prisma/demoSeed.ts
@@ -12,23 +12,37 @@ function generateSessionToken():string {
 }
 
 async function main() {
-   const user = await prisma.user.create({
-      data: {
-         givenName: process.env.USER_GIVEN_NAME,
-         familyName: process.env.USER_FAMILY_NAME,
-         email: process.env.USER_EMAIL,
-         emailVerified: true,
-         employee: true,
-         admin: true,
-      }
-   });
-   const session = await prisma.session.create({
-      data: {
-         userId: user.id,
-         expiresAt: dayjs().add(1, 'month').toDate(),
-         id: process.env.DEMO_SESSION_TOKEN!,
+   let user = await prisma.user.findUnique({
+      where: {
+         email: process.env.USER_EMAIL
       }
    })
+   if(!user){
+      user = await prisma.user.create({
+         data: {
+            givenName: process.env.USER_GIVEN_NAME,
+            familyName: process.env.USER_FAMILY_NAME,
+            email: process.env.USER_EMAIL,
+            emailVerified: true,
+            employee: true,
+            admin: true,
+         }
+      });
+   }
+   let session = await prisma.session.findFirst({
+      where: {
+         userId: user.id
+      }
+   })
+   if(!session){
+      session = await prisma.session.create({
+         data: {
+            userId: user.id,
+            expiresAt: dayjs().add(1, 'month').toDate(),
+            id: process.env.DEMO_SESSION_TOKEN!,
+         }
+      })
+   }
 }
 
 main().catch((error) => {
