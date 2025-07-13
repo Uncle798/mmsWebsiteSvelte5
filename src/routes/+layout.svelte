2fd7cc3dbcb48e8f00c7f5adeabf15d8f52e013c
Eric Branson <eric.branson@gmail.com>
2025-07-13 09:10:12 -0600

    everything has a tooltip now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,24 +27,25 @@
 		{link: '/units', label:'All Units', toolTip: 'See all your units in one place and keep notes about their status'},
 		{link: '/units/available', label:'Available Units', toolTip: 'All your available units with open revenue'},
 		{link: '/units/unavailable', label: 'Unavailable units', toolTip: 'See which units you\'ve marked unavailable'},
-		{link: '/units/recentlyMovedOut', label: 'Recently moved out Units'},
-		{link: '/units/size', label: 'Units by size'},
-		{link: '/discounts', label: 'Discounts'},
-		{link: '/users/customers', label: 'Customers'},
-		{link: '/leases', label: 'All Leases'},
-		{link: '/invoices', label: 'Invoices'},
-		{link: '/invoices/unpaid', label: 'Unpaid invoices'},
-		{link: '/invoices/pastDue', label: 'Past due invoices'},
-		{link: '/invoices/year', label: 'Invoices by year'},
-		{link: '/invoices/new', label: 'New Invoice'},
-		{link: '/paymentRecords', label: 'Payment Records'},
-		{link: '/paymentRecords/new', label: 'New Payment Record'},
-		{link: '/paymentRecords/deposits', label: 'Deposits'},
-		{link: '/paymentRecords/year', label: 'Payment Records by year'},
-		{link: '/refundRecords', label:'Refund Records'},
-		{link: '/refundRecords/year', label: 'Refunds by year'},
-		{link: '/employeeNewLease', label:'New lease'},
-		{link: '/employeeNewCustomer', label:'New customer'},
+		{link: '/units/recentlyMovedOut', label: 'Recently moved out Units', toolTip: 'See who just left'},
+		{link: '/units/size', label: 'Units by size', toolTip: 'Change unit prices and see how much revenue each size generates'},
+		{link: '/discounts', label: 'Discounts', toolTip: 'Manage discounts here'},
+		{link: '/users/customers', label: 'Customers', toolTip: 'View all current customers here'},
+		{link: '/leases', label: 'All Leases', toolTip: 'Find every lease old and new here'},
+		{link: '/invoices', label: 'Invoices', toolTip: 'All invoices, may take a second to load'},
+		{link: '/invoices/new', label: 'New Invoice', toolTip: 'Create a new invoice, the first step in getting paid'},
+		{link: '/invoices/unpaid', label: 'Unpaid invoices', toolTip: 'Open invoices'},
+		{link: '/invoices/pastDue', label: 'Past due invoices', toolTip: 'Invoices that are unpaid and past due'},
+		{link: '/invoices/year', label: 'Invoices by year', toolTip: 'If you know what year you\'re looking for this will load faster'},
+		{link: '/paymentRecords', label: 'Payment Records', toolTip: 'All payment records, may take a second to load'},
+		{link: '/paymentRecords/new', label: 'New Payment Record', toolTip: 'Record a payment here'},
+		{link: '/paymentRecords/deposits', label: 'Deposits', toolTip: 'Currently held deposits'},
+		{link: '/paymentRecords/year', label: 'Payment Records by year', toolTip: 'If you know what year you\'re looking for this will load faster' },
+		{link: '/refundRecords', label:'Refund Records', toolTip: 'Track refunds here'},
+		{link: '/refundRecords/new', label: 'New Refund Record', toolTip: 'Create a new refund record'},
+		{link: '/refundRecords/year', label: 'Refunds by year', toolTip: 'If you know what year you\'re looking for this will load faster'},
+		{link: '/employeeNewLease', label:'New lease', toolTip: 'Rent a unit here'},
+		{link: '/employeeNewCustomer', label:'New customer', toolTip: 'Create a new customer'},
 	]
 	let adminLinks:Link[] = [
 		{link: '/users', label:'All Users', toolTip: 'Change the employment status of a user here'},
@@ -63,28 +64,57 @@
 	<Modal
 		bind:open={menuOpen}
 		triggerBase="btn bg-primary-50-950 hover:shadow-xl hover:border-2 border-secondary-50-950 fixed top-0 left-0 z-50"
-		contentBase={`bg-surface-100-900 space-y-2 shadow-xl w-[240px] h-screen`}
+		contentBase="bg-surface-100-900 space-y-2 shadow-xl w-[240px] h-screen"
 		positionerJustify="justify-start"
 		positionerAlign=""
 		positionerPadding=""
 		transitionsPositionerIn={{ x: -280, duration: 400 }}
 		transitionsPositionerOut={{ x: -280, duration: 400 }}
 	>
-	{#snippet trigger()}
+		{#snippet trigger()}
 			<Menu class='mx-2 border-2 z-50' />	
-	{/snippet}
-	{#snippet content()}
-		<article class="h-full">
-			<ul class="overflow-auto h-8/9 m-1">
-				{#if data.user?.employee}
-					{#each employeeLinks as employeeLink}
-						{#if employeeLink.toolTip}
-							<li><Tooltip
-								open={tooltipOpenIdentifier === employeeLink.link}
+		{/snippet}
+		{#snippet content()}
+			<article class="h-full">
+				<ul class="overflow-auto h-8/9 m-1">
+					{#if data.user?.employee}
+						{#each employeeLinks as employeeLink}
+							{#if employeeLink.toolTip}
+								<li><Tooltip
+									open={tooltipOpenIdentifier === employeeLink.link}
+									onOpenChange={(event) => {
+										if(event.open){
+											tooltipOpenIdentifier = employeeLink.link
+										} else if (tooltipOpenIdentifier === employeeLink.link){
+											tooltipOpenIdentifier = null
+										}
+									}}
+									positioning={{placement: 'top-end'}}
+									contentBase="card preset-filled p-2"
+									openDelay={200}
+									zIndex='30'
+								>
+									{#snippet content()}
+										{employeeLink.toolTip}
+									{/snippet}
+									{#snippet trigger()}
+										<a class="anchor" href={employeeLink.link}>{employeeLink.label}</a>
+									{/snippet}
+								</Tooltip>
+								</li>
+							{:else if !employeeLink.toolTip}
+								<li><a class="anchor" href={employeeLink.link}>{employeeLink.label}</a></li>
+							{/if}
+						{/each}
+					{/if}
+					{#if data.user?.admin}
+						{#each adminLinks as adminLink}
+							<Tooltip
+								open={tooltipOpenIdentifier === adminLink.link}
 								onOpenChange={(event) => {
 									if(event.open){
-										tooltipOpenIdentifier = employeeLink.link
-									} else if (tooltipOpenIdentifier === employeeLink.link){
+										tooltipOpenIdentifier = adminLink.link
+									} else if (tooltipOpenIdentifier === adminLink.link){
 										tooltipOpenIdentifier = null
 									}
 								}}
@@ -93,27 +123,25 @@
 								openDelay={200}
 								zIndex='30'
 							>
-								{#snippet content()}
-									{employeeLink.toolTip}
-								{/snippet}
-								{#snippet trigger()}
-									<a class="anchor" href={employeeLink.link}>{employeeLink.label}</a>
-								{/snippet}
+							{#snippet content()}
+								{adminLink.toolTip}
+							{/snippet}
+							{#snippet trigger()}
+								<li><a class="anchor" href={adminLink.link}>{adminLink.label}</a></li>
+							{/snippet}
 							</Tooltip>
-							</li>
-						{:else if !employeeLink.toolTip}
-							<li><a class="anchor" href={employeeLink.link}>{employeeLink.label}</a></li>
-						{/if}
-					{/each}
-				{/if}
-				{#if data.user?.admin}
-					{#each adminLinks as adminLink}
+						{/each}
+					{/if}
+				</ul>
+				<div class="fixed w-[240px] bottom-0 bg-surface-100-900 border-1 border-primary-50-950 rounded-lg">
+					<ul class="m-1">
+						{#if data.user}
 						<Tooltip
-							open={tooltipOpenIdentifier === adminLink.link}
+							open={tooltipOpenIdentifier === '/accountSettings'}
 							onOpenChange={(event) => {
 								if(event.open){
-									tooltipOpenIdentifier = adminLink.link
-								} else if (tooltipOpenIdentifier === adminLink.link){
+									tooltipOpenIdentifier = '/accountSettings'
+								} else if (tooltipOpenIdentifier === '/accountSettings'){
 									tooltipOpenIdentifier = null
 								}
 							}}
@@ -122,40 +150,24 @@
 							openDelay={200}
 							zIndex='30'
 						>
-						{#snippet content()}
-							{adminLink.toolTip}
-						{/snippet}
-						{#snippet trigger()}
-							<li><a class="anchor" href={adminLink.link}>{adminLink.label}</a></li>
-						{/snippet}
+							{#snippet content()}
+								Change your name or address here. View all invoices, payment receipts, and refund records. Best of all, sign up for Auto-pay here.
+							{/snippet}
+							{#snippet trigger()}
+								<li><a href="/accountSettings" class="anchor">Settings</a></li>
+							{/snippet}
 						</Tooltip>
-					{/each}
-				{/if}
-			</ul>
-			<div class="fixed w-[240px] bottom-0 bg-surface-100-900 border-1 border-primary-50-950 rounded-lg">
-				<ul class="m-1">
-					{#if data.user}
-					<Tooltip
-
-					>
-						{#snippet content()}
-							Change your name or address here. View all invoices, payment receipts, and refund records. Best of all, sign up for Auto-pay here.
-						{/snippet}
-						{#snippet trigger()}
-							<li><a href="/accountSettings" class="anchor">Settings</a></li>
-						{/snippet}
-					</Tooltip>
-						<form action="/logout" method="post" use:enhance>
-							<li><button class="anchor" type="submit">Logout</button></li>
-						</form>
-					{:else}
-						<li><a class="anchor" href="/login">Login</a></li>
-					{/if}
-				</ul>
-			</div>
-			<button class="absolute top-1 left-[205px] btn-icon" onclick={()=>{menuOpen=false}}><XCircleIcon aria-label='close' class=''/></button>
-		</article>
-	{/snippet}
+							<form action="/logout" method="post" use:enhance>
+								<li><button class="anchor" type="submit">Logout</button></li>
+							</form>
+						{:else}
+							<li><a class="anchor" href="/login">Login</a></li>
+						{/if}
+					</ul>
+				</div>
+				<button class="absolute top-1 left-[205px] btn-icon" onclick={()=>{menuOpen=false}}><XCircleIcon aria-label='close' class=''/></button>
+			</article>
+		{/snippet}
 	</Modal>
 		<div class="bg-tertiary-50-950 fixed w-screen top-0 left-0 h-9 text-center font-bold z-40 "><a href="/">{PUBLIC_COMPANY_NAME}</a></div>
 	</header>
@@ -168,8 +180,8 @@
 			positionerJustify="justify-start"
 			positionerAlign=""
 			positionerPadding=""
-			transitionsPositionerIn={{ x: -280, duration: 400 }}
-			transitionsPositionerOut={{ x: -280, duration: 400 }}
+			transitionsPositionerIn={{ x: -125, duration: 400 }}
+			transitionsPositionerOut={{ x: -125, duration: 400 }}
 		>
 		{#snippet trigger()}
 			<Menu class='mx-2 border-2 z-50' aria-label='Main Menu'/>	
