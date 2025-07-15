2c2bce3b529c6739ee7c174a00de49231aa1f888
Eric Branson <eric.branson@gmail.com>
2025-07-15 13:40:37 -0600

    styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -176,7 +176,7 @@
 	<header> 
 		<Modal
 			bind:open={menuOpen}
-			triggerBase="btn bg-primary-50-950 hover:shadow-xl hover:border-2 border-secondary-50-950 fixed top-0 left-0 z-50 h-12 sm:h-9"
+			triggerBase="btn bg-primary-50-950 hover:shadow-xl hover:border-2 border-secondary-50-950 fixed top-0 left-0 z-50 h-14 sm:h-9"
 			contentBase="bg-surface-100-900 p-2 space-y-2 shadow-xl w-[125px] h-screen"
 			positionerJustify="justify-start"
 			positionerAlign=""
@@ -185,7 +185,7 @@
 			transitionsPositionerOut={{ x: -125, duration: 400 }}
 		>
 		{#snippet trigger()}
-			<Menu class='mx-2 border-2 z-50 h-12 sm:h-auto' aria-label='Main Menu'/>	
+			<Menu aria-label='Main Menu'/>	
 		{/snippet}
 		{#snippet content()}
 			<article class="">
