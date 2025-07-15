716a4d450763f672b6cd22cb4ee031e4434b8f60
Eric Branson <eric.branson@gmail.com>
2025-07-15 13:36:38 -0600

    init

---
+++ src/lib/demo/ExplainerModal.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+   import { Modal } from "@skeletonlabs/skeleton-svelte";
+   interface Props {
+      modalOpen: boolean
+      copy: string
+   }
+   let { modalOpen=$bindable(), copy }:Props = $props()
+</script>
+
+<Modal
+   open={modalOpen}
+   onOpenChange={(event) =>(modalOpen = event.open)}
+   contentBase='card bg-surface-100-900 p-2 space-y-4 shadow-xl max-w-screen-sm'
+   backdropClasses='backdrop-blur-lg'
+>
+   {#snippet content()}
+      {copy}
+      <button class="btn preset-tonal" onclick={()=>(modalOpen=false)}>Close</button>
+   {/snippet}
+</Modal>
