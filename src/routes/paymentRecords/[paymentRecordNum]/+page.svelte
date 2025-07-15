4f4cbe3eff0b5b69fdd67c98b09bf0b1f622a1bd
Eric Branson <eric.branson@gmail.com>
2025-07-15 14:16:11 -0600

    refactored to make more sense

--- src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
+++ src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
@@ -13,39 +13,41 @@
 
 </script>
 {#if data.paymentRecord }
-   <Header title='Payment Record Num: {data.paymentRecord}' />
-   <div class="grid grid-cols-1 m-1 sm:m-2 mt-14 sm:mt-10 mb-22 sm:mb-12 lg:mb-7 border-2 border-primary-50-950 rounded-lg w-fit">
-         {#if data.user?.employee}
-            <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes=''/>
-         {:else}
-            <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
-         {/if}
-         <div class="flex flex-col mx-2">
-         {#if data.customer}
-            {#if data.user?.employee}
-               <UserEmployee user={data.customer} classes=''/>
-            {:else}
-               <UserCustomer user={data.customer} />
-            {/if}
-            {#if data.address}
+   {#if data.customer}
+      {#if data.customer}
+         {#if data.address}         
+            <Header title='Payment Record Num: {data.paymentRecord}' />
+            <div class="m-1 sm:m-2 mt-14 sm:mt-10 mb-22 sm:mb-12 lg:mb-7 border-2 border-primary-50-950 rounded-lg">
                {#if data.user?.employee}
-                  <AddressEmployee address={data.address} />
+                  <div class="grid grid-cols-2">
+                     <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes=''/>
+                     <div>
+                        <UserEmployee user={data.customer} classes=''/>
+                        <AddressEmployee address={data.address} />
+                     </div>
+                     {#if !data.paymentRecord.refunded}
+                        <a href='/refundRecords/new?paymentNumber={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2 col-span-2">Refund this payment</a>
+                     {/if}
+                  </div>
                {:else}
-                  <AddressCustomer address={data.address} />
+                  <div class="grid grid-cols-2">
+                     <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
+                     <div>
+                        <UserCustomer user={data.customer} />
+                        <AddressCustomer address={data.address} />
+                     </div>
+                  </div>
+                  {#if data.customer.email && data.customer.emailVerified}
+                     <EmailCustomer 
+                        recordNum={data.paymentRecord.paymentNumber} 
+                        emailAddress={data.customer.email} 
+                        apiEndPoint='/api/sendReceipt'
+                        buttonText='Email Receipt'
+                     />
+                  {/if}
                {/if}
-            {/if}
-            {#if data.customer.email && data.customer.emailVerified}
-               <EmailCustomer 
-                  recordNum={data.paymentRecord.paymentNumber} 
-                  emailAddress={data.customer.email} 
-                  apiEndPoint='/api/sendReceipt'
-                  buttonText='Email Receipt'
-               />
-            {/if}
+            </div>
          {/if}
-         </div>
-         {#if !data.paymentRecord.refunded}
-            <a href='/refundRecords/new?paymentNumber={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2 mt-0">Refund this payment</a>
-         {/if}
-   </div>
+      {/if}
+   {/if}
 {/if}
