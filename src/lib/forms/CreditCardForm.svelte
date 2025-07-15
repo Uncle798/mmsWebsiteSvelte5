4a236d028072714d32805077abdb1731885bae37
Eric Branson <eric.branson@gmail.com>
2025-07-15 13:53:02 -0600

    explainer modal and tool tips

--- src/lib/forms/CreditCardForm.svelte
+++ src/lib/forms/CreditCardForm.svelte
@@ -10,11 +10,12 @@
 	import { browser } from '$app/environment';;
 	import { goto } from '$app/navigation';
    import { fade } from "svelte/transition";
-	import { ProgressRing, Progress } from "@skeletonlabs/skeleton-svelte";
+	import { ProgressRing, Progress, Tooltip } from "@skeletonlabs/skeleton-svelte";
 	import { valibot, } from "sveltekit-superforms/adapters";
 	import type { Invoice } from "@prisma/client";
    import Payment from "payment";
 	import { onMount } from "svelte";
+	import ExplainerModal from "$lib/demo/ExplainerModal.svelte";
 
    interface Props {
       data: SuperValidated<Infer<CreditCardFormSchema>>,
@@ -37,9 +38,9 @@
             let date:string = data.exp.substring(0, data.exp.indexOf(' ')) + data.exp.substring(data.exp.lastIndexOf(' ')+1);
             const paymentData = {
                ssl_txn_auth_token: sessionToken, 
-               ssl_card_number: data.ccNum,
-               ssl_exp_date: date,
-               ssl_cvv2cvc2: data.cvv,
+               ssl_card_number: '4000000000000002',
+               ssl_exp_date: '1230',
+               ssl_cvv2cvc2: '012',
                ssl_cvv2cvc2_indicator: 1,
                ssl_amount: invoice.invoiceAmount,
                ssl_avs_zip: data.postalCode,
@@ -99,8 +100,21 @@
          Payment.formatCardExpiry(ccExpElement);
       }
    })
+   let ccToolTipOpen = $state(false);
+   let expToolTipOpen = $state(false);
+   let cvvToolTipOpen = $state(false);
+   let explainerModalOpen = $state(true);
+   onMount(()=>{
+      setTimeout(()=>{
+         explainerModalOpen=false
+      }, 5000)
+   })
 </script>
 <FormMessage message={$message} />
+<ExplainerModal 
+   bind:modalOpen={explainerModalOpen}
+   copy="I've hard coded the credit card info to a test card but feel free to fill out the form"
+/>
 <form method="POST" use:enhance>
    <div class='{classes}'> 
       <TextInput
@@ -122,36 +136,65 @@
          autocomplete='cc-family-name'
       />
       <div>
-         <label for="ccNum" class="label-text">Credit card number
-            <input 
-               name="ccNum" 
-               id="ccNum" 
-               bind:this={ccNumElement} 
-               class="input" 
-               bind:value={$form.ccNum} 
-               autocomplete="cc-number"
-               {...$constraints.ccNum}
-               />
-         </label>
-         {#if $errors.ccNum}
-            <span class="invalid">{$errors.ccNum}</span>
-         {/if}
+         <Tooltip
+            open={ccToolTipOpen}
+            onOpenChange={(e) => (ccToolTipOpen = e.open)}
+            positioning={{placement: 'top-end'}}
+            contentBase="card preset-filled p-2"
+            openDelay={200}
+            zIndex='30'
+         >
+            {#snippet trigger()}               
+               <label for="ccNum" class="label-text">Credit card number
+                  <input 
+                  name="ccNum" 
+                  id="ccNum" 
+                  bind:this={ccNumElement} 
+                  class="input" 
+                  bind:value={$form.ccNum} 
+                  autocomplete="cc-number"
+                  {...$constraints.ccNum}
+                  />
+               </label>
+               {#if $errors.ccNum}
+               <span class="invalid">{$errors.ccNum}</span>
+               {/if}
+            {/snippet}
+            {#snippet content()}
+               I've hard coded a test CC number for this demo
+            {/snippet}
+         </Tooltip>
       </div>
       <div class="flex gap-2 flex-row">
          <div>
-            <label for="ccExp" class="label-text">Expiration
-               <input
-                  name="ccExp"
-                  id="ccExp"
-                  class="input"
-                  bind:this={ccExpElement}
-                  autocomplete="cc-exp"
-                  bind:value={$form.exp}
-               />
-            </label>
-            {#if $errors.exp}
-               <span class="invalid">{$errors.exp}</span>
-            {/if}
+            <Tooltip
+            open={expToolTipOpen}
+            onOpenChange={(e) => (expToolTipOpen = e.open)}
+            positioning={{placement: 'top-end'}}
+            contentBase="card preset-filled p-2"
+            openDelay={200}
+            zIndex='30'
+         >
+            {#snippet trigger()}               
+               <label for="ccExp" class="label-text">Expiration
+                  <input
+                     name="ccExp"
+                     id="ccExp"
+                     class="input"
+                     bind:this={ccExpElement}
+                     autocomplete="cc-exp"
+                     bind:value={$form.exp}
+                  />
+               </label>
+               {#if $errors.exp}
+                  <span class="invalid">{$errors.exp}</span>
+               {/if}
+            {/snippet}
+            {#snippet content()}
+               I've hard coded a test expiration date for this demo.
+            {/snippet}
+         </Tooltip>
+
          </div>
          <TextInput
             bind:value={$form.cvv}
