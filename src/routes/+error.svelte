8e82ea188c3fe519d159ffcd14d688e1d91a0370
Eric Branson <eric.branson@gmail.com>
2025-07-15 08:13:14 -0600

    styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,6 @@
 	import Header from '$lib/Header.svelte';
 </script>
 <Header title={page.error?.message ? page.error.message : ''} />
-<div class="mt-12 sm:mt-10">
+<div class="mt-12 sm:mt-10 mx-1 sm:mx-2">
     <h1>{page.status}: {page.error?.message}</h1>
 </div>
