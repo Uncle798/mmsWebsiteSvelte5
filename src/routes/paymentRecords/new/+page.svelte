a6a0852a877446ab2674864518b79d544749d5b3
Eric Branson <eric.branson@gmail.com>
2025-07-18 16:31:01 -0600

    moved create customer to form

--- src/routes/paymentRecords/new/+page.svelte
+++ src/routes/paymentRecords/new/+page.svelte
@@ -1,32 +1,14 @@
 <script lang="ts">
     import NewPaymentRecordForm from '$lib/forms/NewPaymentRecordForm.svelte';
     import type { PageData } from './$types';
-    import { Modal } from '@skeletonlabs/skeleton-svelte';
     import { fade } from 'svelte/transition';
-    import RegisterForm from '$lib/forms/RegisterForm.svelte';
 	import Header from '$lib/Header.svelte';
 
     let { data }: { data: PageData } = $props();
-    let registerFormModalOpen = $state(false);
-    let customerSelected = $state(false)
 </script>
 
-<Modal
-   open={registerFormModalOpen}
-   onOpenChange={(e) => registerFormModalOpen = e.open}
-   contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-(--breakpoint-sm)"
-   backdropClasses="backdrop-blur-xs"
->
-    {#snippet content()}
-        <RegisterForm data={data.registerForm} registerFormModalOpen={registerFormModalOpen} formType='employee'/>
-        <button class="btn" onclick={()=>registerFormModalOpen=false}>Cancel</button>
-    {/snippet}
-</Modal>
 <Header title='New Payment Record' />
 <div in:fade={{duration:600}} out:fade={{duration:0}} class="mt-14 sm:mt-10 mx-1 sm:mx-2">
-   {#if !customerSelected}
-      <button class="btn preset-filled-primary-50-950 rounded-lg" type="button" onclick={()=>registerFormModalOpen = true}>Create New Customer</button>
-   {/if}
    <NewPaymentRecordForm 
       data={data.newPaymentRecordForm} 
       invoices={data.invoices} 
@@ -34,8 +16,8 @@
       employeeId={data.user?.id}
       leases={data.leases}
       invoiceForm={data.invoiceForm}
+      registerForm={data.registerForm}
       defaultCustomer={data.defaultCustomer}
       defaultInvoice={data.defaultInvoice}
-      bind:customerSelected={customerSelected}
    />
 </div>
