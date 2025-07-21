416d8798e335fd6cbf7f4187a72ae10c16fe9e30
Eric Branson <eric.branson@gmail.com>
2025-07-21 11:08:26 -0600

    more console.log

--- src/routes/paymentRecords/+page.svelte
+++ src/routes/paymentRecords/+page.svelte
@@ -71,12 +71,16 @@
       }
       return 0
    }))
-   let dateSearchPayments = $derived((paymentRecords:PaymentRecord[]) => paymentRecords.filter((paymentRecord) => {
-      if(!startDate || !endDate){
-         return paymentRecord
-      }
-      return paymentRecord.paymentCreated >= startDate && paymentRecord.paymentCreated <= endDate;
-   }))
+   let dateSearchPayments = $derived((paymentRecords:PaymentRecord[]) => {
+      const returnedPayments = paymentRecords.filter((paymentRecord) => {
+         if(!startDate || !endDate){
+            return paymentRecord
+         }
+         return paymentRecord.paymentCreated >= startDate && paymentRecord.paymentCreated <= endDate;
+      })
+      console.log('dateSearchedPayments', returnedPayments)
+      return returnedPayments
+   })
    let nameSearch = $state('');
    let currentUsers = $derived((users:User[]) => users.filter((user) => {
       return user.givenName?.toLowerCase().includes(nameSearch.toLowerCase()) 
