2bfb25ef1784a758ae0500851a6ca1b153630795
Eric Branson <eric.branson@gmail.com>
2025-07-22 11:52:25 -0600

    added verification badge

--- src/lib/displayComponents/UserEmployee.svelte
+++ src/lib/displayComponents/UserEmployee.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import EmailVerificationForm from '$lib/forms/EmailVerificationForm.svelte';
 	import type { PartialUser } from '$lib/server/partialTypes';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+	import { Check } from 'lucide-svelte';
 
 	interface Props {
 		user: PartialUser;
@@ -13,5 +16,9 @@
 	{#if user.organizationName}
 		<span>{user.organizationName}</span>
 	{/if}
-	<span class=""><a href="mailto:{user.email}" class="anchor">{user.email}</a></span>
+	<span class=""><a href="mailto:{user.email}" class="anchor">{user.email}</a>
+	{#if user.emailVerified}
+		<Check aria-label='Email verified' color='green' />
+	{/if}
+	</span>
 </div>
