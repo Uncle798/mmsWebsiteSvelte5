910bb2ac1f993606d3624133601905bc4828d7d0
Eric Branson <eric.branson@gmail.com>
2025-07-30 14:08:12 -0600

    redoing redirect

--- src/routes/login/magicLink/[linkToken]/+page.server.ts
+++ src/routes/login/magicLink/[linkToken]/+page.server.ts
@@ -25,33 +25,33 @@ export const load = (async (event) => {
    }
    const token =  generateSessionToken();
    const session = await createSession(token, user.id!);
+   console.log('session', session)
    setSessionTokenCookie(event, token, session.expiresAt);
    const redirectTo = event.url.searchParams.get('redirectTo');
    const unitNum = event.url.searchParams.get('unitNum');
    const invoiceNum = event.url.searchParams.get('invoiceNum');
    const paymentRecordNum = event.url.searchParams.get('paymentRecordNum');
-   // switch (redirectTo) {
-   //    case 'home':
-   //       redirect(303, '/');
-   //    case 'newLease':
-   //       redirect(302, `/newLease?unitNum=${unitNum}`);
-   //    case 'units':
-   //       if(unitNum){
-   //          redirect(302, `/units/${unitNum}`);
-   //       }
-   //       redirect(302, '/units');
-   //    case 'invoice':
-   //       if(invoiceNum){
-   //          redirect(302, `/invoices/${invoiceNum}`);
-   //       }
-   //       redirect(302, `/invoices`);
-   //    case 'paymentRecord':
-   //       if(paymentRecordNum){
-   //          redirect(302, `/paymentRecords/${paymentRecordNum}`);
-   //       }
-   //       redirect(302, '/paymentRecords');
-   //    default:
-   //       redirect(302, '/');
-   // }
-   return {}
+   switch (redirectTo) {
+      case 'home':
+         redirect(303, '/');
+      case 'newLease':
+         redirect(302, `/newLease?unitNum=${unitNum}`);
+      case 'units':
+         if(unitNum){
+            redirect(302, `/units/${unitNum}`);
+         }
+         redirect(302, '/units');
+      case 'invoice':
+         if(invoiceNum){
+            redirect(302, `/invoices/${invoiceNum}`);
+         }
+         redirect(302, `/invoices`);
+      case 'paymentRecord':
+         if(paymentRecordNum){
+            redirect(302, `/paymentRecords/${paymentRecordNum}`);
+         }
+         redirect(302, '/paymentRecords');
+      default:
+         redirect(302, '/');
+   }
 }) satisfies PageServerLoad;
