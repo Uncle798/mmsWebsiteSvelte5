f4c4aa2f1b000652f02e22b78a93fca3e576df95
Eric Branson <eric.branson@gmail.com>
2025-07-31 11:41:12 -0600

    styling

--- src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
+++ src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
@@ -20,12 +20,12 @@
             <div class="m-1 sm:m-2 mt-14 sm:mt-10 mb-22 sm:mb-12 lg:mb-7 border-2 border-primary-50-950 rounded-lg">
                {#if data.user?.employee}
                   <div class="flex flex-col sm:flex-row">
-                     <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes='w-1/2'/>
+                     <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes='sm:w-1/2'/>
                      <div class="m-2">
                         <UserEmployee user={data.customer} classes='truncate'/>
                         <AddressEmployee address={data.address} />
                         {#if !data.paymentRecord.refunded}
-                           <a href='/refundRecords/new?paymentNum={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2 col-span-2 h-8">Refund this payment</a>
+                           <a href='/refundRecords/new?paymentNum={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 my-2 h-8">Refund this payment</a>
                         {/if}
                      </div>
                   </div>
