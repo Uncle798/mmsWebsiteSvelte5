309364a3d2b84116067a4f4bed6a01539e5b3d59
Eric Branson <eric.branson@gmail.com>
2025-08-08 11:55:24 -0600

    init

---
+++ src/routes/api/users/+server.ts
@@ -0,0 +1,60 @@
+import { prisma } from '$lib/server/prisma';
+import type { RequestHandler } from './$types';
+
+export const DELETE: RequestHandler = async (event) => {
+   if(!event.locals.user?.admin){
+      return new Response(null, {status: 401});
+   }
+   const body = await event.request.json();
+   const { userId } = body;
+   if(!userId){
+      return new Response(JSON.stringify('User ID not provided'), { status:400 });
+   }
+   const user = await prisma.user.findUnique({
+      where: {
+         id: userId
+      }
+   })
+   await prisma.verification.deleteMany({
+      where: {
+         userId: user?.id
+      }
+   })
+   await
+   await prisma.session.deleteMany({
+      where: {
+         userId: user?.id
+      }
+   });
+   await prisma.refundRecord.deleteMany({
+      where: {
+         customerId: user?.id
+      }
+   })
+   await prisma.paymentRecord.deleteMany({
+      where: {
+         customerId: user?.id
+      }
+   })
+   await prisma.invoice.deleteMany({
+      where: {
+         customerId: user?.id
+      }
+   })
+   await prisma.lease.deleteMany({
+      where: {
+         customerId: user?.id
+      }
+   })
+   await prisma.address.deleteMany({
+      where: {
+         userId: user?.id
+      }
+   })
+   await prisma.user.deleteMany({
+      where: {
+         id: user?.id
+      }
+   })
+   return new Response(JSON.stringify('User deleted'), { status:200 });
+};
