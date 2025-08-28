cf9696352641c3e421bd8e73d6ca174b971c28ed
Eric Branson <eric.branson@gmail.com>
2025-08-28 11:16:26 -0600

    lease ending

--- src/routes/leases/[leaseId]/+page.server.ts
+++ src/routes/leases/[leaseId]/+page.server.ts
@@ -2,12 +2,15 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 import type { Address } from '@prisma/client';
+import { superValidate } from 'sveltekit-superforms';
+import { valibot } from 'sveltekit-superforms/adapters';
+import { leaseEndFormSchema } from '$lib/formSchemas/schemas';
 
 export const load = (async (event) => {
    if(!event.locals.user){
       redirect(302, '/login?toast=unauthorized')
    }
-   const leaseId = event.params.leaseId
+   const leaseId = event.params.leaseId;
    const lease = await prisma.lease.findUnique({
       where: {
          leaseId,
@@ -16,6 +19,7 @@ export const load = (async (event) => {
    if(!event.locals.user.employee && lease?.customerId !== event.locals.user.id){
       redirect(302, '/login?toast=employee')
    }
+   const leaseEndForm = await superValidate(valibot(leaseEndFormSchema));
    const customer = await prisma.user.findUnique({
       where: {
          id: lease?.customerId
@@ -34,5 +38,5 @@ export const load = (async (event) => {
          }
       })
    }
-   return { lease, customer, address, currentAddress };
+   return { lease, customer, address, currentAddress, leaseEndForm };
 }) satisfies PageServerLoad;
--- src/routes/leases/[leaseId]/+page.svelte
+++ src/routes/leases/[leaseId]/+page.svelte
@@ -7,15 +7,40 @@
    import Header from '$lib/Header.svelte';
 	import { fade } from 'svelte/transition';
    import type { PageData } from './$types';
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
+   import LeaseEndForm from '$lib/forms/LeaseEndForm.svelte';
 
    let { data }: { data: PageData } = $props();
+   let modalOpen = $state(false);
 </script>
-
 <Header title='Lease {data.lease?.leaseId}' />
+{#if  data.lease}   
+   <Modal
+      open={modalOpen}
+      onOpenChange={(event) => 
+         modalOpen = event.open
+      }
+      contentBase="card bg-surface-400-600 p-4 space-y-4 shadow-xl max-w-(--breakpoint-sm)"
+      backdropClasses="backdrop-blur-xs"
+   >
+      {#snippet content()}
+         <LeaseEndForm data={data.leaseEndForm} leaseId={data.lease!.leaseId} bind:leaseEndModalOpen={modalOpen}/>
+         <button type="button" class="btn preset-filled-primary-50-950" onclick={()=> modalOpen = false}>Close</button>
+      {/snippet}
+   </Modal>
+{/if}
 {#if data.user?.employee}   
    <div class="grid sm:grid-cols-2 m-1 sm:m-2 border-2 border-primary-50-950 rounded-lg mt-14 sm:mt-10">
       {#if data.lease}
-         <LeaseEmployee lease={data.lease} classes='border-b border-primary-50-950'/>
+         <div>
+            <LeaseEmployee lease={data.lease} classes='border-b border-primary-50-950'/>
+            {#if !data.lease.leaseEnded}
+            <button type="button" class="btn preset-filled-primary-50-950 m-2" onclick={()=>{
+               modalOpen = true;
+            }}
+            >End Lease</button>
+            {/if}
+         </div>
       {/if}
       <div class="m-1 sm:m-2">
          {#if data.customer}
