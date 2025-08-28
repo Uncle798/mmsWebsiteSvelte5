05ec771c4c8d2987c593c3b58b14160e451a9f3e
Eric Branson <eric.branson@gmail.com>
2025-08-28 11:15:42 -0600

    styling

--- src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
+++ src/routes/paymentRecords/[paymentRecordNum]/+page.svelte
@@ -20,23 +20,28 @@
             <div class="m-1 sm:m-2 mt-14 sm:mt-10 mb-22 sm:mb-12 lg:mb-7 border-2 border-primary-50-950 rounded-lg">
                {#if data.user?.employee}
                   <div class="flex flex-col sm:flex-row">
-                     <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes='sm:w-1/2'/>
+                     <div class="sm:w-1/2">
+                        <PaymentRecordEmployee paymentRecord={data.paymentRecord} classes=''/>
+                        <div class="flex">
+                           {#if data.customer.email && data.customer.emailVerified}
+                              <EmailCustomer 
+                                 recordNum={data.paymentRecord.paymentNumber} 
+                                 emailAddress={data.customer.email} 
+                                 apiEndPoint='/api/sendReceipt'
+                                 buttonText='Email Receipt'
+                                 classes=''
+                              />
+                           {/if}
+                           {#if !data.paymentRecord.refunded}
+                              <a href='/refundRecords/new?paymentNum={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 m-2">Refund this payment</a>
+                           {/if}
+                        </div>
+                     </div>
                      <div class="m-2">
                         <UserEmployee user={data.customer} classes='truncate'/>
                         <AddressEmployee address={data.address} />
-                        {#if !data.paymentRecord.refunded}
-                           <a href='/refundRecords/new?paymentNum={data.paymentRecord.paymentNumber}' class="btn rounded-lg preset-filled-primary-50-950 my-2 h-8">Refund this payment</a>
-                        {/if}
                      </div>
                   </div>
-                  {#if data.customer.email && data.customer.emailVerified}
-                     <EmailCustomer 
-                        recordNum={data.paymentRecord.paymentNumber} 
-                        emailAddress={data.customer.email} 
-                        apiEndPoint='/api/sendReceipt'
-                        buttonText='Email Receipt'
-                     />
-                  {/if}
                {:else}
                   <div class="grid grid-cols-1 sm:grid-cols-2">
                      <PaymentRecordCustomer paymentRecord={data.paymentRecord} />
