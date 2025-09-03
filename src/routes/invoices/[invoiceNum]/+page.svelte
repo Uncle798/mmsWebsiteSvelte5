f2c1252c60ea2d29300c332a30d2cab71f1c9a4e
Eric Branson <eric.branson@gmail.com>
2025-09-03 11:21:40 -0600

    cleanup

--- src/routes/invoices/[invoiceNum]/+page.svelte
+++ src/routes/invoices/[invoiceNum]/+page.svelte
@@ -11,31 +11,33 @@
    let { data }: { data: PageData } = $props();
 </script>
 {#if data.user?.employee}
-    {#if data.invoice}      
-        <Header title="Invoice number {data.invoice.invoiceNum}" />
-        <div class="flex flex-col sm:flex-row gap-x-1 mx-1 sm:mx-2 mt-14 sm:mt-10 border-2 border-primary-50-950 rounded-lg">
-            <InvoiceEmployee invoice={data.invoice} classes="min-w-64 " />
-            <div class="flex flex-col min-w-64"> 
-               {#if data.customer}
-                  <UserEmployee user={data.customer} classes="px-2 pt-2" />
-               {/if}
-               {#if data.address}
-                  <Address address={data.address} classes='px-2'/>
-               {/if}
-               {#if !data.invoice.paymentRecordNum}
-                  <a href="/paymentRecords/new?defaultInvoice={data.invoice.invoiceNum}" class="btn preset-filled-primary-50-950 rounded-lg m-1">Make a payment record for this invoice</a>
-               {/if}
-            </div>
+   {#if data.invoice}      
+      <Header title="Invoice number {data.invoice.invoiceNum}" />
+      <div class="flex flex-col sm:flex-row gap-x-1 mx-1 sm:mx-2 mt-14 sm:mt-10 border-2 border-primary-50-950 rounded-lg">
+         <div class="flex flex-col">
+         <InvoiceEmployee invoice={data.invoice} classes="min-w-64 " />
             {#if data.customer?.email && data.customer?.emailVerified}         
-                <EmailCustomer
-                    recordNum={data.invoice.invoiceNum}
-                    apiEndPoint='/api/sendInvoice'
-                    emailAddress={data.customer?.email}
-                    buttonText='Send invoice'
-                />
+               <EmailCustomer
+                  recordNum={data.invoice.invoiceNum}
+                  apiEndPoint='/api/sendInvoice'
+                  emailAddress={data.customer?.email}
+                  buttonText='Send invoice'
+               />
             {/if}
-        </div>
-    {/if}
+            {#if !data.invoice.paymentRecordNum}
+               <a href="/paymentRecords/new?invoiceNum={data.invoice.invoiceNum}" class="btn rounded-lg preset-filled-primary-50-950 mx-2 mb-2 w-84">Make a payment record for this invoice</a>
+            {/if}
+         </div>
+         <div class="flex flex-col min-w-64"> 
+            {#if data.customer}
+               <UserEmployee user={data.customer} classes="px-2 pt-2" />
+            {/if}
+            {#if data.address}
+               <Address address={data.address} classes='px-2'/>
+            {/if}
+         </div>
+      </div>
+   {/if}
 {:else}
    {#if data.invoice}
       {#if data.user?.id === data.customer?.id}         
@@ -58,7 +60,7 @@
                      classes='m-1 sm:m-2'
                   />
                {/if}
-         </div>
+            </div>
          </div>
       {/if}
    {/if}
