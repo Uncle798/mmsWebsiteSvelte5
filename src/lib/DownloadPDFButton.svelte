233895359855fd634a8a1a9163449ce0961bfd1e
Eric Branson <eric.branson@gmail.com>
2025-09-11 13:46:59 -0600

    init

---
+++ src/lib/DownloadPDFButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+   interface Props {
+      recordType: 'refundNum' | 'paymentNum' | 'invoiceNum';
+      num: number
+      classes?: string
+   }
+   let { recordType, num, classes }:Props=$props();
+</script>
+<div class={classes}>
+   <a href="/api/downloadPDF?{recordType}={num.toString()}" 
+      class="btn preset-filled-primary-50-950 h-8" 
+      target="_blank"
+   >
+      Download PDF
+   </a>
+</div>
