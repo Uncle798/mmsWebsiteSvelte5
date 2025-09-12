5c93bf9d710173038742881027e4fa76a339e96f
Eric Branson <eric.branson@gmail.com>
2025-09-12 16:09:06 -0600

    styling cleanup moved if then block

--- src/routes/units/[unitNum]/+page.svelte
+++ src/routes/units/[unitNum]/+page.svelte
@@ -35,45 +35,34 @@
 </Modal>
    {#if unit}
       <Header title='Unit number: {unit.num}' />
-      <Revenue label='Total revenue from this unit' amount={data.totalRevenue} classes="flex sticky top-9 bg-tertiary-50-950 rounded-b-lg w-full p-2"/>
-      <div transition:fade={{duration:600}} class="m-1 sm:m-2 pt-8 mb-20 sm:mb-12 lg:mb-8">
-         <UnitEmployee {unit} classes=''/>
-         <a href="/employeeNewLease?unitNum={unit.num}" class="btn preset-filled-primary-50-950 m-2 rounded-lg">Rent Unit {unit.num.replace(/^0+/gm, '')}</a>
+      <Revenue label='Total revenue from this unit' amount={data.totalRevenue} classes="flex sticky top-10 bg-tertiary-50-950 rounded-b-lg w-full p-2"/>
+      <div transition:fade={{duration:600}} class="m-1 sm:m-2 mt-12 mb-8">
+         <div class="border-2 border-primary-50-950 rounded-lg">
+            <UnitEmployee {unit} classes='mx-2'/>
+            <a href="/employeeNewLease?unitNum={unit.num}" class="btn preset-filled-primary-50-950 m-2 rounded-lg">Rent Unit {unit.num.replace(/^0+/gm, '')}</a>
+         </div>
          <UnitNotesForm data={data.unitNotesForm} {unit} classes=''/>
          <UnitPricingForm data={data.unitPricingForm} size={unit.size} oldPrice={unit.advertisedPrice} unitPricingFormModalOpen={modalOpen} />
          <div class="grid grid-cols-1 gap-y-2 gap-x-1">
             {#each data.leases as lease}
             {@const customer = data.customers.find((customer) => customer.id === lease.customerId)}
-               {#if !lease.leaseEnded}
-                  <div class="rounded-lg border-2 border-primary-50-950">
-                     <div class="flex flex-col ">
-                        <LeaseEmployee {lease}/>
+               <div class="rounded-lg border-2 border-primary-50-950 ">
+                  <div class="flex flex-col sm:flex-row">
+                     <LeaseEmployee {lease}/>
+                     {#if !lease.leaseEnded}
                         <button class="btn rounded-lg preset-filled-primary-50-950 mx-2" onclick={()=>{modalOpen=true; currentLeaseId=lease.leaseId}}>End lease</button>
-                     </div>                 
-                     {#if customer}  
-                     {@const address = data.addresses.find((address) => address.userId === customer.id)}
+                     {/if}
+                  </div>                 
+                  {#if customer}  
+                  {@const address = data.addresses.find((address) => address.userId === customer.id)}
                      <div class="flex flex-col p-2">
                         <UserEmployee user={customer}/>
                         {#if address}
-                        <Address {address} />
+                           <Address {address} />
                         {/if}
                      </div>
-                     {/if}
-                  </div>
-               {:else}
-                  <div class="rounded-lg border-2 border-primary-50-950">
-                     <LeaseEmployee {lease} classes=''/>
-                     {#if customer}
-                     {@const address = data.addresses.find((address) => address.userId === customer.id)}
-                        <div class="flex flex-col sm:p-2 px-2 ">
-                           <UserEmployee user={customer}/>
-                           {#if address}
-                                 <Address {address} />
-                           {/if}
-                        </div>
-                     {/if}
-                  </div>
-               {/if}
+                  {/if}
+               </div>
             {/each}
          </div>
       </div>
