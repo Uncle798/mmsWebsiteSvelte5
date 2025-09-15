1d283eee4c45ae59e237f60d92483a35579fb739
Eric Branson <eric.branson@gmail.com>
2025-09-15 09:45:36 -0600

    Fixed case better typing for apiEndPoint

---
+++ src/lib/EmailCustomer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+   interface Props {
+      recordNum: number;
+      apiEndPoint: '/api/sendReceipt' | '/api/sendInvoice' | '/api/sendRefund';
+      emailAddress: string;
+      buttonText: string;
+      classes?: string
+   }
+   let { recordNum, apiEndPoint, emailAddress, buttonText, classes }:Props = $props();
+   let emailing = $state(false);
+   let emailed = $state(false);
+   async function sendEmail(recordNum:number) {
+      emailing = true;
+      const res = await fetch(apiEndPoint, {
+         method: "POST",
+         body: JSON.stringify({recordNum})
+      })
+      const body = await res.json();
+      if(body.success){
+         emailed = true;
+      }
+      setTimeout(()=>{
+         emailing = false
+         emailed = false
+      }, 5000)
+   }
+</script>
+<div class={classes}>
+   {#if emailed}
+      <div class="m-2 h-8 truncate">An email has been sent to {emailAddress}</div>
+   {:else if emailing}
+      <div class="m-2 h-8">Sending email...</div>
+   {:else if !emailing}
+      <button class="btn rounded-lg preset-filled-primary-50-950 h-8" onclick={()=>sendEmail(recordNum)}>{buttonText}</button>
+   {/if}
+</div>
