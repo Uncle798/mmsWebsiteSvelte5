8bd45bdb95d4b67b3b785bd2a9b9c7624de8d34a
Eric Branson <eric.branson@gmail.com>
2025-09-15 10:37:09 -0600

    added path

--- src/routes/api/demoSetCookie/+server.ts
+++ src/routes/api/demoSetCookie/+server.ts
@@ -3,7 +3,9 @@ import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async (event) => {
    const demoPage = event.url.searchParams.get('demoPage');
    if(demoPage){
-      event.cookies.set(`${demoPage}Complete`, 'true', {path:demoPage, maxAge:60*60*24})
+      let path=demoPage.replace(/(?<!^)(?=[A-Z])/, '/');
+      console.log(path)
+      event.cookies.set(`${demoPage}Complete`, 'true', {path:'/' + path?.toLowerCase(), maxAge:60*60*24})
    }
    return new Response(null, {status: 200});
 };
