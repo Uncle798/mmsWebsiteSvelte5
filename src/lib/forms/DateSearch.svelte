8baa646f84ff77f820adcfb2b7ca599dfd2d0566
Eric Branson <eric.branson@gmail.com>
2025-09-21 11:25:05 -0600

    styling

--- src/lib/forms/DateSearch.svelte
+++ src/lib/forms/DateSearch.svelte
@@ -48,7 +48,7 @@
 					max={maxDate}
 				/>
 				<button
-					class="btn rounded-lg preset-filled-primary-50-950"
+					class="btn rounded-lg preset-filled-primary-50-950 h-8 my-1"
 					type="button"
 					onclick={() => {
 						$form.startDate = undefined;
@@ -67,7 +67,7 @@
 					max={maxDate}
 				/>
 				<button
-					class="btn preset-filled-primary-50-950 rounded-lg"
+					class="btn preset-filled-primary-50-950 rounded-lg h-8 my-1"
 					type="button"
 					onclick={() => {
 						$form.endDate = undefined;
