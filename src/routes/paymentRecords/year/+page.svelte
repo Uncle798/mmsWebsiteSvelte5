2a04138d99917e2a2d313684b46134965db0d585
Eric Branson <eric.branson@gmail.com>
2025-09-28 11:36:49 -0600

    navDelayed and navTimeout

--- src/routes/paymentRecords/year/+page.svelte
+++ src/routes/paymentRecords/year/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-   import { goto } from '$app/navigation';
+   import { goto, onNavigate } from '$app/navigation';
    import type { PageData } from './$types';
-   import {Combobox } from '@skeletonlabs/skeleton-svelte'
+   import {Combobox, Progress, ProgressRing } from '@skeletonlabs/skeleton-svelte'
    let { data }: { data: PageData } = $props();
    let yearSelect = $state(['']);
    interface ComboboxData {
@@ -12,8 +12,14 @@
       label: year.toString(),
       value: year.toString()
    }))) 
+   let navDelayed = $state(false);
+   let navTimeout = $state(false);
+   onNavigate(() => {
+      navDelayed = false;
+      navTimeout = false;
+   })
 </script>
-<div class="mx-1 sm:mx-2 mt-14 sm:mt-10 ">
+<div class="mx-1 sm:mx-2 mt-14 sm:mt-10 flex flex-row w-screen ">
    {#if data.years}
       <Combobox
          data={yearComboboxData}
@@ -22,9 +28,36 @@
          placeholder='Select year...'
          openOnClick={true}
          onValueChange={(details) => {
+            setTimeout(() => {
+               navDelayed = true
+            }, 300);
             goto(`/paymentRecords/year/${details.value[0]}`)
          }}
          zIndex='50'
+         width='w-11/12'
+      />
+   {/if}
+   {#if navDelayed}
+      <ProgressRing  
+         value={null} 
+         size="size-8" 
+         meterStroke="stroke-tertiary-600-400" 
+         trackStroke="stroke-tertiary-50-950"
+         classes='mt-6 mx-2'
+         {@attach () => {
+            setTimeout(() => {
+               navDelayed = false;
+               navTimeout = true;
+            }, 800)
+         }}
+      />
+   {/if}
+   {#if navTimeout}
+      <Progress 
+         value={null}
+         meterBg="bg-tertiary-500"
+         width='w-12'
+         classes='mt-9 mx-2'
       />
    {/if}
 </div>
