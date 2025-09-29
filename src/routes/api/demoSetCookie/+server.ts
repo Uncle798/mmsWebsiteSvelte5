f9666fc3c9ecebeb3f5a6ade812d0736401bb1f7
Eric Branson <eric.branson@gmail.com>
2025-09-28 18:08:51 -0600

    home

--- src/routes/api/demoSetCookie/+server.ts
+++ src/routes/api/demoSetCookie/+server.ts
@@ -4,6 +4,9 @@ export const GET: RequestHandler = async (event) => {
    const demoPage = event.url.searchParams.get('demoPage');
    if(demoPage){
       let path=demoPage.replace(/(?<!^)(?=[A-Z])/, '/');
+      if(demoPage === 'home'){
+         path = '';
+      }
       console.log(path)
       event.cookies.set(`${demoPage}Complete`, 'true', {path:'/' + path?.toLowerCase(), maxAge:60*60*24})
    }
