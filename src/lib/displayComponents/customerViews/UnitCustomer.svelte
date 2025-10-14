f33a81c7673f8c41e609dd8da7c584e30b840b39
Eric Branson <eric.branson@gmail.com>
2025-10-14 10:36:20 -0600

    no more tooltip

--- src/lib/displayComponents/customerViews/UnitCustomer.svelte
+++ src/lib/displayComponents/customerViews/UnitCustomer.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
-   import type { Unit, Lease } from '@prisma/client';
+   import type { Unit } from '@prisma/client';
 	import HorizontalDivider from '../HorizontalDivider.svelte';
-	import { Tooltip } from '@skeletonlabs/skeleton-svelte';
    interface Props {
       unit: Unit,
       classes?: string;
    }
    let { unit, classes }:Props = $props();
    const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
-   let unitCustomerDescriptionTooltipOpen = $state(false)
 </script>
 
 <div class="grid grid-cols-2 gap-x-2 {classes}">
@@ -26,20 +24,6 @@
    <div>{currencyFormatter.format(unit.deposit)}</div>
    <HorizontalDivider classes='col-span-2'/>
    <div class="text-right col-start-1">Description</div>
-   <Tooltip
-      open={unitCustomerDescriptionTooltipOpen}
-      onOpenChange={(e) => unitCustomerDescriptionTooltipOpen = e.open}
-      positioning={{placement: 'top-end'}}
-      contentBase="card preset-filled p-2"
-      openDelay={200}
-      zIndex='30'
-   >
-      {#snippet trigger()}
-         <div class="font-medium text-left text-wrap">{unit.description}</div>
-      {/snippet}
-      {#snippet content()}
-         We can customize this description.
-      {/snippet}
-   </Tooltip>
+   <div class="font-medium text-left text-wrap pr-0.5">{unit.description}</div>
    
 </div>
