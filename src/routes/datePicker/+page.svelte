36c573fa0555651549baa424da716f609b360f39
Eric Branson <eric.branson@gmail.com>
2025-10-14 10:39:35 -0600

    towards a working date picker

--- src/routes/datePicker/+page.svelte
+++ src/routes/datePicker/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import DatePicker from '$lib/formComponents/DatePicker.svelte';
-	import DateSearchFrom from '$lib/forms/DateSearchFrom.svelte';
+	import DateSearchFrom from '$lib/forms/DateSearchForm.svelte';
 	import dayjs from 'dayjs';
    import type { PageProps } from './$types';
 
    let { data }: PageProps = $props();
+   let startDate = $state(dayjs('01/01/2018').toDate());
+   let endDate = $state(new Date());
 </script>
 <div class="mt-10 mb-8 mx-2">
-   <DateSearchFrom data={data.dateSearchForm} startDate={dayjs('01/01/2018').toDate()} endDate={new Date()} />
+   <DateSearchFrom data={data.dateSearchForm} bind:startDate={startDate} bind:endDate={endDate} minDate={dayjs('01/01/2018').toDate()} maxDate={new Date()}/>
+</div>
+<div class="mx-2">
+   start date: {startDate}
+   end date: {endDate}
 </div>
