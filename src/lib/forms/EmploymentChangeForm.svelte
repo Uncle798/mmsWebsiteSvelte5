58ba6cf3cb93862e3bf17efb1e19b30170e8f54d
Eric Branson <eric.branson@gmail.com>
2025-10-21 12:29:42 -0600

    towards admin triggering employee

--- src/lib/forms/EmploymentChangeForm.svelte
+++ src/lib/forms/EmploymentChangeForm.svelte
@@ -13,18 +13,38 @@
       classes?: string;
    }
    let { data,  employeeChecked=$bindable(false), adminChecked=$bindable(false), userId, classes }:Props = $props();
-   let { message, enhance, delayed, timeout, } = superForm(data, {
-
-      warnings:{
-         duplicateId: false
+   let { form, message, enhance, delayed, timeout, submit, } = superForm(data, {
+      id: userId,
+      onSubmit: (input) => {
+         console.log(input.formData)
       }
    });
 </script>
 <div class="my-2 {classes}">
    <FormMessage message={$message} />
    <form action="/forms/employmentChangeForm" method="POST" use:enhance>
-      <Switch name='employee' checked={employeeChecked} label='Employee'/>
-      <Switch name='admin' checked={adminChecked} label='Admin'/>
+      <Switch 
+         name='employee' 
+         checked={employeeChecked} 
+         label='Employee' 
+         onCheckedChange={(e) => {
+            employeeChecked = e.checked;
+            $form.employee = e.checked; 
+            submit();
+         }}
+      />
+      <Switch 
+         name='admin' 
+         checked={adminChecked} 
+         label='Admin' 
+         onCheckedChange={(e) => {
+            adminChecked = e.checked; 
+            employeeChecked = e.checked;
+            $form.admin = e.checked;
+            $form.employee = e.checked;
+            submit();
+         }}
+      />
       <input type="hidden" name='userId' value={userId} />
       <FormSubmitWithProgress delayed={$delayed} timeout={$timeout} buttonText="Change employment status"/>
    </form>
