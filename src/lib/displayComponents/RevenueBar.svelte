f02aa4567d275c9944e88933473f9ad6e7e60e73
Eric Branson <eric.branson@gmail.com>
2025-10-27 11:40:21 -0600

    init

---
+++ src/lib/displayComponents/RevenueBar.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	import type { Snippet } from "svelte";
+
+interface Props {
+   content: Snippet;
+   classes?: string; 
+}
+let { content, classes }:Props = $props()
+</script>
+
+<div class="bg-tertiary-50-950 w-screen rounded-b-lg fixed top-10 sm:top-7 p-1 flex z-30 {classes}">
+   {@render content()}
+</div>
