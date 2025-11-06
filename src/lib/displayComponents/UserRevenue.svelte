ba35ade7c21fd8a73d10df67752e6d25fcbf9454
Eric Branson <eric.branson@gmail.com>
2025-11-06 11:27:37 -0700

    better link

--- src/lib/displayComponents/UserRevenue.svelte
+++ src/lib/displayComponents/UserRevenue.svelte
@@ -1,22 +1,26 @@
 <script lang='ts'>
-	import type { User } from "@prisma/client";
+	import type { User, Invoice } from "@prisma/client";
 
    interface Props {
       totalInvoiced: number;
       totalPaid: number;
       overdueAmount: number;
       customer: User;
+      invoices?: Invoice[];
       classes?: string; 
    }
-   let { totalInvoiced, totalPaid, overdueAmount, customer, classes }:Props = $props();
+   let { totalInvoiced, totalPaid, overdueAmount, customer, classes, invoices }:Props = $props();
 
    let currencyFormatter = Intl.NumberFormat('EN-US', {style: 'currency', currency: 'USD'});
 </script>
 <div class="flex flex-col sm:flex-row gap-2 bg-primary-contrast-100-900 p-2 rounded-lg {classes}">
    <p class="text-error-400-600">Total invoiced: {currencyFormatter.format(totalInvoiced)}</p>
    <p class="text-success-300-700">Total paid: {currencyFormatter.format(totalPaid)}</p>
-   {#if overdueAmount > 1}
+   {#if overdueAmount > 0 && invoices?.length === 1}
+      <p class="text-error-100-900">Overdue amount: <a href="/paymentRecords/new?userId={customer.id}&invoiceNum={invoices[0].invoiceNum}">{currencyFormatter.format(overdueAmount)}</a></p>
+   {:else if overdueAmount > 0}
       <p class="text-error-100-900">Overdue amount: <a href="/paymentRecords/new?userId={customer.id}">{currencyFormatter.format(overdueAmount)}</a></p>
+
    {:else}
       <p class="text-success-300-700">Overdue amount: {currencyFormatter.format(0.0)}</p>
    {/if}
