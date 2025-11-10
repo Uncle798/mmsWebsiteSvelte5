e5bcec58d1311f0cbf9a80a2399c6b06f6823b43
Eric Branson <eric.branson@gmail.com>
2025-11-10 14:05:06 -0700

    optional names

--- src/lib/formSchemas/nameFormSchema.ts
+++ src/lib/formSchemas/nameFormSchema.ts
@@ -1,8 +1,8 @@
 import * as v from 'valibot'
 
 export const nameFormSchema = v.object({
-   familyName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
-   givenName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
+   familyName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
+   givenName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
    organizationName: v.nullable(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
 });
 export type NameFormSchema = typeof nameFormSchema;
