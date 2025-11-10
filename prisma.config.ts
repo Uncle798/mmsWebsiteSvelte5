78fe46f36fc9ed9873dc16b3d7c2b9a685380cec
Eric Branson <eric.branson@gmail.com>
2025-11-10 12:57:49 -0700

    import dotenv

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
+import "dotenv/config"
 import path from "node:path";
-import type { PrismaConfig } from 'prisma';
+import type { PrismaConfig, } from 'prisma';
 
 export default {
    schema: path.join('prisma', 'schema.prisma'),
    migrations: {
       seed: 'tsx prisma/demoSeed.ts'
-   }
+   },
 } satisfies PrismaConfig
