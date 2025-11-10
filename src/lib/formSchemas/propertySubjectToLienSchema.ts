fc5ff3df5a05a3bf22087142aa7153e83d169629
Eric Branson <eric.branson@gmail.com>
2025-11-10 14:05:25 -0700

    correct merging

--- src/lib/formSchemas/propertySubjectToLienSchema.ts
+++ src/lib/formSchemas/propertySubjectToLienSchema.ts
@@ -5,7 +5,7 @@ import { addressFormSchema } from './addressFormSchema';
 
 export const propertySubjectToLienSchema = v.object({
    description: v.string(),
-   nameFormSchema,
-   addressFormSchema,
+   ...nameFormSchema.entries,
+   ...addressFormSchema.entries,
 });
 export type PropertySubjectToLienSchema = typeof propertySubjectToLienSchema;
