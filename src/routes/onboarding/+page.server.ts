2dd138ecf3cd258620bc9eeddc3ecfa76754d3ae
Eric Branson <eric.branson@gmail.com>
2025-11-11 12:00:30 -0700

    property with lien

--- src/routes/onboarding/+page.server.ts
+++ src/routes/onboarding/+page.server.ts
@@ -5,8 +5,9 @@ import { valibot } from 'sveltekit-superforms/adapters';
 import { onboardingExistingLeaseSchema } from '$lib/formSchemas/onboardingExistingLeaseSchema';
 import { registerFormSchema } from '$lib/formSchemas/registerFormSchema';
 import { addressFormSchema } from '$lib/formSchemas/addressFormSchema';
-import type { Address, User, Lease } from '@prisma/client';
+import type { Address, User, Lease, PropertyWithLien } from '@prisma/client';
 import { propertySubjectToLienSchema } from '$lib/formSchemas/propertySubjectToLienSchema';
+import { alternativeContactFormSchema } from '$lib/formSchemas/alternativeContactFormSchema';
 
 export const load = (async (event) => {
    const userId = event.url.searchParams.get('userId');
@@ -48,13 +49,42 @@ export const load = (async (event) => {
       })
    }
    let lease:Lease | null = null;
+   let properties:PropertyWithLien[] | null = null;
+   let lienHolderAddresses:Address[] = [];
+   const lienHolderContacts:User[] = [];
    if(leaseId){
       lease = await prisma.lease.findUnique({
          where: {
             leaseId
          }
       })
+      properties = await prisma.propertyWithLien.findMany({
+         where: {
+         leaseId,
+         },
+      });
+      if(properties){
+         for(const property of properties){
+            const address = await prisma.address.findUnique({
+               where: {
+                  addressId: property.addressId
+               }
+            })
+            if(address){
+               lienHolderAddresses.push(address)
+            }
+            const contact = await prisma.user.findUnique({
+               where: {
+                  id: property.userId
+               }
+            })
+            if(contact){
+               lienHolderContacts.push(contact)
+            }
+         }
+      }
    }
+   const alternativeContactForm = await superValidate(valibot(alternativeContactFormSchema));
    let propertySubjectToLienForm = null;
    console.log(lien);
    if(lien === 'true'){
@@ -68,9 +98,13 @@ export const load = (async (event) => {
       customer, 
       address, 
       lease, 
+      properties,
+      lienHolderAddresses,
+      lienHolderContacts,
       registerForm, 
       addressForm, 
       onboardingExistingLeaseForm, 
-      propertySubjectToLienForm
+      propertySubjectToLienForm,
+      alternativeContactForm
    };
 }) satisfies PageServerLoad;
