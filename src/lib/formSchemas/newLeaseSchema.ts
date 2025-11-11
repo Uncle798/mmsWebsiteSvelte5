2a97777284392c6f5048bae3a31b9384c6c006cf
Eric Branson <eric.branson@gmail.com>
2025-11-11 12:51:37 -0700

    optional alternative contact

--- src/lib/formSchemas/newLeaseSchema.ts
+++ src/lib/formSchemas/newLeaseSchema.ts
@@ -6,6 +6,7 @@ export const newLeaseSchema = v.object({
    organization: v.boolean(),
    discountId: v.optional(v.pipe(v.string(), v.cuid2())),
    customerId: v.pipe(v.string(), v.cuid2()),
+   alternativeContactId: v.optional(v.pipe(v.string(), v.cuid2())),
    paymentType: v.enum(PaymentType),
 })
 export type NewLeaseSchema = typeof newLeaseSchema;
