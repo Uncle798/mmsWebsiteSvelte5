b7aeff442495c53cbcfa88aaefcbba0b175a44d2
Eric Branson <eric.branson@gmail.com>
2025-11-11 16:26:28 -0700

    removing tool tips

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,25 +64,12 @@
 
 {#await data.availableUnits}
    <article class="m-2 mt-14 sm:mt-10">
-      <div>
-         <Tooltip open={copyTooltipOpen}>
-            <Tooltip.Trigger>
-                  Thank you for visiting {PUBLIC_COMPANY_NAME}!
-                  Conveniently located, {PUBLIC_COMPANY_NAME} is the place to safely and securely store your belongings.
-                  <p>Family owned and operated, you can contact us at 
-                     <a href="tel:{PUBLIC_PHONE}" class="anchor">
-                        { formattedPhone }</a>, or <a href="mailto:{PUBLIC_COMPANY_EMAIL}" class="anchor">{PUBLIC_COMPANY_EMAIL}</a> the office and gates are open 8:00 am to 8:00 pm.
-                  </p>
-            </Tooltip.Trigger>
-            <Portal>
-               <Tooltip.Positioner>
-                  <Tooltip.Content class='card bg-surface-300-700 p-2'>
-                     Tell your story here. We can customize copy to your exact specifications.
-                  </Tooltip.Content>
-               </Tooltip.Positioner>
-            </Portal>
-         </Tooltip>
-      </div>
+      Thank you for visiting {PUBLIC_COMPANY_NAME}!
+      Conveniently located, {PUBLIC_COMPANY_NAME} is the place to safely and securely store your belongings.
+      <p>Family owned and operated, you can contact us at 
+         <a href="tel:{PUBLIC_PHONE}" class="anchor">
+            { formattedPhone }</a>, or <a href="mailto:{PUBLIC_COMPANY_EMAIL}" class="anchor">{PUBLIC_COMPANY_EMAIL}</a> the office and gates are open 8:00 am to 8:00 pm.
+      </p>
    </article>
    <div in:fade={{duration:600}} out:fade={{duration:0}}>
       <span class="mx-2">Loading available units...</span>
@@ -93,29 +80,16 @@
    </div>
    {:then units}
       <article class="m-2 mt-14 sm:mt-10">
-         <div>
-            <Tooltip>
-               <Tooltip.Trigger>
-                  <div class="homeCopy">
-                     Thank you for visiting {PUBLIC_COMPANY_NAME}!
-                     Conveniently located, {PUBLIC_COMPANY_NAME} is the place to safely and securely store your belongings.
-                     <p>Family owned and operated, you can contact us at 
-                        <a href="tel:{PUBLIC_PHONE}" class="anchor">
-                           { formattedPhone }</a>, or <a href="mailto:{PUBLIC_COMPANY_EMAIL}" class="anchor">{PUBLIC_COMPANY_EMAIL}</a> the office and gates are open 8:00 am to 8:00 pm.
-                     </p>
-                  </div>
-               </Tooltip.Trigger>
-               <Portal>
-                  <Tooltip.Positioner>
-                     <Tooltip.Content class='card bg-surface-300-700 p-2'>
-                        Tell your story here. We can customize copy to your exact specifications.
-                     </Tooltip.Content>
-                  </Tooltip.Positioner>
-               </Portal>
-            </Tooltip>
+         <div class="homeCopy">
+            Thank you for visiting {PUBLIC_COMPANY_NAME}!
+            Conveniently located, {PUBLIC_COMPANY_NAME} is the place to safely and securely store your belongings.
+            <p>Family owned and operated, you can contact us at 
+               <a href="tel:{PUBLIC_PHONE}" class="anchor">
+                  { formattedPhone }</a>, or <a href="mailto:{PUBLIC_COMPANY_EMAIL}" class="anchor">{PUBLIC_COMPANY_EMAIL}</a> the office and gates are open 8:00 am to 8:00 pm.
+            </p>
          </div>
       </article>
-   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mx-2 mb-24 sm:mb-14 lg:mb-9" in:fade={{duration:600}}>
+   <!-- <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 mx-2 mb-24 sm:mb-14 lg:mb-9" in:fade={{duration:600}}>
       <h3 class="h3 text-center lg:col-start-2 lg:col-end-4 sm:col-span-2 md:col-start-2 md:col-end-3 ">Available Units</h3>
       <div class="col-span-1 sm:col-span-2 md:col-start-3 md:col-span-1 lg:col-start-4">
          <label for="size" class="label-text">Filter by size: 
@@ -149,5 +123,5 @@
    </div>
    {#if data.demoCookie}
       <button class="btn preset-filled-primary-50-950 h-8 mx-2" onclick={()=>{mainPageTour.drive()}}>Restart tour</button>
-   {/if}
+   {/if} -->
 {/await}
