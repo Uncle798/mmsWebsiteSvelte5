5f1db48f42fd94bb1a21d52a81a5396f01374c57
Eric Branson <eric.branson@gmail.com>
2025-11-12 13:46:51 -0700

    address is optional

--- src/lib/formSchemas/alternativeContactFormSchema.ts
+++ src/lib/formSchemas/alternativeContactFormSchema.ts
@@ -1,11 +1,18 @@
 import * as v from 'valibot';
 import { nameFormSchema } from './nameFormSchema';
-import { addressFormSchema } from './addressFormSchema';
 
 export const alternativeContactFormSchema = v.object({
+   ...nameFormSchema.entries,   
    email: v.pipe(v.string(), v.email()),
    leaseId: v.pipe(v.string(), v.cuid2()),
-   ...nameFormSchema.entries,
-   ...addressFormSchema.entries,
+   organizationName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
+   address1: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   address2: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   city: v.optional(v.string()),
+   state: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   postalCode: v.optional(v.pipe(v.string(), v.minLength(5), v.maxLength(9))),
+   country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
+   phoneNum1: v.optional(v.pipe(v.string(), v.minLength(10), v.maxLength(14))),
+   phoneNum1Country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
 });
 export type AlternativeContactFormSchema = typeof alternativeContactFormSchema;
