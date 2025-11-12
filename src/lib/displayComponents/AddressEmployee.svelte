583d6f4a18c1c7fee063f5a2e085e286b9878fe3
Eric Branson <eric.branson@gmail.com>
2025-11-12 13:43:52 -0700

    address alternative

--- src/lib/displayComponents/AddressEmployee.svelte
+++ src/lib/displayComponents/AddressEmployee.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type { Address } from "@prisma/client";
-	import { onMount } from "svelte";
    interface Props {
       address: Address;
       classes?: string;
@@ -10,10 +9,14 @@
 </script>
 
 <div class="{classes}">
-   <a href="tel:{address.phoneNum1}" class="anchor">{address.phoneNum1.substring(0,3) + '.' + address.phoneNum1.substring(3,6) + '.' + address.phoneNum1.substring(6,10)}</a>
+   {#if address.phoneNum1}
+      <a href="tel:{address.phoneNum1}" class="anchor">{address.phoneNum1.substring(0,3) + '.' + address.phoneNum1.substring(3,6) + '.' + address.phoneNum1.substring(6,10)}</a>
+   {/if}
    <p>{address.address1}</p>
    {#if address.address2}
       <p>{address.address2}</p>
    {/if}
-   <p>{address.city}, {address.state}, {address.postalCode}</p>
+   {#if address.city && address.state}
+      <p>{address.city}, {address.state}, {address.postalCode}</p>
+   {/if}
 </div>
