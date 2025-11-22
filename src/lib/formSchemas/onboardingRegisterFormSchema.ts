9bb808205ebb5fd7e41c0f148933a9effb6f817a
Eric Branson <eric.branson@gmail.com>
2025-11-22 14:38:24 -0800

    everything optional

--- src/lib/formSchemas/onboardingRegisterFormSchema.ts
+++ src/lib/formSchemas/onboardingRegisterFormSchema.ts
@@ -3,10 +3,10 @@ import * as v from 'valibot';
 
 export const onboardingRegisterFormSchema = v.pipe(
    v.object({
-      familyName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
-      givenName: v.pipe(v.string(), v.minLength(1), v.maxLength(255)),
+      familyName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
+      givenName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
       organizationName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
-      email: v.pipe(v.string(), v.email()),
+      email: v.optional(v.pipe(v.string(), v.email())),
    }),
 );
 export type OnboardingRegisterFormSchema = typeof onboardingRegisterFormSchema;
