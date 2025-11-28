c8ec4c74a7889a1f662e5b50732f257f5ee878ab
Eric Branson <eric.branson@gmail.com>
2025-11-28 16:30:28 -0700

    everything optional

--- src/lib/formSchemas/addressFormSchema.ts
+++ src/lib/formSchemas/addressFormSchema.ts
@@ -2,14 +2,14 @@ import * as v from 'valibot';
 
 
 export const addressFormSchema = v.object({
-   address1: v.pipe(v.string(), v.minLength(2), v.maxLength(255)),
+   address1: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
    address2: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
-   city: v.string(),
-   state: v.pipe(v.string(), v.minLength(2), v.maxLength(255)),
-   postalCode: v.pipe(v.string(), v.minLength(5), v.maxLength(9)),
-   country: v.pipe(v.string(), v.minLength(2), v.maxLength(2)),
-   phoneNum1: v.pipe(v.string(), v.minLength(10), v.maxLength(14)),
-   phoneNum1Country: v.pipe(v.string(), v.minLength(2), v.maxLength(2)),
+   city: v.optional(v.string()),
+   state: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   postalCode: v.optional(v.pipe(v.string(), v.minLength(5), v.maxLength(9))),
+   country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
+   phoneNum1: v.optional(v.pipe(v.string(), v.minLength(10), v.maxLength(14))),
+   phoneNum1Country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
    userId: v.pipe(v.string(), v.cuid2()),
 });
 export type AddressFormSchema = typeof addressFormSchema;
