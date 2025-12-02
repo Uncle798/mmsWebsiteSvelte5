d47696c3200ba6a0c12dd8b8f0ac95fa94544a2b
Eric Branson <eric.branson@gmail.com>
2025-12-02 12:42:13 -0700

    collapsible details

--- src/lib/displayComponents/LeaseEmployee.svelte
+++ src/lib/displayComponents/LeaseEmployee.svelte
@@ -2,42 +2,47 @@
    import type { Lease } from "../../generated/prisma/browser";
 	import dayjs from "dayjs";
 	import HorizontalDivider from "./HorizontalDivider.svelte";
+   import { Collapsible } from "@skeletonlabs/skeleton-svelte";
    interface Props {
       lease: Lease;
+      open?: boolean;
       classes?: string;
    }
-   let { lease, classes }:Props = $props();
+   let { lease, classes, open = $bindable()}:Props = $props();
    const currencyFormatter = new Intl.NumberFormat('en-US', {style:'currency', currency:'USD'});
 </script>
-
-<div class=" {classes} grid grid-cols-2 gap-x-2">
-   <div class="col-span-2 text-center text-sm font-semibold">Lease</div>
-   <div class="text-right">Unit number</div>
-   <div class="font-medium"><a href="/units/{lease.unitNum}" class="anchor">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
-   <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right -indent-2">Effective date</div>
-   <div class="font-medium">{dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</div>
-   <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right">Price</div>
-   <div class="font-medium">{currencyFormatter.format(lease.price)}</div>
-   <HorizontalDivider classes="col-span-2"/>
-   {#if lease.leaseEnded}
-      <div class="text-right">End date</div>
-      <div class="font-medium"> {dayjs(lease.leaseEnded).format('M/D/YYYY')}</div>
+<Collapsible {open} onOpenChange={(d) => open = d.open}>
+   <Collapsible.Trigger class='{classes} grid grid-cols-2 gap-x-2'>
+      <div class="col-span-2 text-center text-sm font-semibold">Lease</div>
+      <div class="text-right">Unit number</div>
+      <div class="font-medium"><a href="/units/{lease.unitNum}" class="anchor">{lease.unitNum.replace(/^0+/gm,'')}</a></div>
       <HorizontalDivider classes="col-span-2"/>
-   {:else}
-      <div class="font-medium col-span-2 text-center">Current Customer</div>
+   </Collapsible.Trigger>
+   <Collapsible.Content class='grid grid-cols-2 gap-x-2'>
+      <div class="text-right -indent-2">Effective date</div>
+      <div class="font-medium">{dayjs(lease.leaseEffectiveDate).format('M/D/YYYY')}</div>
       <HorizontalDivider classes="col-span-2"/>
-   {/if}
-
-   <div class="text-right">lease EID</div>
-   <div class="truncate font-medium">{lease.anvilEID}</div>
-   <HorizontalDivider classes="col-span-2"/>
-   <div class="text-right">leaseID</div>
-   <div class="truncate font-medium"><a href="/leases/{lease.leaseId}" class="anchor">{lease.leaseId}</a></div>
-   {#if lease.subscriptionId}
-      On auto-pay
-   {:else if !lease.leaseEnded}
-      <a href="/autopay?leaseId={lease.leaseId}" class="anchor mx-2 col-span-2">Sign up for Auto-pay</a>
-   {/if}
-</div>
+      <div class="text-right">Price</div>
+      <div class="font-medium">{currencyFormatter.format(lease.price)}</div>
+      <HorizontalDivider classes="col-span-2"/>
+      {#if lease.leaseEnded}
+         <div class="text-right">End date</div>
+         <div class="font-medium"> {dayjs(lease.leaseEnded).format('M/D/YYYY')}</div>
+         <HorizontalDivider classes="col-span-2"/>
+      {:else}
+         <div class="font-medium col-span-2 text-center">Current Customer</div>
+         <HorizontalDivider classes="col-span-2"/>
+      {/if}
+   
+      <div class="text-right">lease EID</div>
+      <div class="truncate font-medium">{lease.anvilEID}</div>
+      <HorizontalDivider classes="col-span-2"/>
+      <div class="text-right">leaseID</div>
+      <div class="truncate font-medium"><a href="/leases/{lease.leaseId}" class="anchor">{lease.leaseId}</a></div>
+      {#if lease.subscriptionId}
+         On auto-pay
+      {:else if !lease.leaseEnded}
+         <a href="/autopay?leaseId={lease.leaseId}" class="anchor mx-2 col-span-2">Sign up for Auto-pay</a>
+      {/if}
+   </Collapsible.Content>
+</Collapsible>
