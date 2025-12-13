d38c18a7ebeca21f314a25a3c2e04d6cc8d71c60
Eric Branson <eric.branson@gmail.com>
2025-12-13 16:16:08 -0700

    multiple invoice numbers

--- src/routes/api/downloadPDF/+server.ts
+++ src/routes/api/downloadPDF/+server.ts
@@ -2,16 +2,16 @@ import { makeReceiptPdf } from "$lib/server/pdfMake/makeReceiptPdf";
 import { makeRefundPdf } from "$lib/server/pdfMake/makeRefundPdf";
 import { makeInvoicePdf } from "$lib/server/pdfMake/makeInvoicePdf";
 import { prisma } from "$lib/server/prisma";
-import { Prisma } from '@prisma/client'
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { PUBLIC_COMPANY_NAME } from "$env/static/public";
 import type { Invoice } from "../../../generated/prisma/client";
+import { makeMultipleInvoicesPDF } from "$lib/server/pdfMake/makeMultipleInvoicesPDF";
 
 export const GET:RequestHandler = async (event) => {
    const refundNum = event.url.searchParams.get('refundNum');
    const paymentNum = event.url.searchParams.get('paymentNum');
-   const invoiceNum = event.url.searchParams.get('invoiceNum');
    const invoiceNumbers = event.url.searchParams.getAll('invoiceNum');
+   console.log(invoiceNumbers)
    if(refundNum){
       const refund = await prisma.refundRecord.findUnique({
          where: {
@@ -114,74 +114,45 @@ export const GET:RequestHandler = async (event) => {
             message: 'No invoices found'
          });
       }
-      if(invoices.length === 1){
-         const customer = await prisma.user.findUnique({
-            where: {
-               id: invoices[0].customerId
-            }
-         });
-         if(!customer){
-            throw error(500, {message: 'Customer not found'});
-         }
-         const address = await prisma.address.findFirst({
-            where: {
-               AND: [
-                  {
-                     userId: customer.id,
-                  },
-                  {
-                     softDelete: false,
-                  }
-               ]
-            }
-         });
-         if(!address){
-            throw error(500, 'Address not found');
-         }
-         const pdf = await makeInvoicePdf(invoices[0], customer, address, true) as Blob;
-         return new Response(pdf, {
-            status: 200,
-         })
-      }
-   }
-   if(invoiceNum){
-      const invoice = await prisma.invoice.findUnique({
+      const customer = await prisma.user.findUnique({
          where: {
-            invoiceNum: parseInt(invoiceNum, 10)
+            id: invoices[0].customerId
          }
-      })
-      if(invoice){
-         const customer = await prisma.user.findUnique({
-            where: {
-               id: invoice.customerId
-            }
-         })
-         if(!customer){
-            throw error(500, {
-               message: 'Customer not found'
-            })
-         }
-         const address = await prisma.address.findFirst({
-            where: {
-               AND: [
-                  {userId: customer.id},
-                  {softDelete: false}
-               ]
-            }
-         })
-         if(!address){
-            throw error(500, { 
-               message: 'Address not found'
-            })
-         }
-         const pdf = await makeInvoicePdf(invoice, customer, address, true) as Blob;
-         return new Response(pdf, {
-            status: 200
-         })
-      }
-      throw error(404, {
-         message: 'Invoice not found'
       });
+      if(!customer){
+         throw error(500, {message: 'Customer not found'});
+      }
+      const address = await prisma.address.findFirst({
+         where: {
+            AND: [
+               {
+                  userId: customer.id,
+               },
+               {
+                  softDelete: false,
+               }
+            ]
+         }
+      });
+      if(!address){
+         throw error(500, 'Address not found');
+      }
+      if(invoices.length === 1){
+         const pdf = await makeInvoicePdf(invoices[0], customer, address, true) as Blob;
+         const res = new Response(pdf, {
+            status: 200,
+         });
+         res.headers.set('Content-Disposition', `attachment; filename="${PUBLIC_COMPANY_NAME} invoice number ${invoices[0].invoiceNum}.pdf`);
+         return res;
+      } else {
+         const pdf = await makeMultipleInvoicesPDF(invoices, customer, address, true) as Blob;
+         const res = new Response(pdf, {
+            status: 200,
+         });
+         res.headers.set('Content-Disposition', `attachment; filename="${PUBLIC_COMPANY_NAME} invoice number ${invoices[0].invoiceNum}.pdf`);
+         console.log(res);
+         return res;
+      }
    }
    throw error(400, {
       message: 'Record Number not provided'
