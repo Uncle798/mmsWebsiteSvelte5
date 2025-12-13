68dad3bd1a59ab46bbd9b992faa0e9cb866f0040
Eric Branson <eric.branson@gmail.com>
2025-12-13 10:07:32 -0700

    towards file names

--- src/routes/api/downloadPDF/+server.ts
+++ src/routes/api/downloadPDF/+server.ts
@@ -2,12 +2,16 @@ import { makeReceiptPdf } from "$lib/server/pdfMake/makeReceiptPdf";
 import { makeRefundPdf } from "$lib/server/pdfMake/makeRefundPdf";
 import { makeInvoicePdf } from "$lib/server/pdfMake/makeInvoicePdf";
 import { prisma } from "$lib/server/prisma";
+import { Prisma } from '@prisma/client'
 import { error, type RequestHandler } from "@sveltejs/kit";
+import { PUBLIC_COMPANY_NAME } from "$env/static/public";
+import type { Invoice } from "../../../generated/prisma/client";
 
 export const GET:RequestHandler = async (event) => {
    const refundNum = event.url.searchParams.get('refundNum');
    const paymentNum = event.url.searchParams.get('paymentNum');
    const invoiceNum = event.url.searchParams.get('invoiceNum');
+   const invoiceNumbers = event.url.searchParams.getAll('invoiceNum');
    if(refundNum){
       const refund = await prisma.refundRecord.findUnique({
          where: {
@@ -38,6 +42,8 @@ export const GET:RequestHandler = async (event) => {
          const pdf = await makeRefundPdf(refund, customer, address, true) as Blob
          event.setHeaders({
             'Content-Type': 'application/pdf',
+            'Content-Disposition': 'inline',
+            'filename': `${PUBLIC_COMPANY_NAME} refund ${refund.refundNumber}`
          })
          return new Response(pdf,{
             status: 200
@@ -81,13 +87,62 @@ export const GET:RequestHandler = async (event) => {
          event.setHeaders({
             'Content-Type': 'application/pdf'
          })
-         return new Response(pdf, {
-            status: 200
+         const res = new Response(pdf, {
+            status: 200,
          });
+         res.headers.set('Content-Disposition', 'attachment; filename``')
+         return res
       }
       throw error(404, {
          message: 'Payment not found'
       })
+   }
+   if(invoiceNumbers.length > 0){
+      const invoices:Invoice[] = [];
+      for(const invoiceNum of invoiceNumbers){
+         const invoice = await prisma.invoice.findUnique({
+            where: {
+               invoiceNum: parseInt(invoiceNum, 10),
+            },
+         });
+         if(invoice){
+            invoices.push(invoice);
+         }
+      }
+      if(invoices.length === 0){
+         throw error(404, {
+            message: 'No invoices found'
+         });
+      }
+      if(invoices.length === 1){
+         const customer = await prisma.user.findUnique({
+            where: {
+               id: invoices[0].customerId
+            }
+         });
+         if(!customer){
+            throw error(500, {message: 'Customer not found'});
+         }
+         const address = await prisma.address.findFirst({
+            where: {
+               AND: [
+                  {
+                     userId: customer.id,
+                  },
+                  {
+                     softDelete: false,
+                  }
+               ]
+            }
+         });
+         if(!address){
+            throw error(500, 'Address not found');
+         }
+         const pdf = await makeInvoicePdf(invoices[0], customer, address, true) as Blob;
+         return new Response(pdf, {
+            status: 200,
+         })
+      }
    }
    if(invoiceNum){
       const invoice = await prisma.invoice.findUnique({
