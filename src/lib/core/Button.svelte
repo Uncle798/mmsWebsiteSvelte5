351f653c89141836b65e7b402e2d0ff81c46dc7b
Eric Branson <eric.branson@gmail.com>
2025-12-14 09:19:10 -0700

    disabled

--- src/lib/core/Button.svelte
+++ src/lib/core/Button.svelte
@@ -3,10 +3,18 @@
       label: string;
       onClick?: ()=>void;
       classes?: string;
+      disabled?: boolean;
       type: 'button' | 'submit' | 'reset' | null | undefined
    }
-   let { label, onClick, classes, type }:Props = $props();
+   let { label, onClick, classes, type, disabled }:Props = $props();
 
 </script>
 
-<button class="btn preset-filled-primary-50-950 h-8 {classes}" {type} onclick={onClick}>{label}</button>
+<button 
+   class="btn preset-filled-primary-50-950 h-8 {classes}" 
+   {type}
+   onclick={onClick}
+   disabled={disabled}
+>
+   {label}
+</button>
