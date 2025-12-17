0bdaada998efed25c75cd21bd95a96a1f405caef
Eric Branson <eric.branson@gmail.com>
2025-12-17 10:38:00 -0700

    towards a new expense form

--- src/lib/forms/NewExpenseForm.svelte
+++ src/lib/forms/NewExpenseForm.svelte
@@ -1,11 +1,48 @@
 <script lang='ts'>
+	import FormMessage from "$lib/formComponents/FormMessage.svelte";
+	import FormSubmitWithProgress from "$lib/formComponents/FormSubmitWithProgress.svelte";
+	import NumberInput from "$lib/formComponents/NumberInput.svelte";
+	import TextArea from "$lib/formComponents/TextArea.svelte";
 	import type { NewExpenseFormSchema } from "$lib/formSchemas/newExpenseFormSchema";
 	import type { Infer, SuperValidated } from "sveltekit-superforms";
+   import { superForm } from "sveltekit-superforms";
 
    interface Props {
       data: SuperValidated<Infer<NewExpenseFormSchema>>;
-      
+      employeeId: string;
+      vendorId: string;
+      modalOpen?: boolean;
+      classes?: string;
    }
-   let { data }:Props = $props()
-   
+   let { data, employeeId, vendorId, modalOpen=$bindable(), classes }:Props = $props()
+     // svelte-ignore state_referenced_locally
+   let { form, errors, message, constraints, enhance, delayed, timeout, } = superForm(data, {
+      onUpdated() {
+         if(!$message && !$errors){
+            modalOpen = false;
+         }
+      }
+   });
 </script>
+<div class="{classes}">
+   <FormMessage message={$message} />
+   <form action="/forms/newExpenseForm" method="POST" use:enhance>
+      <TextArea
+         value={$form.explanation}
+         errors={$errors.explanation}
+         constraints={$constraints.explanation}
+         name='explanation'
+         label='Explanation'
+      />
+      <NumberInput
+         value={$form.amount}
+         errors={$errors.amount}
+         constraints={$constraints.amount}
+         name='amount'
+         label='Amount'
+      />
+      <input type="hidden" name='vendorId' id='vendorId' value={vendorId} />
+      <input type="hidden" name="employeeId" id="employeeId" value={employeeId} />
+      <FormSubmitWithProgress delayed={$delayed} timeout={$timeout} />
+   </form>
+</div>
--- src/routes/expenses/new/+page.svelte
+++ src/routes/expenses/new/+page.svelte
@@ -1,5 +1,46 @@
 <script lang="ts">
+	import { Steps } from '@skeletonlabs/skeleton-svelte';
     import type { PageProps } from './$types';
+	import Combobox from '$lib/formComponents/Combobox.svelte';
+	import NewVendorForm from '$lib/forms/NewVendorForm.svelte';
+	import NewExpenseForm from '$lib/forms/NewExpenseForm.svelte';
 
     let { data }: PageProps = $props();
+    let comboboxData = $derived(data.vendors.map((vendor) => {
+        return {
+            label: vendor.organizationName ? vendor.organizationName : `${vendor.givenName} ${vendor.familyName}`,
+            value: vendor.id,
+        }
+    }));
+    let step = $derived(data.step);
 </script>
+<div class=" mb-8">
+    <Steps count={2} {step} onStepChange={(details) => (step = details.step)}>
+        <Steps.List>
+            <Steps.Item index={0}>
+                <Steps.Trigger>
+                    <Steps.Indicator>1</Steps.Indicator>
+                    Chose vendor
+                </Steps.Trigger>
+                <Steps.Separator />
+            </Steps.Item>
+            <Steps.Item index={1}>
+                <Steps.Trigger>
+                    <Steps.Indicator>2</Steps.Indicator>
+                    Enter expense details
+                </Steps.Trigger>
+            </Steps.Item>
+        </Steps.List>
+        <Steps.Content index={0}>
+            <Combobox
+                data={comboboxData}
+                label='Select vendor'
+            />
+            or
+            <NewVendorForm data={data.newVendorForm} />
+        </Steps.Content>
+        <Steps.Content index={1}>
+            <NewExpenseForm data={data.newExpenseForm} employeeId = />
+        </Steps.Content> 
+    </Steps>
+</div>
