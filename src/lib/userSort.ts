8a9168e1c65b8d8591dac6f5474a797b9848c1c9
Eric Branson <eric.branson@gmail.com>
2025-12-26 13:50:51 -0700

    corrected sort

--- src/lib/userSort.ts
+++ src/lib/userSort.ts
@@ -4,33 +4,33 @@ export function sortUsers(users:User[]){
    return users.sort((a, b) => {
       if(a.organizationName && b.organizationName){
          if(a.organizationName > b.organizationName){
-            return 1;
-         } else if(a.organizationName < b.organizationName){
             return -1;
+         } else if(a.organizationName < b.organizationName){
+            return 1;
          } else{
             return 0;
          }
       }else if(a.organizationName && b.familyName){
          if(a.organizationName > b.familyName){
-            return 1;
-         }else if(a.organizationName < b.familyName){
             return -1;
+         }else if(a.organizationName < b.familyName){
+            return 1;
          }else{
             return 0;
          }
       }else if(a.familyName && b.organizationName){
          if(a.familyName > b.organizationName){
-            return 1;
-         }else if(a.familyName < b.organizationName){
             return -1;
+         }else if(a.familyName < b.organizationName){
+            return 1;
          }else{
             return 0;
          }
       }else if(a.familyName && b.familyName){
          if(a.familyName > b.familyName){
-            return 1;
-         }else if(a.familyName < b.familyName){
             return -1;
+         }else if(a.familyName < b.familyName){
+            return 1;
          }else {
             return 0;
          }
