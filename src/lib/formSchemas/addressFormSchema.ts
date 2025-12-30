42776b5273f10e5fe97b662ed0a34b66536baa6d
Eric Branson <eric.branson@gmail.com>
2025-12-30 14:57:53 -0700

    userId optional

--- src/lib/formSchemas/addressFormSchema.ts
+++ src/lib/formSchemas/addressFormSchema.ts
@@ -10,6 +10,6 @@ export const addressFormSchema = v.object({
    country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
    phoneNum1: v.optional(v.pipe(v.string(), v.minLength(10), v.maxLength(14))),
    phoneNum1Country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
-   userId: v.pipe(v.string(), v.cuid2()),
+   userId: v.optional(v.pipe(v.string(), v.cuid2())),
 });
 export type AddressFormSchema = typeof addressFormSchema;
