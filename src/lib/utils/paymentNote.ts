faa689f0ed678910e59552e8884d966ad3e7478f
Eric Branson <eric.branson@gmail.com>
2026-01-01 11:57:20 -0700

    better copy

--- src/lib/utils/paymentNote.ts
+++ src/lib/utils/paymentNote.ts
@@ -1,6 +1,6 @@
 export function paymentNote(invoiceNum: number, invoiceNotes: string | null){
    if(invoiceNotes){
-      return `Payment for ${invoiceNum.toString()}, ${invoiceNotes}`
+      return `Payment for invoice ${invoiceNum.toString()}, ${invoiceNotes}`
    } else {
       return `Payment for ${invoiceNum.toString}.`
    }
