fb79e0554adc34c1302ef023d32ade5f8382b15e
Eric Branson <eric.branson@gmail.com>
2026-01-04 13:58:53 -0700

    states enum

--- src/lib/formSchemas/addressFormSchema.ts
+++ src/lib/formSchemas/addressFormSchema.ts
@@ -1,11 +1,11 @@
 import * as v from 'valibot';
-
+import { States } from '../../generated/prisma/enums';
 
 export const addressFormSchema = v.object({
    address1: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
    address2: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
    city: v.optional(v.string()),
-   state: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   state: v.optional(v.enum(States)),
    postalCode: v.optional(v.pipe(v.string(), v.minLength(5), v.maxLength(9))),
    country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
    phoneNum1: v.optional(v.pipe(v.string(), v.minLength(10), v.maxLength(14))),
@@ -13,3 +13,5 @@ export const addressFormSchema = v.object({
    userId: v.optional(v.pipe(v.string(), v.cuid2())),
 });
 export type AddressFormSchema = typeof addressFormSchema;
+
+
--- src/lib/formSchemas/alternativeContactFormSchema.ts
+++ src/lib/formSchemas/alternativeContactFormSchema.ts
@@ -1,14 +1,14 @@
 import * as v from 'valibot';
+import { States } from '../../generated/prisma/enums';
 
 export const alternativeContactFormSchema = v.object({
    familyName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
-   givenName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),   
-   leaseId: v.pipe(v.string(), v.cuid2()),
+   givenName: v.optional(v.pipe(v.string(), v.minLength(1), v.maxLength(255))),
    email: v.optional(v.pipe(v.string(), v.email())),
    address1: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
    address2: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
    city: v.optional(v.string()),
-   state: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(255))),
+   state: v.optional(v.enum(States)),
    postalCode: v.optional(v.pipe(v.string(), v.minLength(5), v.maxLength(9))),
    country: v.optional(v.pipe(v.string(), v.minLength(2), v.maxLength(2))),
    phoneNum1: v.optional(v.pipe(v.string(), v.minLength(10), v.maxLength(14))),
