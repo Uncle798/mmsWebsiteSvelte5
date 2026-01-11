e2c788727a5c31f384d739ee3789cdb61461751a
Eric Branson <eric.branson@gmail.com>
2026-01-11 10:40:12 -0700

    styling cleanup

--- src/routes/users/[userId]/+page.svelte
+++ src/routes/users/[userId]/+page.svelte
@@ -25,7 +25,6 @@
 	import NewPaymentRecordForm from '$lib/forms/NewPaymentRecordForm.svelte';
 	import { PUBLIC_COMPANY_NAME } from '$env/static/public';
 	import SearchDrawer from '$lib/displayComponents/Modals/SearchDrawer.svelte';
-	import Search from '$lib/forms/Search.svelte';
 	import Combobox from '$lib/formComponents/Combobox.svelte';
 	import { goto } from '$app/navigation';
 	import { MenuIcon } from 'lucide-svelte';
@@ -34,7 +33,6 @@
    let { data }: { data: PageData } = $props();
    let modalOpen = $state(false);
    let modalReason = $state('');
-   let currentLeaseId = $state('');
    let pageNum = $state(1);
    let size = $state(5);
    const slicedInvoices = $derived((invoices:Invoice[]) => invoices.slice((pageNum - 1) * size, pageNum * size));
@@ -180,8 +178,8 @@
 
 {#if data.dbUser}
    <Header title={ data.dbUser.organizationName ? data.dbUser.organizationName : `${data.dbUser.givenName} ${data.dbUser.familyName}`} />
-   <div class="grid grid-cols-1 mt-14 sm:mt-10 mx-1 sm:mx-2">
-      <UserEmployee user={data.dbUser} classes='place-self-center'/>
+   <div class="relative mt-12 sm:mt-10 mx-2 sm:mx-2">
+      <UserEmployee user={data.dbUser} classes=''/>
          <Menu
             onSelect={(d) => {
                switch (d.value) {
@@ -195,7 +193,7 @@
                }
             }}
          >
-            <Menu.Trigger class='btn preset-filled-primary-50-950 absolute top-10 left-2'><MenuIcon class='size-6' /></Menu.Trigger>
+            <Menu.Trigger class='btn preset-filled-primary-50-950 absolute top-2 left-2'><MenuIcon class='size-6' /></Menu.Trigger>
             <Portal>
                <Menu.Positioner>
                   <Menu.Content>
@@ -244,15 +242,13 @@
       Loading leases...
    </div>
 {:then leases}
-   <div class="grid grid-cols-1 sm:grid-cols-2">
+   <div class=" columns-2">
       {#each leases as lease}
-         <div class="rounded-lg border-2 border-primary-50 dark:border-primary-950 flex flex-col m-2 relative">
+         <div class="rounded-lg border-2 border-primary-50-950 flex flex-col m-2 relative">
             <LeaseEmployee lease={lease} classes='m-2' open={true} />
-            {#if !lease?.leaseEnded}
             <Menu
                onSelect={(e) => {
                   switch (e.value) {
-
                      default:
                         modalReason = e.value;
                         currentLease = lease;
@@ -265,20 +261,19 @@
                <Portal>
                   <Menu.Positioner>
                      <Menu.Content>
-                        <Menu.Item value='endLease'>
-                           <Menu.ItemText>End lease</Menu.ItemText>
+                        <Menu.Item value='endLease' disabled={lease.leaseEnded ? true : undefined}>
+                           <Menu.ItemText>End lease </Menu.ItemText>
                         </Menu.Item>
-                        <Menu.Item value='newInvoice'>
+                        <Menu.Item value='newInvoice' disabled={lease.leaseEnded ? true : undefined}>
                            <Menu.ItemText>Make single invoice for this lease</Menu.ItemText>
                         </Menu.Item>
-                        <Menu.Item value='manyInvoices'>
+                        <Menu.Item value='manyInvoices' disabled={lease.leaseEnded ? true : undefined}>
                            <Menu.ItemText>Create many invoices for this lease</Menu.ItemText>
                         </Menu.Item>
                      </Menu.Content>
                   </Menu.Positioner>
                </Portal>
             </Menu>
-            {/if}
          </div>
       {/each}
    </div>
