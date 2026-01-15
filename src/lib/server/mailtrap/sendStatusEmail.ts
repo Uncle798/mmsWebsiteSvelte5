ebc912e47d7d66ec33e6e7d354f0df57b5a304cc
Eric Branson <eric.branson@gmail.com>
2026-01-15 14:28:09 -0700

    better formatting and copy

--- src/lib/server/mailtrap/sendStatusEmail.ts
+++ src/lib/server/mailtrap/sendStatusEmail.ts
@@ -5,7 +5,7 @@ import { buffer } from "stream/consumers";
 import type { User, Lease, PaymentRecord } from "../../../generated/prisma/client";
 import { mailtrap, sender } from "./mailtrap";
 
-export async function sendStatusEmail(admin: User, invoiceCount: number, totalInvoice: number, emptyUnits: number, leases:Lease[], pdf: PDFKit.PDFDocument, payments:PaymentRecord[], paymentsPdf: PDFKit.PDFDocument) {
+export async function sendStatusEmail(admin: User, invoiceCount: number, totalInvoice: number, emptyUnits: number, leases:Lease[], invoicePdf: PDFKit.PDFDocument, payments:PaymentRecord[], paymentsPdf: PDFKit.PDFDocument) {
    if (admin.email?.includes('veryFakeEmail.com'.toLowerCase()) || admin.email?.includes('yetAnotherFakeEmail.com'.toLowerCase())) {
       return null;
    }
@@ -15,11 +15,11 @@ export async function sendStatusEmail(admin: User, invoiceCount: number, totalIn
    }
    let html = `Hello ${admin.givenName}<br/> ${invoiceCount} invoices were created this morning totaling ${currencyFormatter(totalInvoice)}.\
       There are ${emptyUnits} empty units as of this morning. <br/>
-      Yesterday ${currencyFormatter(totalPaid)} in payments were recorded from ${payments.length} payments`;
+      Yesterday ${currencyFormatter(totalPaid)} in payments were recorded from ${payments.length} payments<br/>`;
    for(const lease of leases){
       html += `${lease.unitNum} <br/>`
    }
-   const buf = await buffer(pdf);
+   const buf = await buffer(invoicePdf);
    const buf2 = await buffer(paymentsPdf);
    if (admin.admin) {
       try {
@@ -30,7 +30,7 @@ export async function sendStatusEmail(admin: User, invoiceCount: number, totalIn
             html,
             attachments: [
                {
-                  filename: `${PUBLIC_COMPANY_NAME} Invoices ${dayjs().format('MMMM D YYYY')}`,
+                  filename: `${PUBLIC_COMPANY_NAME} invoices ${dayjs().format('MMMM D YYYY')}.pdf`,
                   content: buf.toString('base64'),
                   type: 'application/pdf'
                },
