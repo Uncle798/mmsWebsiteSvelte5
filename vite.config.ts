87dd42c8b5199bf3c6cd8a56c3f7011494e70c8a
Eric Branson <eric.branson@gmail.com>
2026-02-04 10:25:50 -0700

    removing themes

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
-import { readdirSync, readFileSync } from 'fs';
-import { join } from 'path';
-
-const modulePath = 'node_modules/@skeletonlabs/skeleton/src/themes';
-const files = readdirSync(modulePath);
-const fileContents = files.map(file => {
-	return {
-		name: file.substring(0,file.indexOf('.')),
-		content: readFileSync(join(modulePath, file), 'utf-8')
-	}
-})
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
@@ -22,9 +11,6 @@ export default defineConfig({
 		allowedHosts: [
 			'relative-oryx-endlessly.ngrok-free.app'
 		]
-	},
-	define: {
-		THEMES: fileContents
 	},
 	ssr: {
 		external: ["@prisma/client"]
