2de001076be5144bee2ff010d630f82cbecaee01
Eric Branson <eric.branson@gmail.com>
2026-09-15 14:57:05 -0600

    better console.log

--- src/routes/units/+page.svelte
+++ src/routes/units/+page.svelte
@@ -232,7 +232,7 @@
 										valueState = fromStore(value);
 										csv = connection.select('csv');
 										csvState = fromStore(csv);
-										console.log(`/api/csv?allUnits=true&date=${csvDate.toISOString()}`)
+										console.log($state.snapshot(csvDate))
 									}}
 								/>
 								<DatePickerSingle
