7785a73b64711c1c5eba541c989e04af52f7a765
Eric Branson <eric.branson@gmail.com>
2026-09-24 14:27:34 -0600

    console.log

--- src/routes/api/csv/+server.ts
+++ src/routes/api/csv/+server.ts
@@ -501,6 +501,7 @@ export const POST: RequestHandler = async (event) => {
             }
          }
          csv.end();
+         console.log(data.join(''));
          emit('csv', data.join(''));
          emit('message', 'CSV ready');
          return function cancel(){};
