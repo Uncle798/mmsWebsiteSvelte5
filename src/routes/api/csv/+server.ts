c7aef57140f73a9b123ce8091551d85989509947
Eric Branson <eric.branson@gmail.com>
2026-09-24 15:26:25 -0600

    better formatting

--- src/routes/api/csv/+server.ts
+++ src/routes/api/csv/+server.ts
@@ -496,7 +496,7 @@ export const POST: RequestHandler = async (event) => {
                const json = {
                   'Size': humanUnitSize(size.size),
                   '# of Units': size.count,
-                  '%': Intl.NumberFormat().format(size.count / units.length),
+                  '%': Intl.NumberFormat('en-US', {style: 'percent'}).format((size.count / units.length)*100),
                   'SF': x*y,
                   'Total SF of Size': (x*y)*size.count,
                   monthlyRentKey: size.monthlyRent,
